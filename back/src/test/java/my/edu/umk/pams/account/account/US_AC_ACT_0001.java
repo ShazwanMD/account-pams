be83d6bd3447ededd27f77882dc51cfaacf9fb35
max.zain <max.zain@canang.com.my>
2017-03-08 09:59:01 +0800

    trying to get this test to run

--- back/src/test/java/my/edu/umk/pams/account/account/US_AC_ACT_0001.java
+++ back/src/test/java/my/edu/umk/pams/account/account/US_AC_ACT_0001.java
@@ -3,16 +3,22 @@ package my.edu.umk.pams.account.account;
 import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.account.TestSupport;
 import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.account.identity.dao.AcActorDao;
+import my.edu.umk.pams.account.identity.model.AcActor;
+import my.edu.umk.pams.account.identity.model.AcStaffImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author PAMS
@@ -28,9 +34,17 @@ public class US_AC_ACT_0001 extends TestSupport{
 
     private static final Logger LOG = LoggerFactory.getLogger(US_AC_ACT_0001.class);
 
+    @Autowired
+    private AcActorDao actorDao;
+
     @Before
     public void before() {
         super.before();
+        AcActor actor = new AcStaffImpl();
+        actor.setIdentityNo("ABC001");
+        actor.setName("Yo Name");
+
+        actorDao.save(actor,currentUser);
     }
 
     @After
@@ -39,9 +53,39 @@ public class US_AC_ACT_0001 extends TestSupport{
 
     @Test
     @Rollback
-    public void testSomething(){
+    public void test1(){
         Assert.notNull(currentUser);
         Assert.notNull("something");
+        LOG.debug("11111111111111111111");
+
+        AcActor actor = new AcStaffImpl();
+        actor.setIdentityNo("ABC123");
+        actor.setName("Mah Name");
+
+        actorDao.save(actor,currentUser);
+        actorDao.refresh(actor);
+    }
+
+    @Test
+    @Rollback
+    public void test2(){
+        Assert.notNull(currentUser);
+        Assert.notNull("something");
+        LOG.debug("2222222222222222");
+        AcActor actor = new AcStaffImpl();
+        actor.setIdentityNo("ABC12345");
+        actor.setName("Noah Name");
+
+        actorDao.save(actor,currentUser);
+        actorDao.refresh(actor);
+
+        List<AcActor> acActors = actorDao.find();
+        acActors.forEach(
+                acActor -> {
+                    LOG.debug(acActor.getId() + " <--");
+                    LOG.debug(acActor.getIdentityNo() + " <--");
+                    LOG.debug(acActor.getName() + " <--");
+                });
     }
 }
 
