d93f6a1460a36271b25e1c14df26681fb013eb1d
max.zain <max.zain@canang.com.my>
2017-03-09 15:18:23 +0800

    start filling these .sql files with potentially needed statements -- warning: Rough state, missing tables, columns and constraints

--- data/src/test/resources/ddl/CREATE.sql
+++ data/src/test/resources/ddl/CREATE.sql
@@ -1,1 +1,59 @@
 -- TODO
+
+CREATE TABLE AC_PCPL (
+  ID              BIGSERIAL    NOT NULL PRIMARY KEY,
+  NAME            VARCHAR(100) NOT NULL,
+  ENABLED         BOOLEAN      NOT NULL,
+  LOCKED          BOOLEAN      NOT NULL,
+  PRINCIPAL_TYPE  INT          NOT NULL,
+  M_ST            INT          NOT NULL,
+  C_ID            INT          NOT NULL,
+  C_TS            TIMESTAMP     ,
+  D_ID            INT           ,
+  M_ID            INT           ,
+  D_TS            TIMESTAMP
+  --  CONSTRAINT WHATEVER_FK FOREIGN KEY (WHATEVER_ID) REFERENCES WHATEVER_TABLE (WHATEVER_ID)
+);
+
+CREATE TABLE AC_PCPL_ROLE (
+  ID              BIGSERIAL    NOT NULL PRIMARY KEY,
+  PRINCIPAL_ID    BIGSERIAL    NOT NULL,
+  ROLE_TYPE       INT          NOT NULL,
+  M_TS            TIMESTAMP     ,
+  C_ID            INT           ,
+  D_ID            INT           ,
+  M_ID            INT           ,
+  C_TS            TIMESTAMP     ,
+  D_TS            TIMESTAMP
+);
+
+CREATE TABLE AC_USER (
+  ID              BIGSERIAL    NOT NULL PRIMARY KEY,
+  REAL_NAME       VARCHAR(100) NOT NULL,
+  EMAIL           VARCHAR(100) NOT NULL,
+  PASSWORD        VARCHAR(100) NOT NULL,
+  M_TS            INT          ,
+  C_ID            INT          ,
+  C_TS            TIMESTAMP
+);
+
+CREATE TABLE AC_GROP (
+  ID              BIGSERIAL    NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE AC_GROP_MMBR (
+  ID              BIGSERIAL    NOT NULL PRIMARY KEY,
+  PRINCIPAL_ID    BIGSERIAL    NOT NULL,
+  GROUP_ID        BIGSERIAL    NOT NULL,
+  M_TS            INT          ,
+  M_ID            INT          ,
+  C_ID            INT          ,
+  D_ID            INT          ,
+  C_TS            TIMESTAMP    ,
+  D_TS            TIMESTAMP    ,
+
+  CONSTRAINT FOREIGN_FK_1 FOREIGN KEY (PRINCIPAL_ID)  REFERENCES AC_PCPL (ID),
+  CONSTRAINT FOREIGN_FK_2 FOREIGN KEY (GROUP_ID)      REFERENCES AC_GROP (ID)
+);
+
+GRANT ALL PRIVILEGES ON DATABASE account to account;
--- data/src/test/resources/ddl/DROP.sql
+++ data/src/test/resources/ddl/DROP.sql
@@ -1,1 +1,19 @@
 -- TODO
+
+DROP TABLE IF EXISTS bat_job_instance CASCADE ;
+DROP TABLE IF EXISTS bat_job_execution CASCADE ;
+DROP TABLE IF EXISTS bat_job_execution_context CASCADE ;
+DROP TABLE IF EXISTS bat_job_execution_params CASCADE ;
+DROP TABLE IF EXISTS bat_step_execution CASCADE ;
+DROP TABLE IF EXISTS bat_step_execution_context CASCADE ;
+
+DROP TABLE IF EXISTS ac_user CASCADE ;
+DROP TABLE IF EXISTS ac_pcpl CASCADE ;
+DROP TABLE IF EXISTS ac_pcpl_role CASCADE ;
+DROP TABLE IF EXISTS ac_grop CASCADE ;
+DROP TABLE IF EXISTS ac_grop_mmbr CASCADE ;
+
+DROP TABLE IF EXISTS acl_object_identity CASCADE ;
+DROP TABLE IF EXISTS acl_class CASCADE ;
+DROP TABLE IF EXISTS acl_sid CASCADE ;
+DROP TABLE IF EXISTS acl_entry CASCADE ;
