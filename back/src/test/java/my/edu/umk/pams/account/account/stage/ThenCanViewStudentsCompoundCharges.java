ae06d113bf6f94600239e5a9db1d0d033c06eb3c
AIDA <AIDA@10.3.32.63>
2017-03-16 15:57:29 +0800

    Update User Story

---
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenCanViewStudentsCompoundCharges.java
@@ -0,0 +1,52 @@
+package my.edu.umk.pams.account.account.stage;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.account.model.AcAccount;
+import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+
+
+@JGivenStage
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class ThenCanViewStudentsCompoundCharges extends Stage<ThenCanViewStudentsCompoundCharges> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIListAllCompoundStudent.class);
+
+	@ExpectedScenarioState
+	private AcAcademicSession academicSession;
+
+	@ProvidedScenarioState
+	private AcAccount account;
+
+	@ProvidedScenarioState
+	private AcAccountCharge accountCharge;
+
+	@ProvidedScenarioState
+	private AcStudent student;
+
+	@Autowired
+	private AccountService accountService;
+
+	public ThenCanViewStudentsCompoundCharges can_view_students_compound_charges() {
+		List<AcAccountCharge> charges = accountService.findAccountCharges(academicSession, account);
+		Assert.isTrue(!charges.isEmpty());
+		return self();
+
+	}
+
+}
