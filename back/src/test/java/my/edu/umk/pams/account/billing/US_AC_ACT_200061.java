3b6b87d3dc44b550080f5aee10d715c17a920284
max.zain <max.zain@canang.com.my>
2017-03-20 16:18:29 +0800

    add iteration for a story for student

---
+++ back/src/test/java/my/edu/umk/pams/account/billing/US_AC_ACT_200061.java
@@ -0,0 +1,38 @@
+package my.edu.umk.pams.account.billing;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+import my.edu.umk.pams.account.billing.stage.ThenTheAcademicChargesAreListed;
+import my.edu.umk.pams.account.billing.stage.WhenIListAcademicChargesByAccount;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.bdd.stage.GivenIAmBursary;
+import my.edu.umk.pams.bdd.stage.GivenIAmStudent;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_AC_ACT_200061 extends	SpringScenarioTest<GivenIAmStudent, WhenIListAcademicChargesByAccount, ThenTheAcademicChargesAreListed>{
+
+    @Before
+    public void before() {
+    }
+
+    @After
+    public void after() {
+    }
+    
+    @Test
+    @Rollback(true)
+    public void scenarioPayment(){
+    	given().I_am_a_student_in_current_academic_session();
+    	when().I_list_charges_by_acount();
+    	then().the_charges_are_listed();
+    }
+    
+
+}
