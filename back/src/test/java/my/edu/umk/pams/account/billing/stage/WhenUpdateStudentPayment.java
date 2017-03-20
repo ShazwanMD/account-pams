4e3abcb71f8e5b47f89826658ad092bd4bb9168e
Sahir <sahir@umk.edu.my>
2017-03-20 10:37:44 +0800

    unit test US_AC_BLG_2008 & US_AC_BLG_2009

---
+++ back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenUpdateStudentPayment.java
@@ -0,0 +1,9 @@
+package my.edu.umk.pams.account.billing.stage;
+
+import com.tngtech.jgiven.Stage;
+
+public class WhenUpdateStudentPayment extends Stage<WhenUpdateStudentPayment> {
+	public WhenUpdateStudentPayment Update_student_payment(){
+		return self();
+	}
+}
