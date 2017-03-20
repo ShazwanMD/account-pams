6e2fd5aeff35c3f8b02c6211727145621ec36595
personal google <personal google@10.3.32.34>
2017-03-20 16:10:57 +0800

    US_AC_ACT_3001

---
+++ back/src/test/java/my/edu/umk/pams/account/account/US_AC_ACT_3001.java
@@ -0,0 +1,47 @@
+package my.edu.umk.pams.account.account;
+/*
+ * As a Students, 
+ * I want to view fees statement and charges  // When_I_want_to_view_charges
+ * so that I can make payment  //Then_I_can_make_payment
+ */
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.account.account.stage.ThenICanMakePayment;
+import my.edu.umk.pams.account.account.stage.WhenIWantToViewCharges;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.bdd.stage.GivenIAmStudent;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_AC_ACT_3001 extends
+		SpringScenarioTest<GivenIAmStudent, WhenIWantToViewCharges, ThenICanMakePayment> {
+	@Before
+	public void before() {
+	}
+
+	@After
+	public void after() {
+	}
+
+	@Test
+	@Rollback
+	public void testScenario0() {
+		// Given
+		given().I_am_a_student_in_current_academic_session() ;
+		// When
+		when().I_want_to_view_charges();
+		// Then
+		then().I_can_make_payment();
+	}
+}
---
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenICanMakePayment.java
@@ -0,0 +1,52 @@
+package my.edu.umk.pams.account.account.stage;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import io.jsonwebtoken.lang.Assert;
+import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.account.model.AcAccount;
+import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+
+
+@JGivenStage
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class ThenICanMakePayment extends Stage<ThenICanMakePayment> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ThenICanMakePayment.class);
+
+	@ExpectedScenarioState
+	private AcAcademicSession academicSession;
+
+	@ProvidedScenarioState
+	private AcAccount account;
+
+	@ProvidedScenarioState
+	private AcAccountCharge accountCharge;
+
+	@ProvidedScenarioState
+	private AcStudent student;
+
+	@Autowired
+	private AccountService accountService;
+
+	public ThenICanMakePayment I_can_make_payment() {
+		List<AcAccountCharge> charges = accountService.findAccountCharges(academicSession, account);
+		Assert.isTrue(!charges.isEmpty());
+		return self();
+
+	}
+
+}
