29f4acfbe7a6604cf8f6bd9e89d69c9eb63b6df6
max.zain <max.zain@canang.com.my>
2017-03-20 16:12:13 +0800

    add iteration for a story

--- back/src/test/java/my/edu/umk/pams/account/billing/US_AC_ACT_20006.java
+++ back/src/test/java/my/edu/umk/pams/account/billing/US_AC_ACT_20006.java
@@ -1,6 +1,7 @@
 package my.edu.umk.pams.account.billing;
 
 import my.edu.umk.pams.account.billing.stage.ThenTheAcademicChargesAreListed;
+import my.edu.umk.pams.account.billing.stage.WhenIListAcademicChargesByAccount;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,12 +16,10 @@ import my.edu.umk.pams.account.billing.stage.ThenAReceiptIsGenerated;
 import my.edu.umk.pams.account.billing.stage.WhenIPayCompound;
 import my.edu.umk.pams.account.config.TestAppConfiguration;
 import my.edu.umk.pams.bdd.stage.GivenIAmBursary;
-import my.edu.umk.pams.bdd.stage.GivenIAmStudent;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class US_AC_ACT_20006 extends	SpringScenarioTest<GivenIAmBursary, WhenIPayCompound, ThenTheAcademicChargesAreListed>{
-//As a Student, I want to print receipt compound so that it can be used as proof of payment.
+public class US_AC_ACT_20006 extends	SpringScenarioTest<GivenIAmBursary, WhenIListAcademicChargesByAccount, ThenTheAcademicChargesAreListed>{
 
     @Before
     public void before() {
@@ -34,7 +33,7 @@ public class US_AC_ACT_20006 extends	SpringScenarioTest<GivenIAmBursary, WhenIPa
     @Rollback(true)
     public void scenarioPayment(){
     	given().I_am_a_bursary_in_current_academic_session();
-    	when().I_pay_the_compound();
+    	when().I_list_charges_by_acount();
     	then().the_charges_are_listed();
     }
     
