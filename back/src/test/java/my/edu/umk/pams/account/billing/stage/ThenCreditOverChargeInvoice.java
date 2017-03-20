ebc619320fe2459da99fbf37d7fcea356a6e438d
Sahir <sahir@umk.edu.my>
2017-03-20 10:24:07 +0800

    unit test US_AC_BLG_2016

---
+++ back/src/test/java/my/edu/umk/pams/account/billing/stage/ThenCreditOverChargeInvoice.java
@@ -0,0 +1,9 @@
+package my.edu.umk.pams.account.billing.stage;
+
+import com.tngtech.jgiven.Stage;
+
+public class ThenCreditOverChargeInvoice extends Stage<ThenCreditOverChargeInvoice> {
+	public ThenCreditOverChargeInvoice Credit_over_charge_invoice(){
+		return self();
+	}
+}
---
+++ back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenCreateCreditNote.java
@@ -0,0 +1,9 @@
+package my.edu.umk.pams.account.billing.stage;
+
+import com.tngtech.jgiven.Stage;
+
+public class WhenCreateCreditNote extends Stage<WhenCreateCreditNote> {
+	public WhenCreateCreditNote Create_credit_note(){
+		return self();
+	}
+}
