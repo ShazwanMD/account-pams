15551d92c68d9969f65eeed74c6579ac80f6b408
hajar <hajar.ad@umk.edu.my>
2017-03-21 12:36:07 +0800

    US_AC_FNA_3000 - student can view sponsorship info

---
+++ back/src/test/java/my/edu/umk/pams/account/financialaid/US_AC_FNA_3000.java
@@ -0,0 +1,52 @@
+package my.edu.umk.pams.account.financialaid;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.account.financialaid.stage.ThenICanCheckMyFeesStatus;
+import my.edu.umk.pams.account.financialaid.stage.WhenIViewSponsorshipInformation;
+import my.edu.umk.pams.bdd.stage.GivenIAmStudent;
+
+/**
+ * As student
+ * 	I want to view information on my sponsorship, 
+ * 		so that I can check my fee status;
+ * 
+ * @author PAMS
+ *
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class US_AC_FNA_3000 extends SpringScenarioTest<GivenIAmStudent, WhenIViewSponsorshipInformation, ThenICanCheckMyFeesStatus>{
+
+
+	private static final String MATRIC_NO = "A17P002";
+	
+	@Before
+	public void before() {
+	}
+
+	@After
+	public void after() {
+	}
+
+	@Test
+	@Rollback(false)
+	public void testViewSponsorInformation() {
+
+		given().I_am_a_student_in_current_academic_session();
+		when().I_want_to_view_sponsorship_information_$(MATRIC_NO);
+		then().I_can_check_my_fees_status();
+	}
+}
---
+++ back/src/test/java/my/edu/umk/pams/account/financialaid/stage/WhenIViewSponsorshipInformation.java
@@ -0,0 +1,66 @@
+package my.edu.umk.pams.account.financialaid.stage;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.account.account.model.AcAccount;
+import my.edu.umk.pams.account.identity.model.AcCoverage;
+import my.edu.umk.pams.account.identity.model.AcSponsor;
+import my.edu.umk.pams.account.identity.model.AcSponsorship;
+
+import my.edu.umk.pams.account.identity.model.AcStudent;
+import my.edu.umk.pams.account.identity.service.IdentityService;
+
+@JGivenStage
+public class WhenIViewSponsorshipInformation extends Stage<WhenIViewSponsorshipInformation> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIViewSponsorshipInformation.class);
+
+	@Autowired
+	private IdentityService identityService;
+
+	@ExpectedScenarioState
+	private AcStudent student;
+
+	@ExpectedScenarioState
+	private AcAccount account;
+
+	@ExpectedScenarioState
+	private List<AcSponsorship> sponsorship;
+
+	@ExpectedScenarioState
+	private List<AcCoverage> coverage;
+
+	@As("I want to view my sponsorship information")
+	public WhenIViewSponsorshipInformation I_want_to_view_sponsorship_information_$(String matricNo) {
+
+		student = identityService.findStudentByMatricNo(matricNo);
+		LOG.debug("Student ID :" + student.getIdentityNo());
+
+		sponsorship = identityService.findSponsorships(student);
+
+		for (AcSponsorship sponsorships : sponsorship) {
+
+			LOG.debug("Sponsor ID :" + sponsorships.getSponsor().getIdentityNo());
+
+			coverage = identityService.findCoverages(sponsorships.getSponsor());
+
+			for (AcCoverage coverage : coverage) {
+
+				LOG.debug("coverage charge code: (" + coverage.getChargeCode().getCode() + ") "
+						+ coverage.getChargeCode().getDescription());
+
+			}
+
+		}
+		return self();
+	}
+}
