095291569757f61e15d54fafc29f4956d7403536
personal google <personal google@10.3.32.34>
2017-03-21 16:13:06 +0800

    US_AC_ACT_3005

---
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/WhenIWantToPrintMyPaymentSlip.java
@@ -0,0 +1,52 @@
+package my.edu.umk.pams.account.account.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.Pending;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.account.model.AcAccount;
+import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+import my.edu.umk.pams.account.identity.service.IdentityService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@JGivenStage
+public class WhenIWantToPrintMyPaymentSlip extends Stage<WhenIWantToPrintMyPaymentSlip> {
+    private static final Logger LOG = LoggerFactory.getLogger(WhenIWantToPrintMyPaymentSlip.class);
+
+    @ExpectedScenarioState
+    private AcStudent student;
+
+    @ExpectedScenarioState
+    private AcAcademicSession academicSession;
+
+    @ProvidedScenarioState
+    private AcAccount account;
+
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private IdentityService identityservice;
+
+
+    @Pending
+    public WhenIWantToPrintMyPaymentSlip I_want_to_print_my_payment_slip() {
+
+        //todo: get data of the payment and put into a slip layout
+
+
+        return self();
+
+
+    }
+
+}
