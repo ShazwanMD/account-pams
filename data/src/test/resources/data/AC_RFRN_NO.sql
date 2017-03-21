7fa3cbfdd452b01d447ab953795400300a0c7e22
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-03-21 14:51:40 +0800

    Fix seed data

--- data/src/test/resources/data/AC_RFRN_NO.sql
+++ data/src/test/resources/data/AC_RFRN_NO.sql
@@ -2,7 +2,7 @@ INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, P
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcAccountCharge.referenceNo', 1, 'Account Charge Reference No Format', 1, 'CHRG', '{#academicSession.getCode()}/{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
-VALUES (nextval('SQ_AC_RFRN_NO'), 'AcSettlement.referenceNo', 1, 'Settlement Reference No Format', 1, 'STLT', '{#academicSession.getCode()}/{#sponsor.getCode()}/{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
+VALUES (nextval('SQ_AC_RFRN_NO'), 'AcSettlement.referenceNo', 1, 'Settlement Reference No Format', 1, 'STLT', '{#academicSession.getCode()}/{#sponsor.getIdentityNo()}/{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcInvoice.referenceNo', 1, 'Invoice Reference No Format', 1, 'INVC', '{#academicSession.getCode()}/{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
