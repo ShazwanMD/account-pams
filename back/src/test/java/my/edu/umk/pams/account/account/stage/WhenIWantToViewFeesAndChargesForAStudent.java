f21a1df5fd16c2622893c4f5382ee4e69d644319
Sahir <sahir@umk.edu.my>
2017-03-21 10:10:33 +0800

    unit test US_AC_ACT_4001

---
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/WhenIWantToViewFeesAndChargesForAStudent.java
@@ -0,0 +1,54 @@
+package my.edu.umk.pams.account.account.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.account.model.AcAccount;
+import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+import my.edu.umk.pams.account.identity.service.IdentityService;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@JGivenStage
+public class WhenIWantToViewFeesAndChargesForAStudent extends Stage<WhenIWantToViewFeesAndChargesForAStudent> {
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIWantToViewFeesAndChargesForAStudent.class);
+
+	@ExpectedScenarioState
+	private AcStudent student;
+	
+	@ExpectedScenarioState
+	private AcAcademicSession academicSession;
+
+	@ProvidedScenarioState
+	private AcAccount account;
+
+	
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private IdentityService identityservice;
+	
+	//public WhenIWantViewCompoundBill I_want_view_my_compound_bill(String matricNo)
+
+	public WhenIWantToViewFeesAndChargesForAStudent I_want_to_view_fees_and_charges_for_a_student(){
+		
+		List<AcAccountCharge> charges = accountService.findAccountCharges(academicSession, account);
+		
+
+		
+		return self();
+		
+		
+	}
+
+}
