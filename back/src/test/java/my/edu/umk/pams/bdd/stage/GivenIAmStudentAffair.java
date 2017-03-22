ac95cee35caa4315b5285a9f87b5b1256c35e577
AZIETA <azieta.ma@umk.edu.my>
2017-03-22 16:55:26 +0800

    Add GivenIAmStudentAffair

---
+++ back/src/test/java/my/edu/umk/pams/bdd/stage/GivenIAmStudentAffair.java
@@ -0,0 +1,48 @@
+package my.edu.umk.pams.bdd.stage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.identity.model.AcStaff;
+import my.edu.umk.pams.account.identity.model.AcUser;
+import my.edu.umk.pams.account.security.integration.AcUserDetails;
+
+@JGivenStage
+public class GivenIAmStudentAffair {
+	@Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private AccountService accountService;
+
+    @ProvidedScenarioState
+    private AcAcademicSession academicSession;
+
+    @ProvidedScenarioState
+    private AcStaff staff;
+
+    public void I_am_student_affair(){
+    	loginAsStudentAffair();
+        academicSession = accountService.findCurrentAcademicSession();
+    }
+
+    private void loginAsStudentAffair() {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken("studentaffair", "abc123");
+        Authentication authed = authenticationManager.authenticate(token);
+        SecurityContextHolder.getContext().setAuthentication(authed);
+
+        // retrieve staff from user
+        AcUser user = ((AcUserDetails) authed.getPrincipal()).getUser();
+        staff = (AcStaff) user.getActor();
+
+    }
+
+}
