e6780a0506920d7ca8e6f8cfead71ad0f0dd0e11
personal google <personal google@10.3.2.140>
2017-03-28 11:38:16 +0800

    US_AC_ACT_3001 - works if charge data exist for student 1 (account id = 1)

--- back/src/test/java/my/edu/umk/pams/account/account/stage/WhenIWantToViewCharges.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/WhenIWantToViewCharges.java
@@ -2,7 +2,6 @@ package my.edu.umk.pams.account.account.stage;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
-import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.account.account.model.AcAcademicSession;
@@ -10,11 +9,9 @@ import my.edu.umk.pams.account.account.model.AcAccount;
 import my.edu.umk.pams.account.account.model.AcAccountCharge;
 import my.edu.umk.pams.account.account.service.AccountService;
 import my.edu.umk.pams.account.identity.model.AcStudent;
-import my.edu.umk.pams.account.identity.service.IdentityService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import java.util.List;
 
 @JGivenStage
@@ -30,26 +27,21 @@ public class WhenIWantToViewCharges extends Stage<WhenIWantToViewCharges> {
     @ProvidedScenarioState
     private AcAccount account;
 
-
     @Autowired
     private AccountService accountService;
-
-    @Autowired
-    private IdentityService identityservice;
+    
+    @ProvidedScenarioState
+    List<AcAccountCharge> charges;
 
     //public WhenIWantViewCompoundBill I_want_view_my_compound_bill(String matricNo)
 
-    @Pending
     public WhenIWantToViewCharges I_want_to_view_charges() {
-
-        List<AcAccountCharge> charges = accountService.findAccountCharges(academicSession, account);
+        charges = accountService.findAccountCharges(academicSession, account);
         for (AcAccountCharge charge : charges) {
             LOG.debug(charge.getDescription() + " " + charge.getReferenceNo());
         }
 
         return self();
-
-
     }
 
 }
