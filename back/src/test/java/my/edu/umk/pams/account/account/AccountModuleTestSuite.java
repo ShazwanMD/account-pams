dd25a720ec240e085f32dc184543530a856c8fed
AZIETA <azieta.ma@umk.edu.my>
2017-03-29 10:40:07 +0800

    UPDATE ACCOUNT MODULE TEST SUITE

--- back/src/test/java/my/edu/umk/pams/account/account/AccountModuleTestSuite.java
+++ back/src/test/java/my/edu/umk/pams/account/account/AccountModuleTestSuite.java
@@ -8,14 +8,29 @@ import org.junit.runners.Suite;
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        US_AC_ACT_0000.class,
-        US_AC_ACT_0001.class,
-        US_AC_ACT_0002.class,
-        US_AC_ACT_0004.class,
-        US_AC_ACT_0008.class,
-        US_AC_ACT_0010.class,
-        US_AC_ACT_0012.class,
-        US_AC_ACT_5000.class,
+        US_AC_ACT_0000.class, //canang
+        US_AC_ACT_0001.class, //canang
+        US_AC_ACT_0002.class, //canang
+        US_AC_ACT_0004.class, //canang
+        US_AC_ACT_2001.class, //ta
+        US_AC_ACT_2003.class, //sahir
+        US_AC_ACT_2004.class, //ta
+        US_AC_ACT_2005.class, //ta
+        US_AC_ACT_2006.class, //ta
+        US_AC_ACT_3001.class, //zul
+        US_AC_ACT_3002.class, //zul
+        US_AC_ACT_3003.class, //zul
+        US_AC_ACT_3004.class, //zul
+        US_AC_ACT_3005.class, //zul
+        US_AC_ACT_4001.class, //sahir
+        US_AC_ACT_4002.class, //sahir
+        US_AC_ACT_5001.class, //aida
+        US_AC_ACT_5002.class, //ta
+        US_AC_ACT_6001.class, //ta
+        US_AC_ACT_6002.class, //aida
+        US_AC_ACT_7001.class, //ta
+        US_AC_ACT_7002.class, //aida
+        US_AC_ACT_7004.class, //aida     
 })
 public class AccountModuleTestSuite {
 }
