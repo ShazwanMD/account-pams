f6f398b2d67ad6ec88d2bc1aef26794444412010
AZIETA <azieta.ma@umk.edu.my>
2017-04-03 17:42:40 +0800

    Update WhenGenerateReportByFaculty

--- back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenGenerateReportByFaculty.java
+++ back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenGenerateReportByFaculty.java
@@ -1,13 +1,59 @@
 package my.edu.umk.pams.account.billing.stage;
 
-import com.tngtech.jgiven.Stage;
-import com.tngtech.jgiven.integration.spring.JGivenStage;
-@JGivenStage
-public class WhenGenerateReportByFaculty extends Stage<WhenGenerateReportByFaculty>{
+import java.util.List;
 
-	public WhenGenerateReportByFaculty I_want_to_generate_report_by_faculty() {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.account.model.AcAccount;
+import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.account.stage.WhenIListAllCompoundStudent;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+import my.edu.umk.pams.account.identity.service.IdentityService;
+
+@JGivenStage
+public class WhenGenerateReportByFaculty extends Stage<WhenGenerateReportByFaculty> {
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIListAllCompoundStudent.class);
+
+	@ExpectedScenarioState
+	private AcStudent student;
+
+	@ProvidedScenarioState
+	private AcAccount accounts;
+	//private List<AcAccount> accounts;
+
+	@ExpectedScenarioState
+	private AcAcademicSession academicSession;
+
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private IdentityService identityService;
+
+	@ProvidedScenarioState
+	private List<AcAccountCharge> accountCharges;
+
+	public WhenGenerateReportByFaculty I_want_to_generate_report_by_faculty_$(String matricNo) {
+		student = identityService.findStudentByMatricNo(matricNo);
+
+		accounts = accountService.findAccountByActor(student);
+		accountCharges = accountService.findAccountCharges(accounts);
+		for (AcAccountCharge accountCharges : accountCharges) {
+			LOG.debug("Description " + accountCharges.getDescription());
+			LOG.debug("Session " + accountCharges.getSession().getCode());
+		}
+
 		return self();
-		
+
 	}
 
 }
