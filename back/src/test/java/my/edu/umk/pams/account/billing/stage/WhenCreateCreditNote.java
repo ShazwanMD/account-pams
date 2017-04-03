2f59219adf18351b2681be114aef2bca57d0692a
Sahir <sahir@umk.edu.my>
2017-04-03 11:21:31 +0800

    add comment notes

--- back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenCreateCreditNote.java
+++ back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenCreateCreditNote.java
@@ -1,8 +1,22 @@
 package my.edu.umk.pams.account.billing.stage;
 
-import com.tngtech.jgiven.Stage;
+import my.edu.umk.pams.account.billing.service.BillingService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.Pending;
+
+@Pending
 public class WhenCreateCreditNote extends Stage<WhenCreateCreditNote> {
+	
+	@Autowired
+	BillingService billingService;
+	
+	/*
+	 * credit note services not available yet
+	 * */
+	
 	public WhenCreateCreditNote Create_credit_note(){
 		return self();
 	}
