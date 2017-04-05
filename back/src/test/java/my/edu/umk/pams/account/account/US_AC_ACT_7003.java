94bd5b07eebc17d9929078ce789d7c9251fe4e73
AZIETA <azieta.ma@umk.edu.my>
2017-04-05 11:26:47 +0800

    Update US_AC_ACT_7003

--- back/src/test/java/my/edu/umk/pams/account/account/US_AC_ACT_7003.java
+++ back/src/test/java/my/edu/umk/pams/account/account/US_AC_ACT_7003.java
@@ -1,34 +1,41 @@
 package my.edu.umk.pams.account.account;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 
-import my.edu.umk.pams.account.account.stage.ThenDiscountCanAppliedToReceipt;
-import my.edu.umk.pams.account.account.stage.WhenIWantGiveCompoundsDiscountToStudent;
-import my.edu.umk.pams.bdd.stage.GivenIAmStudentAffair;
+import my.edu.umk.pams.account.account.stage.ThenAcademicDiscountCanAppliedToReceipt;
+import my.edu.umk.pams.account.account.stage.WhenIWantGiveCompoundsAcademicDiscountToStudent;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.bdd.stage.GivenIAmMGSEBAdministrator;
 
 /*
  * As Academic (MGSEB/PPS), 
  * I want to give compound's discount to student 
  * so that discount can applied to receipt
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
 public class US_AC_ACT_7003 extends
-SpringScenarioTest<GivenIAmStudentAffair, WhenIWantGiveCompoundsDiscountToStudent, ThenDiscountCanAppliedToReceipt> {
+		SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenIWantGiveCompoundsAcademicDiscountToStudent, ThenAcademicDiscountCanAppliedToReceipt> {
 
-private static final Logger LOG = LoggerFactory.getLogger(US_AC_ACT_7003.class);
+	private static final Logger LOG = LoggerFactory.getLogger(US_AC_ACT_7003.class);
 
+	private static final String MATRIC_NO = "A17P001";
 
-private static final String MATRIC_NO = "A17P001";
-
-@Test
-@Rollback
-public void testScenario0() {
-given().I_am_student_affair();
-when().I_want_give_compounds_discount_to_student_$(MATRIC_NO);
-then().discount_can_applied_to_receipt();
-}
+	@Test
+	@Rollback(false)
+	public void testScenario0() {
+		given().I_am_a_MGSEB_administrator_in_current_academic_session();
+		when().I_want_give_compounds_discount_to_student_$(MATRIC_NO);
+		then().discount_can_applied_to_receipt();
+	}
 }
