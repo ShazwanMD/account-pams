b0f9cc9cb0b1d3b5c2857e12b9405d8a29a4d687
AZIETA <azieta.ma@umk.edu.my>
2017-04-09 11:03:10 +0800

    1. Add I_can_view_student_academic_charges() 2. Add I_can_view_student_security_charges()

--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenViewStudentTypeCharges.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenViewStudentTypeCharges.java
@@ -6,15 +6,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.Assert;
 
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
 import my.edu.umk.pams.account.account.model.AcAcademicSession;
 import my.edu.umk.pams.account.account.model.AcAccount;
 import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.model.AcAccountChargeType;
 import my.edu.umk.pams.account.account.service.AccountService;
 import my.edu.umk.pams.account.config.TestAppConfiguration;
 import my.edu.umk.pams.account.identity.model.AcStudent;
@@ -22,10 +25,10 @@ import my.edu.umk.pams.account.identity.service.IdentityService;
 
 @JGivenStage
 @ContextConfiguration(classes = TestAppConfiguration.class)
-public class ThenViewStudentAcademicCharges extends Stage<ThenViewStudentAcademicCharges>{
+public class ThenViewStudentTypeCharges extends Stage<ThenViewStudentTypeCharges> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ThenViewStudentAcademicCharges.class);
-	
+	private static final Logger LOG = LoggerFactory.getLogger(ThenViewStudentTypeCharges.class);
+
 	@ExpectedScenarioState
 	private AcAcademicSession academicSession;
 
@@ -41,23 +44,51 @@ public class ThenViewStudentAcademicCharges extends Stage<ThenViewStudentAcademi
 	@Autowired
 	private AccountService accountService;
 
-	@Autowired
-	private IdentityService identityService;
-	
-	public ThenViewStudentAcademicCharges I_can_view_student_academic_charges_$(String matricNo) {
-		student = identityService.findStudentByMatricNo(matricNo);
-		LOG.debug("Student ID :" + student.getIdentityNo());
+	@ExpectedScenarioState
+	private AcAccountChargeType chargeType;
 
-		// sponsorship = identityService.findSponsorships(student);
-		accountCharge = accountService.findAccountChargeByReferenceNo("REFNO/1490156078146");
+	@ExpectedScenarioState
+	List<AcAccountCharge> accountCharges;
 
-		List<AcAccountCharge> accountCharge = accountService.findAccountCharges(account);
-
+	public ThenViewStudentTypeCharges I_can_view_student_academic_charges(){
+		
+		AcAccountChargeType chargeType = AcAccountChargeType.ACADEMIC;
+		List<AcAccountCharge> accountCharge = accountService.findAccountCharges(chargeType);
+		
 		for (AcAccountCharge charge : accountCharge) {
+			
+			Assert.notNull(charge, "Charges is empty");
 
-			LOG.debug("Charge Type :" + charge.getChargeType("ACADEMIC"));
+			LOG.debug("===========Academic Charges============");
+			LOG.debug("Student Name :" + charge.getAccount().getActor().getName());
+			LOG.debug("Charge Type :" + charge.getChargeType());
+			LOG.debug("Reference No : " + charge.getReferenceNo());
+			LOG.debug("Description : " + charge.getDescription());
+			LOG.debug("                                       ");
+
+
 		}
 		return self();
 	}
 
+	
+	public ThenViewStudentTypeCharges I_can_view_student_security_charges(){
+		
+		AcAccountChargeType chargeType = AcAccountChargeType.SECURITY;
+		List<AcAccountCharge> accountCharge = accountService.findAccountCharges(chargeType);
+		
+		for (AcAccountCharge charge : accountCharge) {
+			
+			Assert.notNull(charge, "Charges is empty");
+
+			LOG.debug("============Security Charges============");
+			LOG.debug("Student Name :" + charge.getAccount().getActor().getName());
+			LOG.debug("Charge Type :" + charge.getChargeType());
+			LOG.debug("Reference No : " + charge.getReferenceNo());
+			LOG.debug("Description : " + charge.getDescription());
+		
+
+		}
+		return self();
+	}
 }
