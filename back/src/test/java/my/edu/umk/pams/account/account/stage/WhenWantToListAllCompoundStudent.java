c896062353187ffb991967b98a069009ed7a87b7
AIDA <AIDA@10.3.32.100>
2017-04-09 11:58:24 +0800

    Update WhenWantToListAllCompoundStudent US_AC_ACT_6002

--- back/src/test/java/my/edu/umk/pams/account/account/stage/WhenWantToListAllCompoundStudent.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/WhenWantToListAllCompoundStudent.java
@@ -1,14 +1,19 @@
 package my.edu.umk.pams.account.account.stage;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.annotation.ProvidedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 import my.edu.umk.pams.account.account.model.*;
 import my.edu.umk.pams.account.account.service.AccountService;
 import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -21,12 +26,30 @@ public class WhenWantToListAllCompoundStudent extends Stage<WhenWantToListAllCom
 	@Autowired
 	private AccountService accountService;
 
-	public WhenWantToListAllCompoundStudent I_want_to_list_all_compound_student() {
+	@ProvidedScenarioState
+	private AcStudent student;
 
-		AcAccount account = accountService.findAccountByCode("A17P001");
-		List<AcAccountCharge> charges = accountService.findAccountCharges(account);
-		for (AcAccountCharge accountcharge : charges) {
-			LOG.debug(accountcharge.getDescription());
+	@ProvidedScenarioState
+	private AcAccount account;
+
+	@ProvidedScenarioState
+	List<AcAccountCharge> accountCharges;
+
+	@ExpectedScenarioState
+	private AcAcademicSession academicSession;
+
+	public WhenWantToListAllCompoundStudent I_want_to_list_all_compound_student_$(String matricNo) {
+
+		AcAccountChargeType chargeType = AcAccountChargeType.SECURITY;
+
+		accountCharges = accountService.findAccountCharges(academicSession, chargeType);
+
+		Assert.notEmpty(accountCharges, "Account Charges is empty");
+
+		for (AcAccountCharge charges : accountCharges) {
+
+			Assert.notNull(charges, "Charges is empty");
+
 		}
 
 		return self();
