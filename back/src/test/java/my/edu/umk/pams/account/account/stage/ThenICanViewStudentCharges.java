0a1cf2cfac9797819c795c2ea5fb35b3c972f790
AZIETA <azieta.ma@umk.edu.my>
2017-04-10 16:52:12 +0800

    Update ThenICanViewStudentCharges : add academic, security, student affair

--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenICanViewStudentCharges.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenICanViewStudentCharges.java
@@ -1,21 +1,31 @@
 package my.edu.umk.pams.account.account.stage;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.util.Assert;
 
+import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.ProvidedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
 
 import my.edu.umk.pams.account.account.model.AcAcademicSession;
 import my.edu.umk.pams.account.account.model.AcAccount;
 import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.model.AcAccountChargeType;
 import my.edu.umk.pams.account.account.model.AcChargeCode;
+import my.edu.umk.pams.account.account.service.AccountService;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
 import my.edu.umk.pams.account.identity.model.AcStudent;
 import my.edu.umk.pams.account.identity.service.IdentityService;
 
-public class ThenICanViewStudentCharges {
+@JGivenStage
+@ContextConfiguration(classes = TestAppConfiguration.class)
+public class ThenICanViewStudentCharges extends Stage<ThenICanViewStudentCharges> {
 	private static final Logger LOG = LoggerFactory.getLogger(ThenCanViewStudentCharges.class);
 
 	@ExpectedScenarioState
@@ -31,15 +41,70 @@ public class ThenICanViewStudentCharges {
 	private AcStudent student;
 
 	@Autowired
-	private IdentityService identityService;
+	private AccountService accountService;
 
+	@Autowired
+	private IdentityService identityService;
 
 	@ExpectedScenarioState
 	private AcChargeCode chargeCode;
-	
-	public void I_can_view_student_compound_charges_$(String code) {
-		
-		Assert.isNull(chargeCode, "There is no compound charges");
+
+	public ThenICanViewStudentCharges I_can_view_student_academic_compound_charges_$(String code) {
+		LOG.debug("========List Student Charges of Type compound by account=========");
+		AcAccountChargeType chargeType = AcAccountChargeType.ACADEMIC;
+		List<AcAccountCharge> accountCharge = accountService.findAccountCharges(chargeType);
+
+		LOG.debug("===========Academic Charges============");
+		for (AcAccountCharge charge : accountCharge) {
+
+			Assert.notNull(charge, "Charges is empty");
+
+			LOG.debug("Student Name :" + charge.getAccount().getActor().getName());
+			LOG.debug("Charge Type :" + charge.getChargeType());
+			LOG.debug("Reference No : " + charge.getReferenceNo());
+			LOG.debug("Description : " + charge.getDescription());
+			LOG.debug("                                       ");
+
+		}
+		return self();
 	}
 
+	public ThenICanViewStudentCharges I_can_view_student_security_compound_charges_$(String code) {
+
+		AcAccountChargeType chargeType = AcAccountChargeType.SECURITY;
+		List<AcAccountCharge> accountCharge = accountService.findAccountCharges(chargeType);
+
+		LOG.debug("============Security Charges============");
+		for (AcAccountCharge charge : accountCharge) {
+
+			Assert.notNull(charge, "Charges is empty");
+
+			LOG.debug("Student Name :" + charge.getAccount().getActor().getName());
+			LOG.debug("Charge Type :" + charge.getChargeType());
+			LOG.debug("Reference No : " + charge.getReferenceNo());
+			LOG.debug("Description : " + charge.getDescription());
+			LOG.debug("                                       ");
+
+		}
+		return self();
+	}
+
+	public ThenICanViewStudentCharges I_can_view_student_student_affair_compound_charges_$(String code) {
+
+		AcAccountChargeType chargeType = AcAccountChargeType.STUDENT_AFFAIRS;
+		List<AcAccountCharge> accountCharge = accountService.findAccountCharges(chargeType);
+		LOG.debug("========Student Affair Charges=========");
+		for (AcAccountCharge charge : accountCharge) {
+
+			Assert.notNull(charge, "Charges is empty");
+
+			LOG.debug("Student Name :" + charge.getAccount().getActor().getName());
+			LOG.debug("Charge Type :" + charge.getChargeType());
+			LOG.debug("Reference No : " + charge.getReferenceNo());
+			LOG.debug("Description : " + charge.getDescription());
+			LOG.debug("                                       ");
+
+		}
+		return self();
+	}
 }
