b867183140d45db111ed50e2459001711d954927
faizal.m <UMK-PEJA@10.3.32.111>
2017-04-11 16:46:04 +0800

    add

--- back/src/test/java/my/edu/umk/pams/account/billing/US_AC_BLG_2018.java
+++ back/src/test/java/my/edu/umk/pams/account/billing/US_AC_BLG_2018.java
@@ -1,5 +1,8 @@
 package my.edu.umk.pams.account.billing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import my.edu.umk.pams.account.billing.stage.THEN;
 import my.edu.umk.pams.account.billing.stage.WHEN;
 import my.edu.umk.pams.bdd.stage.GivenIAmBursary;
@@ -7,5 +10,5 @@ import my.edu.umk.pams.bdd.stage.GivenIAmBursary;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
 
 public class US_AC_BLG_2018 extends SpringScenarioTest<GivenIAmBursary, WHEN, THEN> {
-
+	  private static final Logger LOG = LoggerFactory.getLogger(US_AC_BLG_2018.class);
 }
