2c3286bcc2f9cf10d3e693f7fc6e6fd99280a028
hajar <hajar.ad@umk.edu.my>
2017-04-11 09:22:11 +0800

    Generate report

---
+++ back/src/test/java/my/edu/umk/pams/account/billing/US_AC_BLG_2009.java
@@ -0,0 +1,33 @@
+package my.edu.umk.pams.account.billing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.account.billing.stage.ThenICanPrintReport;
+import my.edu.umk.pams.account.billing.stage.WhenIGenerateReport;
+import my.edu.umk.pams.account.config.TestAppConfiguration;
+import my.edu.umk.pams.bdd.stage.GivenIAmBursary;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ContextConfiguration(classes = TestAppConfiguration.class)
+@As("As a Bursary, I want to generate report so that I can print report")
+public class US_AC_BLG_2009 extends SpringScenarioTest<GivenIAmBursary, WhenIGenerateReport, ThenICanPrintReport> {
+
+	private static final String PROGRAM_CODE = "FIAT/PHD/0001";
+	
+	@Test
+	@Rollback(false)
+	public void scenario1() {
+		given().I_am_a_bursary_in_current_academic_session();
+		when().I_generate_by_program$(PROGRAM_CODE);
+		then().I_can_print_report();
+	}
+}
---
+++ back/src/test/java/my/edu/umk/pams/account/billing/stage/WhenIGenerateReport.java
@@ -0,0 +1,56 @@
+package my.edu.umk.pams.account.billing.stage;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.annotation.ExpectedScenarioState;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+import my.edu.umk.pams.account.common.model.AcProgramCode;
+import my.edu.umk.pams.account.common.service.CommonService;
+import my.edu.umk.pams.account.identity.model.AcSponsorship;
+import my.edu.umk.pams.account.identity.service.IdentityService;
+
+@JGivenStage
+public class WhenIGenerateReport extends Stage<WhenIGenerateReport> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(WhenIGenerateReport.class);
+
+	@Autowired
+	private CommonService commonService;
+
+	@Autowired
+	private IdentityService identityService;
+	
+	@ExpectedScenarioState
+	private List<AcSponsorship> sponsorship;
+
+	@ExpectedScenarioState
+	private AcProgramCode programCode;
+
+	@As("I generate report by program")
+	public WhenIGenerateReport I_generate_by_program$(String code) {
+
+		programCode = commonService.findProgramCodeByCode(code);
+		LOG.debug("Program Code " + programCode.getDescription());
+
+		sponsorship = identityService.findSponsorships(programCode);
+
+		for (AcSponsorship sponsorship : sponsorship) {
+
+			LOG.debug("Name: " + sponsorship.getStudent().getName());
+			LOG.debug("Matric No: " + sponsorship.getStudent().getMatricNo());
+			LOG.debug("Status: " + sponsorship.getStudent().getStudentStatus());
+			LOG.debug("Sponsorship " + sponsorship.getSponsor().getName());
+			LOG.debug("IC No: " + sponsorship.getStudent().getIdentityNo());
+
+			
+		}
+		return self();
+	}
+}
