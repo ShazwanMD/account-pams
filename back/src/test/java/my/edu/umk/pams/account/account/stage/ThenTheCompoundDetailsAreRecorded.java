ba927b2a81ff1030d7e74a213ba14fa76e522262
hajar <hajar.ad@umk.edu.my>
2017-04-13 12:06:35 +0800

    Remove unused import

--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenTheCompoundDetailsAreRecorded.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenTheCompoundDetailsAreRecorded.java
@@ -1,7 +1,5 @@
 package my.edu.umk.pams.account.account.stage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.util.Assert;
 
 import com.tngtech.jgiven.Stage;
@@ -9,8 +7,6 @@ import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import my.edu.umk.pams.account.account.model.AcAccountCharge;
 
 public class ThenTheCompoundDetailsAreRecorded extends Stage<ThenTheCompoundDetailsAreRecorded> {
-
-	private static final Logger LOG = LoggerFactory.getLogger(ThenTheCompoundDetailsAreRecorded.class);
 
 	@ExpectedScenarioState
 	private AcAccountCharge charge; 
