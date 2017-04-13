3557d19bc0bcf312f17ee85d7a38c990e16f9a28
hajar <hajar.ad@umk.edu.my>
2017-04-13 12:11:38 +0800

    Add assert

--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenICanKeepMyPaymentDetails.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenICanKeepMyPaymentDetails.java
@@ -1,20 +1,24 @@
 package my.edu.umk.pams.account.account.stage;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.tngtech.jgiven.Stage;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.annotation.Pending;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
+import io.jsonwebtoken.lang.Assert;
 import my.edu.umk.pams.account.account.model.AcAcademicSession;
 import my.edu.umk.pams.account.account.model.AcAccount;
 import my.edu.umk.pams.account.account.model.AcAccountCharge;
+import my.edu.umk.pams.account.account.service.AccountService;
 
 @JGivenStage
 public class ThenICanKeepMyPaymentDetails extends Stage<ThenICanKeepMyPaymentDetails> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ThenICanKeepMyPaymentDetails.class);
+	@Autowired
+	private AccountService accountService;
 
 	@ExpectedScenarioState
 	AcAcademicSession academicSession;
@@ -23,12 +27,14 @@ public class ThenICanKeepMyPaymentDetails extends Stage<ThenICanKeepMyPaymentDet
 	AcAccount account;
 
 	@ExpectedScenarioState
-	AcAccountCharge accountCharge;
+	List<AcAccountCharge> charges;
 
 	@Pending
-	public void I_can_keep_my_payment_details() {
+	public ThenICanKeepMyPaymentDetails I_can_keep_my_payment_details() {
 		
-		//todo: assert payment details
+		List<AcAccountCharge> charges = accountService.findAccountCharges(academicSession, account);
+		Assert.isTrue(!charges.isEmpty());
+		return self();
 
 	}
 
--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenMyChargeIsReduced.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenMyChargeIsReduced.java
@@ -14,35 +14,38 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.util.Assert;
 
 import java.math.BigDecimal;
-
 
 @JGivenStage
 @ContextConfiguration(classes = TestAppConfiguration.class)
 public class ThenMyChargeIsReduced extends Stage<ThenMyChargeIsReduced> {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ThenMyChargeIsReduced.class);
+	private static final Logger LOG = LoggerFactory.getLogger(ThenMyChargeIsReduced.class);
 
-    @Autowired
-    private AccountService accountService;
+	@Autowired
+	private AccountService accountService;
 
-    @ExpectedScenarioState
-    private AcAcademicSession academicSession;
+	@ExpectedScenarioState
+	private AcAcademicSession academicSession;
 
-    @ExpectedScenarioState
-    private AcAccount account;
+	@ExpectedScenarioState
+	private AcAccount account;
 
-    @ExpectedScenarioState
-    private AcWaiverApplication waiverApplication;
+	@ExpectedScenarioState
+	private AcWaiverApplication waiverApplication;
 
-    @ExpectedScenarioState
-    private BigDecimal balanceBefore;
+	@ExpectedScenarioState
+	private BigDecimal balanceBefore;
 
-    public ThenMyChargeIsReduced my_charge_is_reduced(){
-        BigDecimal balanceAfter = accountService.sumBalanceAmount(account);
-        LOG.debug("balanceBefore: " + balanceBefore);
-        LOG.debug("balanceAfter: " + balanceAfter);
-        return self();
-    }
+	public ThenMyChargeIsReduced my_charge_is_reduced() {
+
+		Assert.notNull(academicSession, "academic session is a prerequisite");
+
+		BigDecimal balanceAfter = accountService.sumBalanceAmount(account);
+		LOG.debug("balanceBefore: " + balanceBefore);
+		LOG.debug("balanceAfter: " + balanceAfter);
+		return self();
+	}
 }
