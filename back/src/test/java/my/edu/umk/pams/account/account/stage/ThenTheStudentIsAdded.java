231704ff80b0633b4f2d0e8892236b094b91d920
hajar <hajar.ad@umk.edu.my>
2017-04-13 12:40:01 +0800

    Add @as

--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenTheStudentIsAdded.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenTheStudentIsAdded.java
@@ -1,25 +1,18 @@
 package my.edu.umk.pams.account.account.stage;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.annotation.ExpectedScenarioState;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
-import my.edu.umk.pams.account.config.TestAppConfiguration;
 import my.edu.umk.pams.account.identity.model.AcStudent;
 import my.edu.umk.pams.account.identity.service.IdentityService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.util.Assert;
 
 /**
  * Created by PAMS on 3/8/2017.
  */
 @JGivenStage
-@ContextConfiguration(classes = TestAppConfiguration.class)
 public class ThenTheStudentIsAdded extends Stage<ThenTheStudentIsAdded> {
-
-
-    private static final Logger LOG = LoggerFactory.getLogger(WhenIAddAStudent.class);
 
     @ExpectedScenarioState
     private IdentityService identityService;
@@ -27,6 +20,7 @@ public class ThenTheStudentIsAdded extends Stage<ThenTheStudentIsAdded> {
     @ExpectedScenarioState
     private String matricNo;
 
+    @As("the student user is added")
     public ThenTheStudentIsAdded the_student_user_is_added(){
         AcStudent student = identityService.findStudentByMatricNo(matricNo);
         Assert.notNull(student, "Student is found");
