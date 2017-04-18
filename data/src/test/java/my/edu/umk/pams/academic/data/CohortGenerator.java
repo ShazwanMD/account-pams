12544467b3d224aaeb0bb44d6ee6df6849b4f296
hajar <hajar.ad@umk.edu.my>
2017-04-18 10:03:38 +0800

    Update cohort sql

--- data/src/test/java/my/edu/umk/pams/academic/data/CohortGenerator.java
+++ data/src/test/java/my/edu/umk/pams/academic/data/CohortGenerator.java
@@ -23,7 +23,7 @@ public class CohortGenerator extends GeneratorSupport {
             "   (SELECT ID\n" +
             "    FROM AC_PRGM_CODE PC\n" +
             "    WHERE PC.CODE = '%1$s'),\n" +
-            "   (SELECT CODE  || '/CHRT/0001'\n" +
+            "   (SELECT CODE  || '/CHRT/201720181'\n" +
             "    FROM AC_PRGM_CODE PC\n" +
             "    WHERE PC.CODE = '%1$s'),\n" +
             "   (SELECT 'COHORT' || DESCRIPTION\n" +
