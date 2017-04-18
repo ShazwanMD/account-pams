9e07071bca51106915955395de340ed505ebb88e
AZIETA <azieta.ma@umk.edu.my>
2017-04-18 16:32:32 +0800

    Add ThenCanMakePayment

---
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenCanMakePayment.java
@@ -0,0 +1,14 @@
+package my.edu.umk.pams.account.account.stage;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+@JGivenStage
+public class ThenCanMakePayment extends Stage<ThenCanMakePayment>{
+
+	public void I_can_make_payment() {
+	
+		
+	}
+
+}
