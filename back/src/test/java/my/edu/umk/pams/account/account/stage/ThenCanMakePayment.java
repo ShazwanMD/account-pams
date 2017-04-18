a5420543047df8c1cf7648cdbaeef6f861750a99
AZIETA <azieta.ma@umk.edu.my>
2017-04-18 16:36:41 +0800

    Update ThenCanMakePayment

--- back/src/test/java/my/edu/umk/pams/account/account/stage/ThenCanMakePayment.java
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/ThenCanMakePayment.java
@@ -1,14 +1,16 @@
 package my.edu.umk.pams.account.account.stage;
 
 import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.integration.spring.JGivenStage;
 
 @JGivenStage
 public class ThenCanMakePayment extends Stage<ThenCanMakePayment>{
 
-	public void I_can_make_payment() {
-	
+	@As("I can make payment")
+	public ThenCanMakePayment I_can_make_payment() {
 		
+		return self();	
 	}
 
 }
