1ba5ac53ec69f1e63c94af26d1611b8e7d741104
AZIETA <azieta.ma@umk.edu.my>
2017-04-18 16:33:01 +0800

    Add WhenViewFeesStatement

---
+++ back/src/test/java/my/edu/umk/pams/account/account/stage/WhenViewFeesStatement.java
@@ -0,0 +1,19 @@
+package my.edu.umk.pams.account.account.stage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.tngtech.jgiven.Stage;
+import com.tngtech.jgiven.annotation.As;
+import com.tngtech.jgiven.integration.spring.JGivenStage;
+
+@JGivenStage
+public class WhenViewFeesStatement extends Stage<WhenViewFeesStatement>{
+	private static final Logger LOG = LoggerFactory.getLogger(WhenViewFeesStatement.class);
+
+	@As("I want to view fees statement")
+	public void I_want_to_view_fees_statement() {
+		
+		
+	}
+}
