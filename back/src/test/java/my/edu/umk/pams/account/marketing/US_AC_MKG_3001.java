7da457efa221e4830f5de65c2602a345bd374c13
AZIETA <azieta.ma@umk.edu.my>
2017-04-20 15:24:53 +0800

    Update US_AC_MKG_3001

--- back/src/test/java/my/edu/umk/pams/account/marketing/US_AC_MKG_3001.java
+++ back/src/test/java/my/edu/umk/pams/account/marketing/US_AC_MKG_3001.java
@@ -9,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.tngtech.jgiven.annotation.As;
 import com.tngtech.jgiven.integration.spring.SpringScenarioTest;
+
+import my.edu.umk.pams.account.account.stage.WhenWantToRegisterStudentCompoundBill;
 import my.edu.umk.pams.account.config.TestAppConfiguration;
 import my.edu.umk.pams.account.marketing.stage.ThenDiscountCanAppliedToReceipt;
 import my.edu.umk.pams.account.marketing.stage.WhenIWantGiveCompoundsDiscountToStudent;
@@ -22,15 +24,15 @@ import my.edu.umk.pams.bdd.tags.Issue;
 @ContextConfiguration(classes = TestAppConfiguration.class)
 @As("As Academic (MGSEB/CPS), I want to give compound's discount to student so that discount can applied to receipt")
 public class US_AC_MKG_3001 extends
-		SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenIWantGiveCompoundsDiscountToStudent, ThenDiscountCanAppliedToReceipt> {
-
+		SpringScenarioTest<GivenIAmMGSEBAdministrator, WhenWantToRegisterStudentCompoundBill, ThenDiscountCanAppliedToReceipt> {
 	private static final String MATRIC_NO = "A17P001";
 
 	@Test
 	@Rollback
 	public void testScenario0() {
 		given().I_am_a_MGSEB_administrator_in_current_academic_session();
-		when().I_want_give_compounds_discount_to_student_$(MATRIC_NO);
+		when().I_want_to_register_student_compound_bill_by_account_$(MATRIC_NO);
+		addStage(WhenIWantGiveCompoundsDiscountToStudent.class).and().I_want_give_compounds_discount_to_student_$(MATRIC_NO);
 		then().discount_can_applied_to_receipt();
 	}
 }
