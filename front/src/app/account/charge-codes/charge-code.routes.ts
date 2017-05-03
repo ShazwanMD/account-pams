05f574b9940d490593525972e36504851803b6c8
AZIETA <azieta.ma@umk.edu.my>
2017-05-03 10:44:22 +0800

    Add charge code routes

---
+++ front/src/app/account/charge-codes/charge-code.routes.ts
@@ -0,0 +1,6 @@
+import {Routes, RouterModule} from '@angular/router';
+import {ChargeCodeCenterPage} from "./charge-code-center.page";
+
+export const ChargeCodeRoutes: Routes = [
+  {path: 'account/charge-codes', component: ChargeCodeCenterPage},
+];
