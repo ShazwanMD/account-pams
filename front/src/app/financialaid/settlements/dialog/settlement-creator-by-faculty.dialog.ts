84bbcc380031ed4be3b7b297c4989aac373a6334
Sahir <Sahir@10.3.32.202>
2017-05-09 17:03:41 +0800

    NEW [dialog] - create settlement by faculty

---
+++ front/src/app/financialaid/settlements/dialog/settlement-creator-by-faculty.dialog.ts
@@ -0,0 +1,51 @@
+import {Component, ViewContainerRef, OnInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Store} from "@ngrx/store";
+import {MdDialogRef} from "@angular/material";
+import {FinancialaidModuleState} from "../../index";
+import {SettlementActions} from "../settlement.action";
+import {SettlementCreator} from "../settlement-creator.interface";
+import {AcademicSession} from "../../../account/academic-sessions/academic-session.interface";
+import {FacultyCode} from "../../../common/faculty-codes/faculty-code.interface";
+
+@Component({
+  selector: 'pams-settlement-creator-by-faculty',
+  templateUrl: './settlement-creator-by-faculty.dialog.html',
+})
+
+export class SettlementCreatorByFacultyDialog implements OnInit {
+
+  private createForm: FormGroup;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private viewContainerRef: ViewContainerRef,
+              private store: Store<FinancialaidModuleState>,
+              private actions: SettlementActions,
+              private dialog: MdDialogRef<SettlementCreatorByFacultyDialog>) {
+  }
+
+  ngOnInit(): void {
+    this.createForm = this.formBuilder.group(<SettlementCreator>{
+      sourceNo:'',
+      description: '',
+      faculty:<FacultyCode>{},
+      academicSession:<AcademicSession>{},
+    });
+  }
+
+  save(settlementCreator: SettlementCreator, isValid: boolean) {
+    this.store.dispatch(this.actions.initSettlementByFacultyCode(settlementCreator,settlementCreator.facultyCode));
+    this.dialog.close();
+
+    // .subscribe(res => {
+    //   let snackBarRef = this._snackBar.open("Invoice started", "OK");
+    //   snackBarRef.afterDismissed().subscribe(() => {
+    //     this.goBack();
+    //   });
+    // });
+  }
+}
