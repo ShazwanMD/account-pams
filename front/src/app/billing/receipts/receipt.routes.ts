8705f7b864a79cdb5f4a67d1aaaac882363db78d
Sahir <sahir@umk.edu.my>
2017-05-12 11:23:56 +0800

    ADD view task page route

--- front/src/app/billing/receipts/receipt.routes.ts
+++ front/src/app/billing/receipts/receipt.routes.ts
@@ -1,11 +1,11 @@
 import {Routes, RouterModule} from '@angular/router';
 import {ReceiptCenterPage} from "./receipt-center.page";
-
+import {ReceiptTaskViewPage} from "./receipt-task-view.page";
 
 export const ReceiptRoutes: Routes = [
   {path: 'billing/receipts', component: ReceiptCenterPage},
   // {path: 'billing/receipts/new-task', component: ReceiptNewTaskPage},
   // {path: 'billing/receipts/assigned-tasks', component: ReceiptAssignedTaskPage},
   // {path: 'billing/receipts/pooled-tasks', component: ReceiptPooledTaskPage},
-  // {path: 'billing/receipts/view-task/:taskId', component: ReceiptViewTaskPage},
+  {path: 'billing/receipts/view-task/:taskId', component: ReceiptTaskViewPage},
 ];
