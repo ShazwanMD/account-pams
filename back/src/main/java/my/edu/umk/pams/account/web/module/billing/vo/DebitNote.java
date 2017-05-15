cd9acf879196b0223a13a9079ccd0e711714a865
aidamardiana <aidamardiana@10.3.32.44>
2017-05-15 17:27:58 +0800

    Add DebitNote VO

---
+++ back/src/main/java/my/edu/umk/pams/account/web/module/billing/vo/DebitNote.java
@@ -0,0 +1,76 @@
+package my.edu.umk.pams.account.web.module.billing.vo;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import my.edu.umk.pams.account.web.module.account.vo.Account;
+import my.edu.umk.pams.account.web.module.core.vo.Document;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+/**
+ * @author PAMS
+ */
+public class DebitNote extends Document {
+
+	private String referenceNo;
+	private String sourceNo;
+	private String auditNo;
+	private String description;
+	private BigDecimal totalAmount = new BigDecimal(0.00);
+
+	
+	public String getReferenceNo() {
+		return referenceNo;
+	}
+
+	public void setReferenceNo(String referenceNo) {
+		this.referenceNo = referenceNo;
+	}
+
+	public String getSourceNo() {
+		return sourceNo;
+	}
+
+	public void setSourceNo(String sourceNo) {
+		this.sourceNo = sourceNo;
+	}
+
+	public String getAuditNo() {
+		return auditNo;
+	}
+
+	public void setAuditNo(String auditNo) {
+		this.auditNo = auditNo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(BigDecimal totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	@JsonCreator
+    public static DebitNote create(String jsonString) {
+		DebitNote o = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            o = mapper.readValue(jsonString, DebitNote.class);
+        } catch (IOException e) {
+            // handle
+        }
+        return o;
+    }
+
+}
