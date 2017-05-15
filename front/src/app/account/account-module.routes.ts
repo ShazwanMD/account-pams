9756fcab029bc70075e3f9f777e65431accd5fd8
AZIETA <azieta.ma@umk.edu.my>
2017-05-15 15:36:15 +0800

    Update AccountModuleRoutes - insert AccountChargeRoutes

--- front/src/app/account/account-module.routes.ts
+++ front/src/app/account/account-module.routes.ts
@@ -3,10 +3,12 @@ import {AccountPage} from "./account.page";
 import {AccountRoutes} from "./accounts/account.routes";
 import {ChargeCodeRoutes} from "./charge-codes/charge-code.routes";
 import {FeeScheduleRoutes} from "./fee-schedules/fee-schedule.routes";
+import {AccountChargeRoutes} from "./account-charge/account-charge.routes";
 
 export const AccountModuleRoutes: Routes = [
   {path: 'account', component: AccountPage},
   ...AccountRoutes,
   ...ChargeCodeRoutes,
   ...FeeScheduleRoutes,
+  ...AccountChargeRoutes,
 ];
