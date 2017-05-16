f025548797024b7ded5e3d93b2efcd0d4ed54cfd
aidamardiana <aidamardiana@10.3.32.44>
2017-05-16 16:37:22 +0800

    Add invoice?:Invoice;

--- front/src/app/billing/debit-notes/debit-note.interface.ts
+++ front/src/app/billing/debit-notes/debit-note.interface.ts
@@ -1,6 +1,10 @@
-import {MetaObject} from "../../core/meta-object.interface";
+import { MetaObject } from "../../core/meta-object.interface";
+import { Invoice } from "../invoices/invoice.interface";
 export interface DebitNote extends MetaObject {
   code:string;
   description:string;
   totalAmount:number;
+  invoice?:Invoice;
+  
+
 }
