749df0d230402e4d64a1ddfdb58370e0c5cc453e
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-05-16 10:49:56 +0800

    Add study mode controller

--- back/src/main/java/my/edu/umk/pams/account/web/module/common/controller/CommonTransformer.java
+++ back/src/main/java/my/edu/umk/pams/account/web/module/common/controller/CommonTransformer.java
@@ -2,9 +2,10 @@ package my.edu.umk.pams.account.web.module.common.controller;
 
 import my.edu.umk.pams.account.common.model.AcCohortCode;
 import my.edu.umk.pams.account.common.model.AcFacultyCode;
+import my.edu.umk.pams.account.common.model.AcStudyMode;
 import my.edu.umk.pams.account.web.module.common.vo.CohortCode;
 import my.edu.umk.pams.account.web.module.common.vo.FacultyCode;
-
+import my.edu.umk.pams.account.web.module.common.vo.StudyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -31,6 +32,26 @@ public class CommonTransformer {
     public List<CohortCode> toCohortCodeVos(List<AcCohortCode> e) {
         List<CohortCode> vos = e.stream()
                 .map((e1) -> toCohortCodeVo(e1))
+                .collect(Collectors.toList());
+        return vos;
+    }
+    
+    //====================================================================================================
+    // STUDY MODE
+    //====================================================================================================
+
+    public StudyMode toStudyModeVo(AcStudyMode e) {
+        StudyMode vo = new StudyMode();
+        vo.setId(e.getId());
+        vo.setCode(e.getCode());
+        vo.setDescriptionMs(e.getDescriptionMs());
+        vo.setDescriptionEn(e.getDescriptionEn());
+        return vo;
+    }
+
+    public List<StudyMode> toStudyModeVos(List<AcStudyMode> e) {
+        List<StudyMode> vos = e.stream()
+                .map((e1) -> toStudyModeVo(e1))
                 .collect(Collectors.toList());
         return vos;
     }
