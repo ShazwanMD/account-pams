b94044682b109fefaaf96a7ecdcab2d290163fa6
aidamardiana <aidamardiana>
2017-05-17 11:34:27 +0800

    add invoice?:Invoice

--- front/src/app/billing/credit-notes/credit-note.interface.ts
+++ front/src/app/billing/credit-notes/credit-note.interface.ts
@@ -1,6 +1,9 @@
-import {MetaObject} from "../../core/meta-object.interface";
+import { MetaObject } from "../../core/meta-object.interface";
+import { Invoice } from "../invoices/invoice.interface";
 export interface CreditNote extends MetaObject {
   code:string;
   description:string;
   totalAmount:number;
+  invoice?:Invoice;
+
 }
