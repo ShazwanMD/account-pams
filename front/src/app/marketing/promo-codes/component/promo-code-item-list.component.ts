c11db9c29000979377f9b7f30cbad0a01fe8a73a
Sahir <sahir@umk.edu.my>
2017-05-21 01:47:00 +0800

    Add promoCodeItem component & dialog

--- front/src/app/marketing/promo-codes/component/promo-code-item-list.component.ts
+++ front/src/app/marketing/promo-codes/component/promo-code-item-list.component.ts
@@ -1,11 +1,88 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import {Component, Input, ChangeDetectionStrategy, ViewContainerRef, OnInit} from '@angular/core';
 import {PromoCodeItem} from "../promo-code-item.interface";
+import {PromoCodeItemEditorDialog} from "../dialog/promo-code-item-editor.dialog";
+import {MdDialog, MdDialogConfig, MdDialogRef, MdSnackBar} from "@angular/material";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MarketingModuleState} from "../../index";
+import {Store} from "@ngrx/store";
+import {PromoCodeActions} from "../promo-code.action";
+import {PromoCode} from "../promo-code.interface";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'pams-promo-code-item-list',
   templateUrl: './promo-code-item-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PromoCodeItemComponent {
+export class PromoCodeItemListComponent implements OnInit {
+
+  @Input() promoCode: PromoCode;
   @Input() promoCodeItems: PromoCodeItem[];
+
+  private selectedRows: PromoCodeItem[];
+  private editorDialogRef: MdDialogRef<PromoCodeItemEditorDialog>;
+  private columns: any[] = [
+    {name: 'code', label: 'Code'},
+    //{name: 'applied', label: 'Applied'},
+    {name: 'sourceNo', label: 'SourceNo'},
+    {name: 'account.code', label: 'Account'},
+    {name: 'action', label: ''}
+  ];
+  private PROMO_CODE = "marketingModuleState.promoCode".split(".");
+  private promoCode$: Observable<PromoCode>;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private actions: PromoCodeActions,
+              private store: Store<MarketingModuleState>,
+              private vcf: ViewContainerRef,
+              private dialog: MdDialog,
+              private snackbar: MdSnackBar) {
+      this.promoCode$ = this.store.select(...this.PROMO_CODE)
+  }
+
+  ngOnInit(): void {
+    this.selectedRows = this.promoCodeItems.filter(value => {console.log("selected value",value)}/*value.selected*/);
+  }
+
+  createDialog(): void {
+    this.promoCode$.subscribe(promoCode => this.showDialog(promoCode,null));
+  }
+
+  edit(promoCodeItem: PromoCodeItem): void {
+    this.promoCode$.subscribe(promoCode => this.showDialog(promoCode,promoCodeItem));
+  }
+
+  delete(): void {
+    console.log("length: " + this.selectedRows.length);
+    for (var i = 0; i < this.selectedRows.length; i++) {
+      this.store.dispatch(this.actions.deletePromoCodeItem(this.promoCode, this.selectedRows[i]));
+    }
+  }
+
+  filter(): void {
+  }
+
+  selectRow(promoCodeItem: PromoCodeItem): void {
+  }
+
+  selectAllRows(promoCodeItems: PromoCodeItem[]): void {
+  }
+
+
+  showDialog(promoCode: PromoCode, promoCodeItem: PromoCodeItem): void {
+    console.log("showDialog",promoCode);
+    let config = new MdDialogConfig();
+    config.viewContainerRef = this.vcf;
+    config.role = 'dialog';
+    config.width = '50%';
+    config.height = '60%';
+    config.position = {top: '0px'};
+    this.editorDialogRef = this.dialog.open(PromoCodeItemEditorDialog, config);
+    this.editorDialogRef.componentInstance.promoCode = promoCode;
+    if (promoCodeItem) this.editorDialogRef.componentInstance.promoCodeItem = promoCodeItem; // set
+    this.editorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
+    });
+  }
 }
---
+++ front/src/app/marketing/promo-codes/dialog/promo-code-item-editor.dialog.ts
@@ -0,0 +1,61 @@
+import {Component, ViewContainerRef, OnInit} from '@angular/core';
+import {FormGroup, FormControl} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Router, ActivatedRoute} from '@angular/router';
+import {PromoCodeItem} from "../promo-code-item.interface";
+import {Account} from "../../../account/accounts/account.interface";
+import {MdDialogRef} from "@angular/material";
+import {MarketingModuleState} from "../../index";
+import {Store} from "@ngrx/store";
+import {PromoCodeActions} from "../promo-code.action";
+import {PromoCode} from "../promo-code.interface";
+
+
+@Component({
+  selector: 'pams-promo-code-item-editor',
+  templateUrl: './promo-code-item-editor.dialog.html',
+})
+
+export class PromoCodeItemEditorDialog implements OnInit {
+
+  private editForm: FormGroup;
+  private _promoCodeItem: PromoCodeItem;
+  private _promoCode: PromoCode;
+  private edit: boolean = false;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private formBuilder: FormBuilder,
+              private viewContainerRef: ViewContainerRef,
+              private store: Store<MarketingModuleState>,
+              private actions:PromoCodeActions,
+              private dialog: MdDialogRef<PromoCodeItemEditorDialog>) {
+  }
+
+  set promoCodeItem(value: PromoCodeItem) {
+    this._promoCodeItem = value;
+    this.edit = true;
+  }
+
+  set promoCode(value: PromoCode) {
+    this._promoCode = value;
+  }
+
+  ngOnInit(): void {
+    this.editForm = this.formBuilder.group(<PromoCodeItem>{
+      id: null,
+      code: '',
+      //applied: 0,
+      sourceNo: 0,
+      account: <Account>{},
+    });
+    if (this.edit) this.editForm.patchValue(this._promoCodeItem);
+    console.log(this._promoCode)
+  }
+
+  submit(item: PromoCodeItem, isValid: boolean) {
+    if (!item.id) this.store.dispatch(this.actions.addPromoCodeItem(this._promoCode, item));
+    else  this.store.dispatch(this.actions.updatePromoCodeItem(this._promoCode, item));
+    this.dialog.close();
+  }
+}
