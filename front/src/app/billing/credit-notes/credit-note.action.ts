b77ababddf949557c86b1cf0f0c410bb6cdfbec1
hajar <hajar.ad@umk.edu.my>
2017-05-23 11:50:25 +0800

    Update credit note

--- front/src/app/billing/credit-notes/credit-note.action.ts
+++ front/src/app/billing/credit-notes/credit-note.action.ts
@@ -11,30 +11,70 @@ export class CreditNoteActions {
     };
   }
 
-  static FIND_CREDIT_NOTES_SUCCESS = '[DebitNote] Find DebitNotes Success';
-  findCreditNotesSuccess(creditNote): Action {
+  static FIND_CREDIT_NOTES_SUCCESS = '[CreditNote] Find CreditNotes Success';
+  findCreditNotesSuccess(creditNotes): Action {
     console.log("findCreditNotesSuccess");
-    console.log("CreditNote: " + creditNote.length);
+    console.log("CreditNote: " + creditNotes.length);
     return {
       type: CreditNoteActions.FIND_CREDIT_NOTES_SUCCESS,
+      payload: creditNotes
+    };
+  }
+  static FIND_CREDIT_NOTE_BY_ID = '[CreditNote] Find CreditNote By Id';
+
+  findCreditNoteById(id): Action {
+    return {
+      type: CreditNoteActions.FIND_CREDIT_NOTE_BY_ID,
+      payload: id
+    };
+  }
+
+  static FIND_CREDIT_NOTE_BY_ID_SUCCESS = '[CreditNote] Find CreditNote By Id Success';
+
+  findCreditNoteByIdSuccess(creditNote): Action {
+    return {
+      type: CreditNoteActions.FIND_CREDIT_NOTE_BY_ID_SUCCESS,
       payload: creditNote
     };
   }
 
-  static FIND_CREDIT_NOTE = '[CreditNote] Find CreditNote';
-  findCreditNote(code): Action {
+  static FIND_CREDIT_NOTES_BY_REFERENCE_NO = '[CreditNote] Find CreditNote By reference No';
+
+  findCreditNoteByReferenceNo(referenceNo): Action {
+    console.log("findCreditNoteByReferenceNo");
     return {
-      type: CreditNoteActions.FIND_CREDIT_NOTE,
-      payload: code
+      type: CreditNoteActions.FIND_CREDIT_NOTES_BY_REFERENCE_NO,
+      payload: referenceNo
     };
   }
 
-  static FIND_CREDIT_NOTE_SUCCESS = '[CreditNote] Find DebitNote Success';
-  findCreditNoteSuccess(creditNote): Action {
-    console.log("findCreditNoteSuccess");
+  static FIND_CREDIT_NOTES_BY_REFERENCE_NO_SUCCESS = '[CreditNote] Find CreditNote By reference No Success';
+
+  findCreditNoteByReferenceNoSuccess(creditNote): Action {
+    console.log("findCreditNoteByReferenceNoSuccess");
     return {
-      type: CreditNoteActions.FIND_CREDIT_NOTE_SUCCESS,
+      type: CreditNoteActions.FIND_CREDIT_NOTES_BY_REFERENCE_NO_SUCCESS,
       payload: creditNote
+    };
+  }
+  
+  static FIND_CREDIT_NOTE_TASK_BY_TASK_ID = '[CreditNote] Find CreditNote Task By Task Id';
+
+  findCreditNoteTaskByTaskId(taskId): Action {
+    console.log("findCreditNoteTaskByTaskId");
+    return {
+      type: CreditNoteActions.FIND_CREDIT_NOTE_TASK_BY_TASK_ID,
+      payload: taskId
+    };
+  }
+
+  static FIND_CREDIT_NOTE_TASK_BY_TASK_ID_SUCCESS = '[CreditNote] Find CreditNote Task By Task Id Success';
+
+  findCreditNoteTaskByTaskIdSuccess(task): Action {
+    console.log("findCreditNoteTaskByTaskIdSuccess");
+    return {
+      type: CreditNoteActions.FIND_CREDIT_NOTE_TASK_BY_TASK_ID_SUCCESS,
+      payload: task
     };
   }
 
