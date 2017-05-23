8eac43769a7811eb29ea85ffa0d722faa4d7214b
hajar <hajar.ad@umk.edu.my>
2017-05-23 11:57:20 +0800

    Add credit note reducer

---
+++ front/src/app/billing/credit-notes/credit-note.reducer.ts
@@ -0,0 +1,21 @@
+import {Action} from '@ngrx/store';
+import {CreditNote} from "./credit-note.interface";
+import {CreditNoteActions} from "./credit-note.action";
+
+export type CreditNoteState = CreditNote;
+
+const initialState: CreditNoteState = <CreditNoteState>{};
+
+export function debitNoteReducer(state = initialState, action: Action): CreditNoteState {
+  switch (action.type) {
+    case CreditNoteActions.FIND_CREDIT_NOTE_BY_ID_SUCCESS: {
+      return action.payload;
+    }
+    case CreditNoteActions.FIND_CREDIT_NOTES_BY_REFERENCE_NO_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
