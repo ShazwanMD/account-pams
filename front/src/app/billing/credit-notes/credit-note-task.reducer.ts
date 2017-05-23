c3b673c2ee7b432ddcf87da35bba4dfc45136401
hajar <hajar.ad@umk.edu.my>
2017-05-23 11:56:57 +0800

    Add credit note task reducer

---
+++ front/src/app/billing/credit-notes/credit-note-task.reducer.ts
@@ -0,0 +1,18 @@
+import {Action} from '@ngrx/store';
+import {CreditNoteTask} from "./credit-note-task.interface";
+import {CreditNoteActions} from "./credit-note.action";
+
+export type CreditNoteTaskState = CreditNoteTask;
+
+const initialState: CreditNoteTaskState = <CreditNoteTaskState>{};
+
+export function debitNoteTaskReducer(state = initialState, action: Action): CreditNoteTaskState {
+  switch (action.type) {
+    case CreditNoteActions.FIND_CREDIT_NOTE_TASK_BY_TASK_ID_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
