b7f199c58d5f0077113a0c1b533502cbef315d20
hajar <hajar.ad@umk.edu.my>
2017-05-23 11:01:29 +0800

    Add debit note interface

---
+++ front/src/app/billing/debit-notes/debit-note-task.interface.ts
@@ -0,0 +1,17 @@
+import {FlowState} from "../../core/flow-state.enum";
+import { Document } from "../../core/document.interface";
+import { DebitNote } from "./debit-note.interface";
+export interface DebitNoteTask extends Document {
+  taskId: string;
+  taskName: string;
+  candidate: string;
+  assignee: string;
+  referenceNo: string;
+  sourceNo: string;
+  accountCode: string;
+  description: string;
+  totalAmount: number;
+  issuedDate: number;
+  DebitNote: DebitNote;
+  flowState: FlowState;
+}
