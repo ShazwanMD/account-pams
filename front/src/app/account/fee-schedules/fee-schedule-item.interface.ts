6e52fa6229a0fee44406c47ef5c98666dfadc1b4
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-05-24 13:44:46 +0800

    Add description

--- front/src/app/account/fee-schedules/fee-schedule-item.interface.ts
+++ front/src/app/account/fee-schedules/fee-schedule-item.interface.ts
@@ -2,6 +2,7 @@ import {MetaObject} from "../../core/meta-object.interface";
 import {ChargeCode} from "../charge-codes/charge-code.interface";
 export interface FeeScheduleItem extends MetaObject {
   amount: number;
+  description:string;
   chargeCode: ChargeCode;
 
   // selection
