9032db34151ec6e32f56b794ee4b1437cbeb8642
hajar <hajar.ad@umk.edu.my>
2017-05-31 10:09:23 +0800

    Update AccountWaiverListComponent

--- front/src/app/account/accounts/component/account-waiver-list.component.ts
+++ front/src/app/account/accounts/component/account-waiver-list.component.ts
@@ -1,12 +1,41 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy, ViewContainerRef, OnInit} from '@angular/core';
 import {AccountTransaction} from "../account-transaction.interface";
 import {AccountWaiver} from "../account-waiver.interface";
+import { Observable } from "rxjs/Observable";
+import {Router, ActivatedRoute} from '@angular/router';
+import {MdDialogConfig, MdDialogRef, MdDialog} from "@angular/material";
+import {Store} from "@ngrx/store";
+import {AccountActions} from "../account.action";
+import { AccountModuleState } from "../../index";
+import {Account} from "../account.interface";
 
 @Component({
   selector: 'pams-account-waiver-list',
   templateUrl: './account-waiver-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AccountWaiverListComponent {
+export class AccountWaiverListComponent implements OnInit {
+  
   @Input() waivers: AccountWaiver[];
+  @Input() account: Account;
+  
+  private columns: any[] = [
+       {name: 'sourceNo', label: 'Source No'},
+       {name: 'amount', label: 'Amount'},
+       {name: 'AcademicSession.session', label: 'Session'},
+       {name: 'action', label: ''}
+  ];
+  
+  constructor(private router: Router,
+          private route: ActivatedRoute,
+          private actions: AccountActions,
+          private store: Store<AccountModuleState>,
+          private vcf: ViewContainerRef,
+          private dialog: MdDialog) {
+}
+  
+  ngOnInit(): void{
+
+    }
+  
 }
