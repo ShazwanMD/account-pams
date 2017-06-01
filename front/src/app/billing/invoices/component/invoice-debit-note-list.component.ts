31ea92fbf07d56c875ed89a6537325f03c195a65
aidamardiana <aidamardiana>
2017-06-01 13:34:52 +0800

    Add debit note list component ts

---
+++ front/src/app/billing/invoices/component/invoice-debit-note-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import { DebitNote } from "../../debit-notes/debit-note.interface";
+
+@Component({
+  selector: 'pams-invoice-debit-note-list',
+  templateUrl: './invoice-debit-note-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InvoiceDebitNoteListComponent {
+  @Input() debitNotes: DebitNote[];
+
+private columns: any[] = [
+    {name: 'referenceNo', label: 'ReferenceNo'},
+    {name: 'description', label: 'Description'},
+    {name: 'totalAmount', label: 'Total Amount'},
+    {name: 'flowState', label: 'Status'},
+    {name: 'action', label: ''}
+  ];
+
+}
