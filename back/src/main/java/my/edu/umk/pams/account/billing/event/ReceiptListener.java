1e2923e9022b25587beb31bf3c851fdbfa8ee010
rafizan_baharum <rafizan.baharum@canang.com.my>
2017-06-04 00:24:44 +0800

    Uncomment listener

--- back/src/main/java/my/edu/umk/pams/account/billing/event/ReceiptListener.java
+++ back/src/main/java/my/edu/umk/pams/account/billing/event/ReceiptListener.java
@@ -1,5 +1,8 @@
 package my.edu.umk.pams.account.billing.event;
 
+import my.edu.umk.pams.account.billing.model.AcInvoice;
+import my.edu.umk.pams.account.billing.model.AcInvoiceItem;
+import my.edu.umk.pams.account.billing.model.AcReceipt;
 import my.edu.umk.pams.account.billing.service.BillingService;
 import my.edu.umk.pams.account.security.event.AccessListener;
 import org.slf4j.Logger;
@@ -7,6 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author PAMS
@@ -21,22 +27,20 @@ public class ReceiptListener implements ApplicationListener<ReceiptEvent> {
 
     @Override
     public void onApplicationEvent(ReceiptEvent event) {
-//        if (event instanceof ReceiptApprovedEvent) {
-//            AcReceipt receipt = event.getReceipt();
-//            List<AcInvoice> invoices = receipt.getInvoices();
-//
-//            // knock off invoice items
-//            for (AcInvoice invoice : invoices) {
-//                List<AcInvoiceItem> items = billingService.findInvoiceItems(invoice);
-//                for (AcInvoiceItem invoiceItem : items) {
+        if (event instanceof ReceiptApprovedEvent) {
+            AcReceipt receipt = event.getReceipt();
+            Set<AcInvoice> invoices = receipt.getInvoices();
+            for (AcInvoice invoice : invoices) {
+                List<AcInvoiceItem> invoiceItems = billingService.findInvoiceItems(invoice);
+                for (AcInvoiceItem invoiceItem : invoiceItems) {
 //                    // find matching receipt item
 //                    AcReceiptItem receiptItem = billingService.findReceiptItemByChargeCode(invoiceItem.getChargeCode());
 //                    // knock off
 //                    invoiceItem.setBalanceAmount(
 //                            invoice.getBalanceAmount().subtract(receiptItem.getAppliedAmount()));
 //                    billingService.updateInvoiceItem(invoice, invoiceItem);
-//                }
-//            }
-//        }
+                }
+            }
+        }
     }
 }
