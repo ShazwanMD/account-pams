c87db9242eea47a2364f5bf35a527f6ab83d9c4c
AZIETA <azieta.ma@umk.edu.my>
2017-06-04 10:22:15 +0800

    Add reference no in Account Transformer

--- back/src/main/java/my/edu/umk/pams/account/web/module/account/controller/AccountTransformer.java
+++ back/src/main/java/my/edu/umk/pams/account/web/module/account/controller/AccountTransformer.java
@@ -107,6 +107,7 @@ public class AccountTransformer {
         AccountCharge m = new AccountCharge();
         m.setId(e.getId());
         m.setSourceNo(e.getSourceNo());
+        m.setReferenceNo(e.getReferenceNo());
         m.setDescription(e.getDescription());
         m.setAmount(e.getAmount());
         m.setChargeType(AccountChargeType.get(e.getChargeType().ordinal()));
