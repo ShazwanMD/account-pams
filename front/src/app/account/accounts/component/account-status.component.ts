2b0274fd23d2c3ecec36344536b78b25efb39825
hajar <hajar.ad@umk.edu.my>
2017-06-04 12:03:46 +0800

    Waiver calculation

--- front/src/app/account/accounts/component/account-status.component.ts
+++ front/src/app/account/accounts/component/account-status.component.ts
@@ -1,5 +1,12 @@
-import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy} from '@angular/core';
+import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy, ViewContainerRef, OnInit} from '@angular/core';
 import {Account} from "../account.interface";
+import {AccountWaiver} from "../account-waiver.interface";
+import { Observable } from "rxjs/Observable";
+import { Router, ActivatedRoute } from "@angular/router";
+import { AccountActions } from "../account.action";
+import { Store } from "@ngrx/store";
+import { MdDialog } from "@angular/material";
+import { AccountModuleState } from "../../index";
 
 @Component({
   selector: 'pams-account-status',
@@ -9,4 +16,22 @@ import {Account} from "../account.interface";
 })
 export class AccountStatusComponent {
   @Input() account: Account;
+  @Input() waivers: AccountWaiver[];
+                  
+  getSum(){
+      let sum = 0;
+      for (var i = 0; i < this.waivers.length; i++) {
+          if (this.waivers[i].amount) {
+              sum +=this.waivers[i].amount;
+          }
+     return sum;
+     }
+  }
+  
+  getWaived(){
+      let effectiveBalance = 0;
+      effectiveBalance = this.account.balance-this.getSum();
+      
+      return effectiveBalance;
+  }
 }
