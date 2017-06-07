45de0a167c0aeb8fb1604ff7038aa37edef749d1
hajar <hajar.ad@umk.edu.my>
2017-06-07 10:10:28 +0800

    Add AcTaxCode interface

---
+++ back/src/main/java/my/edu/umk/pams/account/common/model/AcTaxCode.java
@@ -0,0 +1,12 @@
+package my.edu.umk.pams.account.common.model;
+
+public interface AcTaxCode extends my.edu.umk.pams.account.core.AcMetaObject {
+
+    String getCode();
+
+    void setCode(String code);
+
+    String getName();
+
+    void setName(String description);
+}
