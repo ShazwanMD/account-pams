a8f509349a9a81718a460e1f9f15225fa77f4178
hajar <hajar.ad@umk.edu.my>
2017-06-07 10:10:53 +0800

    Add AcTaxCodeImpl class

---
+++ back/src/main/java/my/edu/umk/pams/account/common/model/AcTaxCodeImpl.java
@@ -0,0 +1,79 @@
+package my.edu.umk.pams.account.common.model;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import my.edu.umk.pams.account.core.AcMetadata;
+
+@Entity(name = "AcTaxCode")
+@Table(name = "AC_TAX_CODE")
+public class AcTaxCodeImpl implements AcTaxCode {
+
+    @Id
+    @Column(name = "ID", nullable = false)
+    @GeneratedValue(generator = "SQ_AC_TAX_CODEE")
+    @SequenceGenerator(name = "SQ_AC_TAX_CODE", sequenceName = "SQ_AC_TAX_CODE", allocationSize = 1)
+    private Long id;
+
+    @NotNull
+    @Column(name = "CODE", unique = true, nullable = false)
+    private String code;
+
+    @NotNull
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    @Embedded
+    private AcMetadata metadata;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public void setCode(String code) {
+        this.code = code;
+    }
+    
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void setName(String description) {
+        this.name = description;
+    }
+
+    @Override
+    public AcMetadata getMetadata() {
+        return metadata;
+    }
+
+    @Override
+    public void setMetadata(AcMetadata metadata) {
+        this.metadata = metadata;
+    }
+
+    @Override
+    public Class<?> getInterfaceClass() {
+        return AcTaxCode.class;
+    }
+}
