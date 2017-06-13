3e2b0e36678d10d385bbcb3e8c30f78ccc4f75a2
AZIETA <azieta.ma@umk.edu.my>
2017-06-13 09:47:16 +0800

    create AcCompoundChargeImpl

---
+++ back/src/main/java/my/edu/umk/pams/account/account/model/AcCompoundChargeImpl.java
@@ -0,0 +1,62 @@
+package my.edu.umk.pams.account.account.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author PAMS
+ */
+@Entity(name = "AcCompoundCharge")
+@Table(name = "AC_CMPD_CHRG")
+//@Inheritance(strategy = InheritanceType.JOINED)
+public class AcCompoundChargeImpl extends AcAccountChargeImpl implements AcCompoundCharge {
+
+    @NotNull
+    @Column(name = "COMPOUND_CODE", unique = true, nullable = false)
+    private String compoundCode;
+
+    @Column(name = "COMPOUND_DESCRIPTION", nullable = false)
+    private String compoundDescription;
+    
+    @NotNull
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "ACCOUNT_CHARGE_TYPE")
+    private AcAccountChargeType chargeType;
+    
+	@Override
+	public String getCompoundCode() {
+		return compoundCode;
+	}
+
+	@Override
+	public void setCompoundCode(String compoundCode) {
+		this.compoundCode = compoundCode;
+	}
+
+	@Override
+	public String getCompoundDescription() {
+		return compoundDescription;
+	}
+
+	@Override
+	public void setCompoundDescription(String compoundDescription) {
+		this.compoundDescription = compoundDescription;
+	}
+
+	@Override
+	public AcAccountChargeType getChargeType() {
+		return chargeType;
+	}
+
+	@Override
+	public void setChargeType(AcAccountChargeType chargeType) {
+		this.chargeType = chargeType;
+	}
+
+   
+    
+}
