8e2ffc9d025f3e9baec4988862d533d790ef94bf
AZIETA <azieta.ma@umk.edu.my>
2017-06-18 12:40:40 +0800

    update by add cohort, studymode, chargeType, date, ordinal

--- back/src/main/java/my/edu/umk/pams/account/account/model/AcAccountChargeImpl.java
+++ back/src/main/java/my/edu/umk/pams/account/account/model/AcAccountChargeImpl.java
@@ -2,12 +2,19 @@ package my.edu.umk.pams.account.account.model;
 
 import my.edu.umk.pams.account.billing.model.AcInvoice;
 import my.edu.umk.pams.account.billing.model.AcInvoiceImpl;
+import my.edu.umk.pams.account.common.model.AcCohortCode;
+import my.edu.umk.pams.account.common.model.AcCohortCodeImpl;
+import my.edu.umk.pams.account.common.model.AcStudyMode;
+import my.edu.umk.pams.account.common.model.AcStudyModeImpl;
 import my.edu.umk.pams.account.core.AcMetadata;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.util.Date;
 /**
  * @author PAMS
  */
@@ -38,15 +45,21 @@ public class AcAccountChargeImpl implements AcAccountCharge {
     private BigDecimal amount = BigDecimal.ZERO;
 
     @NotNull
-    @Column(name = "CHARGE_TYPE", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "CHARGE_TYPE")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private AcAccountChargeType chargeType;
+    
+    @NotNull
+    @Column(name = "DOC", nullable = false)
+    private Date doc;
 
     @NotNull
     @ManyToOne(targetEntity = AcAccountImpl.class)
     @JoinColumn(name = "ACCOUNT_ID")
     private AcAccount account;
 
-    @NotNull
+	@NotNull
     @ManyToOne(targetEntity = AcAcademicSessionImpl.class)
     @JoinColumn(name = "SESSION_ID", nullable = false)
     private AcAcademicSession session;
@@ -54,8 +67,52 @@ public class AcAccountChargeImpl implements AcAccountCharge {
     @ManyToOne(targetEntity = AcInvoiceImpl.class)
     @JoinColumn(name = "INVOICE_ID", nullable = true)
     private AcInvoice invoice;
+    
+    @NotNull
+    @OneToOne(targetEntity = AcStudyModeImpl.class)
+    @JoinColumn(name = "STUDY_MODE_ID")
+    private AcStudyMode studyMode;
+    
+    @NotNull
+    @Column(name = "ORDINAL", nullable = false, columnDefinition = "int default 0")
+    private Integer ordinal = 0;
 
-    @Embedded
+    @NotNull
+    @OneToOne(targetEntity = AcCohortCodeImpl.class)
+    @JoinColumn(name = "COHORT_CODE_ID")
+    private AcCohortCode cohortCode;
+
+    @Override
+    public AcStudyMode getStudyMode() {
+		return studyMode;
+	}
+    
+    @Override
+    public Date getDoc() {
+		return doc;
+	}
+
+    @Override
+    public void setDoc(Date doc) {
+		this.doc = doc;
+	}
+
+    @Override
+    public void setStudyMode(AcStudyMode studyMode) {
+		this.studyMode = studyMode;
+	}
+
+	@Override
+	public AcCohortCode getCohortCode() {
+		return cohortCode;
+	}
+
+	@Override
+	public void setCohortCode(AcCohortCode cohortCode) {
+		this.cohortCode = cohortCode;
+	}
+	
+	@Embedded
     private AcMetadata metadata;
 
     @Override
@@ -107,17 +164,18 @@ public class AcAccountChargeImpl implements AcAccountCharge {
         this.amount = amount;
     }
 
+    
     @Override
-    public AcAccountChargeType getChargeType() {
-        return chargeType;
-    }
+    public Integer getOrdinal() {
+		return ordinal;
+	}
 
     @Override
-    public void setChargeType(AcAccountChargeType chargeType) {
-        this.chargeType = chargeType;
-    }
+    public void setOrdinal(Integer ordinal) {
+		this.ordinal = ordinal;
+	}
 
-    @Override
+	@Override
     public AcAccount getAccount() {
         return account;
     }
@@ -162,8 +220,19 @@ public class AcAccountChargeImpl implements AcAccountCharge {
         return AcAccountCharge.class;
     }
 
+    @Override
+    public void setChargeType(AcAccountChargeType chargeType) {
+		this.chargeType = chargeType;
+	}
+    
+    @Override
+    public AcAccountChargeType getChargeType() {
+		return chargeType;
+	}
+    
 	@Override
 	public AcAccountChargeType getChargeType(String i) {
 		return chargeType.valueOf(i);
 	}
+
 }
