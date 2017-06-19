b013a22fc1215a9ca7cc3d93d7e2b0cfaa5702d1
nurazieta <azieta.ma@umk.edu.my>
2017-06-19 08:55:51 +0800

    add code, ordinal, studymode, cohortCode, date

--- front/src/app/account/accounts/account-charge.interface.ts
+++ front/src/app/account/accounts/account-charge.interface.ts
@@ -1,13 +1,21 @@
 import {AcademicSession} from "../academic-sessions/academic-session.interface";
+import {StudyMode} from "../../common/study-modes/study-mode.interface";
+import {CohortCode} from "../../common/cohort-codes/cohort-code.interface";
 import {MetaObject} from "../../core/meta-object.interface";
 import {AccountChargeType} from "./account-charge-type.enum";
 export interface AccountCharge extends MetaObject {
   referenceNo: string;
   sourceNo: string;
+  code: string;
   description: string;
   amount: number;
   chargeType: AccountChargeType;
   session: AcademicSession;
+  doc: Date;
+  ordinal: number;
+
+  studyMode?: StudyMode;
+  cohortCode?: CohortCode;
 
   // transient
   invoiced?: boolean;
