14fc404c2cfc85dbaa3bf9191dde0a166b1f957d
hajar <hajar.ad@umk.edu.my>
2017-06-19 15:52:44 +0800

    Add findByFlowState in dao

--- back/src/main/java/my/edu/umk/pams/account/financialaid/dao/AcWaiverApplicationDao.java
+++ back/src/main/java/my/edu/umk/pams/account/financialaid/dao/AcWaiverApplicationDao.java
@@ -1,6 +1,8 @@
 package my.edu.umk.pams.account.financialaid.dao;
 
 import my.edu.umk.pams.account.account.model.AcAcademicSession;
+import my.edu.umk.pams.account.billing.model.AcInvoice;
+import my.edu.umk.pams.account.core.AcFlowState;
 import my.edu.umk.pams.account.core.GenericDao;
 import my.edu.umk.pams.account.financialaid.model.AcWaiverApplication;
 import java.util.List;
@@ -15,6 +17,8 @@ public interface AcWaiverApplicationDao extends GenericDao<Long, AcWaiverApplica
     // ====================================================================================================
 
     AcWaiverApplication findByReferenceNo(String referenceNo);
+    
+    List<AcWaiverApplication> findByFlowState(AcFlowState acFlowState);
 
     List<AcWaiverApplication> find(AcAcademicSession academicSession, Integer offset, Integer limit);
 
