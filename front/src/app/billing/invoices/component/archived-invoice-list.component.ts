0abb84362f078d3cd9dbd490598643075db2f72f
aidamardiana <aidamardiana>
2017-06-21 14:12:43 +0800

    Update column

--- front/src/app/billing/invoices/component/archived-invoice-list.component.ts
+++ front/src/app/billing/invoices/component/archived-invoice-list.component.ts
@@ -15,7 +15,7 @@ export class ArchivedInvoiceListComponent {
 
   private columns: any[] = [
     {name: 'referenceNo', label: 'ReferenceNo'},
-    {name: 'accountCode', label: 'Account'},
+    {name: 'account.code', label: 'Account'},
     {name: 'description', label: 'Description'},
     {name: 'totalAmount', label: 'Total Amount'},
     {name: 'balanceAmount', label: 'Balance Amount'},
