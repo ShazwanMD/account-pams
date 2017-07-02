cce1d38045ba4c0f5e7881a66f4d8a955dd1bc18
aidamardiana <aidamardiana>
2017-07-02 15:33:23 +0800

    Update CreditNoteTask - add accountCode

--- back/src/main/java/my/edu/umk/pams/account/web/module/billing/vo/CreditNoteTask.java
+++ back/src/main/java/my/edu/umk/pams/account/web/module/billing/vo/CreditNoteTask.java
@@ -1,10 +1,17 @@
 package my.edu.umk.pams.account.web.module.billing.vo;
 
+import java.math.BigDecimal;
+
 import my.edu.umk.pams.account.web.module.core.vo.Task;
 
 public class CreditNoteTask extends Task {
 
     private CreditNote creditNote;
+    
+    private String accountCode;
+    
+    private BigDecimal totalAmount = new BigDecimal(0.00);
+    
 
     public CreditNote getCreditNote() {
         return creditNote;
@@ -13,5 +20,22 @@ public class CreditNoteTask extends Task {
     public void setCreditNote(CreditNote creditNote) {
         this.creditNote = creditNote;
     }
+    
+    public String getAccountCode() {
+		return accountCode;
+	}
+
+	public void setAccountCode(String accountCode) {
+		this.accountCode = accountCode;
+	}
+	
+	public BigDecimal getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(BigDecimal totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+    
 }
 
