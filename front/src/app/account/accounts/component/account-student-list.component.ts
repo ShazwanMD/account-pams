9c7062903c401e99035a3cc4147efe64475edef7
hajar <hajar.ad@umk.edu.my>
2017-07-03 09:48:17 +0800

    Search & pagination for student list ts

--- front/src/app/account/accounts/component/account-student-list.component.ts
+++ front/src/app/account/accounts/component/account-student-list.component.ts
@@ -7,7 +7,7 @@ import {AccountActions} from "../account.action";
 import { AccountModuleState } from "../../index";
 import { Actor } from "../../../identity/actor.interface";
 import { ActorType } from "../../../identity/actor-type.enum";
-
+import { IPageChangeEvent, ITdDataTableSortChangeEvent, TdDataTableSortingOrder, TdDataTableService } from "@covalent/core";
 
 @Component({
   selector: 'pams-account-student-list',
@@ -17,8 +17,7 @@ import { ActorType } from "../../../identity/actor-type.enum";
 export class AccountStudentListComponent {
 
   @Input() actor: Actor;
-
-  @Input() accountStudentList: Account;
+  @Input() accountStudentList: Account[];
   
 private columns: any[] = [
 {name: 'code', label: 'Code'},
@@ -32,12 +31,55 @@ private columns: any[] = [
           private actions: AccountActions,
           private store: Store<AccountModuleState>,
           private vcf: ViewContainerRef,
-          private dialog: MdDialog) {
-
-}
+          private dialog: MdDialog,
+          private _dataTableService: TdDataTableService) {}
   
   viewAccount(account: Account) {
       console.log("account: " + account.id);
       this.router.navigate(['/accounts-detail', account.id]);
     }
+  
+  
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  sortBy: string = 'code';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+    
+  ngAfterViewInit(): void {
+    this.filteredData = this.accountStudentList;
+    this.filteredTotal = this.accountStudentList.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.accountStudentList;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
+  }
 }
