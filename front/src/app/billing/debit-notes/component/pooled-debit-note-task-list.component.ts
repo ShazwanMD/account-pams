afc6c43d75abbfa216292499e1fe83e7071d58ce
aidamardiana <aidamardiana>
2017-07-06 11:49:22 +0800

    Update pooled debit note task list.ts - add search and pagination

--- front/src/app/billing/debit-notes/component/pooled-debit-note-task-list.component.ts
+++ front/src/app/billing/debit-notes/component/pooled-debit-note-task-list.component.ts
@@ -2,6 +2,8 @@ import {Component, Input, EventEmitter, Output, ChangeDetectionStrategy, ViewCon
 import { MdSnackBar } from "@angular/material";
 import { DebitNote } from "../debit-note.interface";
 import { DebitNoteTask } from "../debit-note-task.interface";
+import { Router, ActivatedRoute } from "@angular/router";
+import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, IPageChangeEvent } from "@covalent/core";
 
 @Component({
   selector: 'pams-pooled-debit-note-task-list',
@@ -23,7 +25,10 @@ export class PooledDebitNoteTaskListComponent {
     {name: 'action', label: ''}
   ];
 
-  constructor(private snackBar: MdSnackBar) {
+   constructor(private snackBar: MdSnackBar,
+          private router: Router,
+          private route: ActivatedRoute,
+          private _dataTableService: TdDataTableService) {
   }
 
   claimTask(task: DebitNoteTask): void {
@@ -32,5 +37,48 @@ export class PooledDebitNoteTaskListComponent {
     snackBarRef.afterDismissed().subscribe(() => {
       this.claim.emit(task);
     });
+  }
+
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+    
+  ngAfterViewInit(): void {
+    this.filteredData = this.debitNoteTasks;
+    this.filteredTotal = this.debitNoteTasks.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
+  filter(): void {
+    let newData: any[] = this.debitNoteTasks;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
   }
 }
