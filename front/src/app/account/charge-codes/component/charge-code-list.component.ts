c939309237c299c02590c3919232d0248470f2a7
nurazieta <azieta.ma@umk.edu.my>
2017-07-09 12:15:24 +0800

    add pagination & search

--- front/src/app/account/charge-codes/component/charge-code-list.component.ts
+++ front/src/app/account/charge-codes/component/charge-code-list.component.ts
@@ -6,6 +6,8 @@ import {AccountModuleState} from "../../index";
 import {MdDialogRef, MdDialogConfig, MdDialog} from "@angular/material";
 import {ChargeCodeEditorDialog} from "../dialog/charge-code-editor.dialog";
 import {Observable} from "rxjs/Observable";
+import { TdDataTableService, TdDataTableSortingOrder, ITdDataTableSortChangeEvent, IPageChangeEvent } from "@covalent/core";
+
 
 @Component({
   selector: 'pams-charge-code-list',
@@ -30,8 +32,9 @@ export class ChargeCodeListComponent {
   constructor(private store: Store<AccountModuleState>,
               private actions: ChargeCodeActions,
               private vcf: ViewContainerRef,
-              private dialog: MdDialog) {
-    this.chargeCodes$ = this.store.select(...this.CHARGE_CODES);
+              private dialog: MdDialog,
+              private _dataTableService: TdDataTableService) {
+              this.chargeCodes$ = this.store.select(...this.CHARGE_CODES);
   }
 
   editDialog(code: ChargeCode): void {
@@ -42,7 +45,47 @@ export class ChargeCodeListComponent {
     this.store.dispatch(this.actions.removeChargeCode(code))
   }
 
+filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  sortBy: string = 'code';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
+    
+  ngAfterViewInit(): void {
+    this.filteredData = this.chargeCodes;
+    this.filteredTotal = this.chargeCodes.length;
+    this.filter();
+  }
+
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+
   filter(): void {
+    let newData: any[] = this.chargeCodes;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
   }
 
   private showDialog(code: ChargeCode): void {
