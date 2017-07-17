9bbdb6b45a303e83c416c2fc4a96de936ceadcaf
hajar <hajar.ad@umk.edu.my>
2017-07-17 19:21:35 +0800

    remove @input feescheduleitems

--- front/src/app/account/fee-schedules/component/fee-schedule-status.component.ts
+++ front/src/app/account/fee-schedules/component/fee-schedule-status.component.ts
@@ -4,7 +4,6 @@ import { Router, ActivatedRoute } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { MdDialog } from "@angular/material";
 import { AccountModuleState } from "../../index";
-import { FeeScheduleItem } from "../fee-schedule-item.interface";
 import { FeeScheduleActions } from "../fee-schedule.action";
 import { FeeSchedule } from "../fee-schedule.interface";
 
@@ -17,25 +16,5 @@ import { FeeSchedule } from "../fee-schedule.interface";
 export class FeeScheduleStatusComponent {
 
   @Input() feeSchedule: FeeSchedule;
-  @Input() feeScheduleItems: FeeScheduleItem[];
-
-  constructor(private router: Router,
-          private route: ActivatedRoute,
-          private actions: FeeScheduleActions,
-          private store: Store<AccountModuleState>,
-          private vcf: ViewContainerRef){
-
-    }
-  
-  getSum(){
-      let sum = 0;
-      for (var i = 0; i < this.feeScheduleItems.length; i++) {
-          //if (this.feeScheduleItems[i].amount) {
-              sum +=this.feeScheduleItems[i].amount;
-          //}
-      }
-     return sum;
-     //this.store.dispatch(this.actions.updateFeeSchedule(this.feeSchedule));
-     }
   
 }
