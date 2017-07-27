f827dda82f001aea3fe7449b3ad8c65c7e1e1633
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-07-27 20:24:16 +0800

    Add exception handling

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -4,3 +4,4 @@ export {AccountService} from './account.service';
 export {IdentityService} from './identity.service';
 export {SystemService} from './system.service';
 export {CommonService} from './common.service';
+export {NotificationService} from './notification.service';
