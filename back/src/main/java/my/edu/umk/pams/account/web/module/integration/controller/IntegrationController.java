ca05db71180bce333ce94b50eb14e28f2f9b2ba5
Rafizan Baharum <rafizan.baharum@canang.com.my>
2017-07-29 19:32:58 +0800

    Add integration endpoints

--- back/src/main/java/my/edu/umk/pams/account/web/module/integration/controller/IntegrationController.java
+++ back/src/main/java/my/edu/umk/pams/account/web/module/integration/controller/IntegrationController.java
@@ -6,13 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import my.edu.umk.pams.account.common.model.AcCohortCode;
+import my.edu.umk.pams.account.common.model.AcCohortCodeImpl;
+import my.edu.umk.pams.account.common.model.AcFacultyCode;
+import my.edu.umk.pams.account.common.model.AcFacultyCodeImpl;
+import my.edu.umk.pams.account.common.model.AcProgramCode;
+import my.edu.umk.pams.account.common.model.AcProgramCodeImpl;
 import my.edu.umk.pams.account.common.service.CommonService;
 import my.edu.umk.pams.account.identity.service.IdentityService;
+import my.edu.umk.pams.account.security.integration.AcAutoLoginToken;
+import my.edu.umk.pams.account.security.integration.NonSerializableSecurityContext;
+import my.edu.umk.pams.connector.payload.CohortCodePayload;
+import my.edu.umk.pams.connector.payload.FacultyCodePayload;
+import my.edu.umk.pams.connector.payload.ProgramCodePayload;
 
 @Transactional
 @RestController
@@ -31,21 +46,60 @@ public class IntegrationController {
     private IdentityService identityService;
 
     // ====================================================================================================
-    // COHORT
+    // CODES
     // ====================================================================================================
 
-    @RequestMapping(value = "/cohortCode", method = RequestMethod.POST)
-    public ResponseEntity<String> saveCohortCode() {
+
+    @RequestMapping(value = "/cohortCodes", method = RequestMethod.POST)
+    public ResponseEntity<String> saveCohortCode(@RequestBody CohortCodePayload payload) {
+        SecurityContext ctx = loginAsSystem();
+
+        AcCohortCode cohortCode = new AcCohortCodeImpl();
+        cohortCode.setCode(payload.getCode());
+        cohortCode.setDescription(payload.getDescription());
+        cohortCode.setProgramCode(commonService.findProgramCodeByCode(payload.getProgramCode().getCode()));
+
+        logoutAsSystem(ctx);
         return new ResponseEntity<String>("success", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/programCode", method = RequestMethod.POST)
-    public ResponseEntity<String> saveProgramCode() {
+    @RequestMapping(value = "/programCodes", method = RequestMethod.POST)
+    public ResponseEntity<String> saveProgramCode(ProgramCodePayload payload) {
+        SecurityContext ctx = loginAsSystem();
+
+        AcProgramCode programCode = new AcProgramCodeImpl();
+        programCode.setCode(payload.getCode());
+        programCode.setDescription(payload.getDescription());
+        programCode.setFacultyCode(commonService.findFacultyCodeByCode(payload.getFacultyCode().getCode()));
+
+        logoutAsSystem(ctx);
         return new ResponseEntity<String>("success", HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/facultyCode", method = RequestMethod.POST)
-    public ResponseEntity<String> saveFacultyCode() {
+    @RequestMapping(value = "/facultyCodes", method = RequestMethod.POST)
+    public ResponseEntity<String> saveFacultyCode(@RequestBody FacultyCodePayload payload) {
+        SecurityContext ctx = loginAsSystem();
+
+        AcFacultyCode facultyCode = new AcFacultyCodeImpl();
+        facultyCode.setCode(payload.getCode());
+        facultyCode.setDescription(payload.getDescription());
+
+        logoutAsSystem(ctx);
         return new ResponseEntity<String>("success", HttpStatus.OK);
     }
+
+    private SecurityContext loginAsSystem() {
+        SecurityContext savedCtx = SecurityContextHolder.getContext();
+        AcAutoLoginToken authenticationToken = new AcAutoLoginToken("system");
+        Authentication authed = authenticationManager.authenticate(authenticationToken);
+        SecurityContext system = new NonSerializableSecurityContext();
+        system.setAuthentication(authed);
+        SecurityContextHolder.setContext(system);
+        return savedCtx;
+    }
+
+    private void logoutAsSystem(SecurityContext context) {
+        SecurityContextHolder.setContext(context);
+    }
+
 }
