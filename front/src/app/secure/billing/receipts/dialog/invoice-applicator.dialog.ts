01efe360017d43f0ab77fb2d0a0b07c4a4ea906b
hajar <hajar.ad@umk.edu.my>
2017-08-01 16:30:41 +0800

    add set invoice

--- front/src/app/secure/billing/receipts/dialog/invoice-applicator.dialog.ts
+++ front/src/app/secure/billing/receipts/dialog/invoice-applicator.dialog.ts
@@ -6,6 +6,8 @@ import {BillingModuleState} from '../../index';
 import {MdDialogRef} from '@angular/material';
 import {ReceiptActions} from '../receipt.action';
 import {Store} from '@ngrx/store';
+import { Invoice } from "../../../../shared/model/billing/invoice.interface";
+import { InvoiceActions } from "../../invoices/invoice.action";
 
 @Component({
   selector: 'pams-invoice-applicator',
@@ -17,23 +19,29 @@ export class InvoiceApplicatorDialog implements OnInit {
   private SELECTED_INVOICE: string[] = 'billingModuleState.selectedInvoice'.split('.');
   private SELECTED_INVOICE_ITEMS: string[] = 'billingModuleState.selectedInvoiceItems'.split('.');
   private applyForm: FormGroup;
-  private _receipt: Receipt;
+//  private _receipt: Receipt;
+  private _invoice: Invoice;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
               private formBuilder: FormBuilder,
               private viewContainerRef: ViewContainerRef,
               private store: Store<BillingModuleState>,
-              private actions: ReceiptActions,
+              private actions: InvoiceActions,
               private dialog: MdDialogRef<InvoiceApplicatorDialog>) {
   }
 
-  set receipt(value: Receipt) {
-    this._receipt = value;
-  }
+//  set receipt(value: Receipt) {
+//    this._receipt = value;
+//  }
+  set invoice(value: Invoice) {
+      this._invoice = value;
+    }
 
   ngOnInit(): void {
-    this.applyForm = this.formBuilder.group({});
+    //this.applyForm = this.formBuilder.group({});
+      console.log("invoice "+ this._invoice.referenceNo);
+      this.store.dispatch(this.actions.findInvoiceItems(this._invoice));
   }
 
   submit(isValid: boolean): void {
