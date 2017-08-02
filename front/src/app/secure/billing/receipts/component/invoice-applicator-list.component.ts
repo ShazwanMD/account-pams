a680581ddaed7a9bbb8eebfa88cae9c2e99553cc
hajar <hajar.ad@umk.edu.my>
2017-08-02 12:28:02 +0800

    Add selectedrow

--- front/src/app/secure/billing/receipts/component/invoice-applicator-list.component.ts
+++ front/src/app/secure/billing/receipts/component/invoice-applicator-list.component.ts
@@ -14,49 +14,34 @@ import { InvoiceActions } from "../../invoices/invoice.action";
 })
 export class InvoiceApplicatorListComponent implements OnInit {
 
-    //@Input() invoice: Invoice;
-  @Input() invoiceItems: InvoiceItem[];
-  private selectedRows: InvoiceItem[];
+    @Input() invoiceItems: InvoiceItem[];
+    
+    private selectedRows: InvoiceItem[];
 
-  private columns: any[] = [
-    {name: 'chargeCode.code', label: 'Charge Code'},
-    {name: 'chargeCode.description', label: 'Charge Code Description'},
-    {name: 'chargeCode.taxCode.code', label: 'Tax Code'},
-    {name: 'chargeCode.taxCode.taxRate', label: 'Tax Rate'},
-    {name: 'chargeCode.inclusive', label: 'Inclusive'},
-    {name: 'amount', label: 'Amount'},
-    {name: 'action', label: ''},
-  ];
+    private columns: any[] = [
+      {name: 'chargeCode.code', label: 'Charge Code'},
+      {name: 'chargeCode.description', label: 'Charge Code Description'},
+      {name: 'chargeCode.taxCode.code', label: 'Tax Code'},
+      {name: 'chargeCode.taxCode.taxRate', label: 'Tax Rate'},
+      {name: 'chargeCode.inclusive', label: 'Inclusive'},
+      {name: 'amount', label: 'Amount'},
+      {name: 'action', label: ''},
+    ];
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private store: Store<BillingModuleState>,
-              private vcf: ViewContainerRef,
-              private dialog: MdDialog,
-              private actions: InvoiceActions,
-              private snackbar: MdSnackBar) {
-  }
+    ngOnInit(): void {
+        this.selectedRows = this.invoiceItems.filter((value) => value.selected);
+    }
+    
+    selectRow(invoiceItems: InvoiceItem[]): void {
+        
+        let sum = 0;
+        for(var i=0; i<=this.invoiceItems.length; i++){
+           
+        }
+      
+    }
 
-  ngOnInit(): void {
-    this.selectedRows = this.invoiceItems.filter((value) => value.selected);
-      //this.store.dispatch(this.actions.findInvoiceItems(this.invoice));
-  }
-
-//  delete(): void {
-//    console.log('length: ' + this.selectedRows.length);
-//    for (let i: number = 0; i < this.selectedRows.length; i++) {
-//      //this.store.dispatch(this.actions.deleteInvoiceItem(this.invoice, this.selectedRows[i]));
-//    }
-//    this.selectedRows = [];
-//  }
-//
-  filter(): void {
-  }
-
-  selectRow(invoiceItem: InvoiceItem): void {
-  }
-
-  selectAllRows(invoiceItems: InvoiceItem[]): void {
-  }
+    selectAllRows(invoiceItems: InvoiceItem[]): void {
+    }
 
 }
