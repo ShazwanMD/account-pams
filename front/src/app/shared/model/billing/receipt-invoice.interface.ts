1b0be79d99125f98b137693d5c2403f6ab0dd76b
hajar <hajar.ad@umk.edu.my>
2017-08-06 16:35:40 +0800

    Add receipt Invoice interface

---
+++ front/src/app/shared/model/billing/receipt-invoice.interface.ts
@@ -0,0 +1,8 @@
+import {Receipt} from './receipt.interface';
+import {Invoice} from './invoice.interface';
+import {Document} from '../../../core/document.interface';
+
+export interface ReceiptInvoice extends Document {
+    invoice: Invoice;
+    receipt: Receipt;
+}
