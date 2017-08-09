b8419ed032b90ab7e907b602b37801d4ec152f20
Msyahrul <msyahrul@umk.edu.my>
2017-08-09 17:01:06 +0800

    Program Code OnChange

--- front/src/app/secure/setup/program-codes/program-code-list.page.ts
+++ front/src/app/secure/setup/program-codes/program-code-list.page.ts
@@ -1,62 +1,64 @@
-import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
-import {ProgramCodeCreatorDialog} from './dialog/program-code-creator.dialog';
-import {ProgramCode} from '../../../shared/model/common/program-code.interface';
-import {Component, OnInit, ViewContainerRef} from '@angular/core';
-import {Store} from '@ngrx/store';
-import {SetupActions} from '../setup.action';
-import {SetupModuleState} from '../index';
-import {Observable} from 'rxjs/Observable';
-
+import { ProgramCode } from './../../../shared/model/common/program-code.interface';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  ChangeDetectionStrategy,
+  OnInit,
+  AfterViewInit,
+  ViewContainerRef,
+} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {SetupActions} from "../setup.action";
+import {SetupModuleState} from "../index";
+import {Observable} from "rxjs/Observable";
+import {MdDialog, MdDialogConfig, MdDialogRef} from "@angular/material";
+import {ProgramCodeEditorDialog} from './dialog/program-code-editor.dialog';
 @Component({
-  selector: 'pams-program-list-page',
+  selector: 'pams-program-list.page',
   templateUrl: './program-code-list.page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProgramCodeListPage implements OnInit {
-
-  private PROGRAM_CODES = 'setupModuleState.programCodes'.split('.');
-  private creatorDialogRef: MdDialogRef<ProgramCodeCreatorDialog>;
-  private programCodes$: Observable<ProgramCode>;
-  private columns: any[] = [
-    {name: 'code', label: 'Code'},
-    {name: 'descriptionMs', label: 'DescriptionMs'},
-    {name: 'descriptionEn', label: 'DescriptionEn'},
-    {name: 'action', label: ''},
-  ];
-
+export class ProgramCodeListPage implements OnInit{
+  private PROGRAM_CODES = "setupModuleState.programCodes".split(".");
+  private programCodes$: Observable<ProgramCode[]>;
+  private creatorDialogRef: MdDialogRef<ProgramCodeEditorDialog>;
+  
   constructor(private actions: SetupActions,
               private store: Store<SetupModuleState>,
               private vcf: ViewContainerRef,
               private dialog: MdDialog) {
+              
+              
     this.programCodes$ = this.store.select(...this.PROGRAM_CODES);
   }
-
-  showDialog(): void {
-    console.log('showDialog');
+  ngOnInit(): void {
+    this.store.dispatch(this.actions.findProgramCodes());
+    this.store.dispatch(this.actions.changeTitle("Program Codes"));
+  }
+  createDialog(): void {
+    this.showDialog(null);
+  }
+  editDialog(code:ProgramCode): void {
+    this.showDialog(code);
+  }
+  delete(code: ProgramCode): void {
+    this.store.dispatch(this.actions.removeProgramCode(code))
+  }
+  
+  private showDialog(code:ProgramCode): void {
+    console.log("create");
     let config = new MdDialogConfig();
     config.viewContainerRef = this.vcf;
     config.role = 'dialog';
     config.width = '70%';
     config.height = '65%';
     config.position = {top: '0px'};
-    this.creatorDialogRef = this.dialog.open(ProgramCodeCreatorDialog, config);
-    this.creatorDialogRef.afterClosed().subscribe((res) => {
-      console.log('close dialog');
-      // load something here
+    this.creatorDialogRef = this.dialog.open(ProgramCodeEditorDialog, config);
+    if(code) this.creatorDialogRef.componentInstance.programCode = code; // set
+    this.creatorDialogRef.afterClosed().subscribe(res => {
+      console.log("close dialog");
     });
-  }
-
-  ngOnInit(): void {
-    this.store.dispatch(this.actions.findProgramCodes());
-    this.store.dispatch(this.actions.changeTitle('Program Codes'));
-  }
-
-  filter(filter: string): void {
-    console.log('filter');
-  }
-
-  delete(code: ProgramCode): void {
-    console.log('deleting code: ' + code.code);
-    console.log('deleting code: ' + code.descriptionEn);
-    this.store.dispatch(this.actions.removeProgramCode(code));
   }
 }
