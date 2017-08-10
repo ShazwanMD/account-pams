04ec1667a2c74c4b24adf90c3f188a5f0bd8b24c
hajar <hajar.ad@umk.edu.my>
2017-08-10 13:11:28 +0800

    receipt item dialog

--- front/src/app/secure/billing/receipts/dialog/invoice-receipt-creator.dialog.ts
+++ front/src/app/secure/billing/receipts/dialog/invoice-receipt-creator.dialog.ts
@@ -1,74 +1,90 @@
-import {Component, OnInit, ViewContainerRef, Input} from '@angular/core';
-import {FormBuilder, FormGroup, FormArray} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {ReceiptItem} from '../../../../shared/model/billing/receipt-item.interface';
-import {Receipt} from '../../../../shared/model/billing/receipt.interface';
-import {BillingModuleState} from '../../index';
-import {MdDialogRef} from '@angular/material';
-import {ReceiptActions} from '../receipt.action';
-import {Store} from '@ngrx/store';
+import { Component, OnInit, ViewContainerRef, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReceiptItem } from '../../../../shared/model/billing/receipt-item.interface';
+import { Receipt } from '../../../../shared/model/billing/receipt.interface';
+import { BillingModuleState } from '../../index';
+import { MdDialogRef } from '@angular/material';
+import { ReceiptActions } from '../receipt.action';
+import { Store } from '@ngrx/store';
 import { Invoice } from "../../../../shared/model/billing/invoice.interface";
 import { InvoiceItem } from "../../../../shared/model/billing/invoice-item.interface";
 import { Account } from "../../../../shared/model/account/account.interface";
 import { TdDialogService, ITdDataTableColumn } from "@covalent/core";
 import { ChargeCode } from "../../../../shared/model/account/charge-code.interface";
 
-@Component({
-  selector: 'pams-invoice-receipt-creator',
-  templateUrl: './invoice-receipt-creator.dialog.html',
-})
+@Component( {
+    selector: 'pams-invoice-receipt-creator',
+    templateUrl: './invoice-receipt-creator.dialog.html',
+} )
 
 export class InvoiceReceiptCreatorDialog implements OnInit {
 
-  private createForm: FormGroup;
-  private _receipt: Receipt;
-  private _invoice: Invoice;
-  private edit:boolean = false;
+    private createForm: FormGroup;
+    private displayForm: FormGroup;
+    private _receipt: Receipt;
+    private _invoice: Invoice;
+    private edit: boolean = false;
 
-  @Input() invoiceItem: InvoiceItem;
-
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private formBuilder: FormBuilder,
-              private viewContainerRef: ViewContainerRef,
-              private store: Store<BillingModuleState>,
-              private actions: ReceiptActions,
-              private _dialogService: TdDialogService,
-              private dialog: MdDialogRef<InvoiceReceiptCreatorDialog>) {
-  }
-
-  set receipt(value: Receipt) {
-    this._receipt = value;
-  }
-  
-  set invoice(value: Invoice) {
-      this._invoice = value;
-      this.edit = true;
-      
+    constructor( private router: Router,
+        private route: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private viewContainerRef: ViewContainerRef,
+        private store: Store<BillingModuleState>,
+        private actions: ReceiptActions,
+        private _dialogService: TdDialogService,
+        private dialog: MdDialogRef<InvoiceReceiptCreatorDialog> ) {
     }
 
-  ngOnInit(): void {
-      this.createForm = this.formBuilder.group(<ReceiptItem>{
-          id: null,
-          description: '',
-          dueAmount: 0,
-          totalAmount: 0,
-          adjustedAmount: 0,
-          appliedAmount: 0,
-          price: 0,
-          unit: 0,
-          chargeCode: this.invoiceItem.chargeCode,
-          invoice: <Invoice> {},
-    });
+    set receipt( value: Receipt ) {
+        this._receipt = value;
+    }
+
+    set invoice( value: Invoice ) {
+        this._invoice = value;
+        this.edit = true;
+
+    }
+
+    ngOnInit(): void {
+        this.displayForm = this.formBuilder.group( {
+            invoiceItem: <InvoiceItem>{},
+        } );
+        this.createForm = this.formBuilder.group( <ReceiptItem>{
+            id: undefined,
+            description: '',
+            dueAmount: 0,
+            totalAmount: 0,
+            adjustedAmount: 0,
+            appliedAmount: 0,
+            price: 0,
+            unit: 0,
+            chargeCode: <ChargeCode>{},
+            invoice: <Invoice>{},
+        } );
+
+        if ( this.edit )
+            this.createForm.patchValue( { invoice: this._invoice } );
+    }
+
+    display( invoiceItem: InvoiceItem, isValid: boolean ) {
+        console.log( "Invc Item from select component" +
+            this.displayForm.get( 'invoiceItem' ).value.chargeCode.code );
+
+        let x = this.displayForm.get( 'invoiceItem' ).value;
+
+        if ( isValid ) {
+            this.createForm.patchValue( { description: x.chargeCode.description } );
+            this.createForm.patchValue( { dueAmount: x.amount } );
+            this.createForm.patchValue( { chargeCode: x.chargeCode } );
+        }
+
+    }
+
+    submit( item: ReceiptItem, isValid: boolean ) {
       
-      if (this.edit) 
-          this.createForm.patchValue({invoice: this._invoice});
-  }
-
-  submit(item: Receipt, isValid: boolean) {
-        
-    if(isValid) this.store.dispatch(this.actions.addReceiptInvoiceItems(item, this._invoice));
-
-    this.dialog.close();
-  }
+        console.log("hantar dh kan" + this._receipt.referenceNo);
+        this.store.dispatch( this.actions.addReceiptItem( this._receipt, item ) );
+        this.dialog.close();
+    }
 }
