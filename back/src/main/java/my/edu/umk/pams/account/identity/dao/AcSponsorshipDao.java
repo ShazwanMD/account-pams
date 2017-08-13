454a584b2491998ceb45682d3d4a9b45438415c2
aidamardiana <aidamardiana>
2017-08-13 16:41:23 +0800

    Add AcSponsorshipDao

---
+++ back/src/main/java/my/edu/umk/pams/account/identity/dao/AcSponsorshipDao.java
@@ -0,0 +1,28 @@
+package my.edu.umk.pams.account.identity.dao;
+
+import java.util.List;
+
+import my.edu.umk.pams.account.core.GenericDao;
+import my.edu.umk.pams.account.identity.model.AcSponsor;
+import my.edu.umk.pams.account.identity.model.AcSponsorship;
+
+public interface AcSponsorshipDao extends GenericDao<Long, AcSponsorship> {
+	
+	// ====================================================================================================
+    // FINDER
+    // ====================================================================================================
+	
+	AcSponsorship findSponsorshipById(Long id);
+	
+	List<AcSponsorship> findSponsorships(AcSponsorship sponsorship);
+
+    List<AcSponsorship> find(String filter, Integer offset, Integer limit);
+
+    // ====================================================================================================
+    // HELPER
+    // ====================================================================================================
+
+    //Integer count(String filter);
+
+
+}
