e7e64406b52b18a208b8a8d27cb5543dec6bf66c
hajar <hajar.ad@umk.edu.my>
2017-08-13 17:13:38 +0800

    Add advance payment

--- data/src/test/resources/data/AC_RFRN_NO.sql
+++ data/src/test/resources/data/AC_RFRN_NO.sql
@@ -27,3 +27,6 @@ VALUES (nextval('SQ_AC_RFRN_NO'), 'AcCreditNote.referenceNo', 1, 'Credit Note Re
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcWaiverApplication.referenceNo', 1, 'Waiver Application Reference No Format', 1, 'WVR', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
+
+INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
+VALUES (nextval('SQ_AC_RFRN_NO'), 'AcAdvancePayment.referenceNo', 1, 'Advance Payment Reference No Format', 1, 'ADVC', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
