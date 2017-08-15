46673d716073afb575b15453483b684c555647c9
hajar <hajar.ad@umk.edu.my>
2017-08-15 22:31:05 +0800

    Add advance payment center page

---
+++ front/src/app/secure/billing/advance-payments/advance-payment-center.page.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+
+
+@Component({
+  selector: 'pams-advance-payment-center',
+  templateUrl: './advance-payment-center.page.html',
+})
+
+export class AdvancePaymentCenterPage implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+}
+
---
+++ front/src/app/secure/billing/advance-payments/advance-payment.routes.ts
@@ -0,0 +1,7 @@
+import {Routes} from '@angular/router';
+import {AdvancePaymentCenterPage} from './advance-payment-center.page';
+
+export const AdvancePaymentRoutes: Routes = [
+{path: 'billing/advance-payments', component: AdvancePaymentCenterPage},                                          
+
+];
