6e606f48aa9c7da2e45cb71bf40e05aab22cddc1
aidamardiana <aidamardiana>
2017-08-15 17:46:15 +0800

    Update sponsorship interface

--- front/src/app/shared/model/account/account-sponsorship.interface.ts
+++ front/src/app/shared/model/account/account-sponsorship.interface.ts
@@ -1,10 +1,17 @@
 import {MetaObject} from '../../../core/meta-object.interface';
 import {ChargeCode} from './charge-code.interface';
 import {AcademicSession} from './academic-session.interface';
+import { StudyMode } from "../common/study-mode.interface";
 export interface AccountSponsorship extends MetaObject {
+
+  referenceNo: string;
   sourceNo: string;
+  description: string;
   amount: number;
-  startDate: Date;
-  chargeCode: ChargeCode;
   session: AcademicSession;
+  chargeDate: Date;
+
+  // admission
+  studyMode?: StudyMode;
+  ordinal: number;
 }
