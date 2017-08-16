20f61c23b326bec90961a702929fa78cbf60dcd9
hajar <hajar.ad@umk.edu.my>
2017-08-16 17:00:20 +0800

    knockoff creator dialog

---
+++ front/src/app/secure/billing/knockoffs/dialog/knockoff-creator.dialog.ts
@@ -0,0 +1,14 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Store} from '@ngrx/store';
+import {MdDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'pams-knockoff-creator',
+  templateUrl: './knockoff-creator.dialog.html',
+})
+
+export class KnockoffCreatorDialog implements OnInit {
+
+    private createForm: FormGroup;
+}
