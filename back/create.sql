36144da521b681258f5747f3d0d6ddd30484416f
aidamardiana <aidamardiana>
2017-08-17 09:52:16 +0800

    Add column

--- back/create.sql
+++ back/create.sql
@@ -1041,6 +1041,8 @@
 
     create table AC_SPHP (
         ID int8 not null,
+        REFERENCE_NO varchar(255) not null,
+        ACCOUNT_NO varchar(255) not null,
         AMOUNT numeric(19, 2) not null,
         START_DATE timestamp,
         END_DATE timestamp,
