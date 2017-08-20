24f5ccddc4ff6006073210b4ea145ca22d7491c1
aidamardiana <aidamardiana@10.3.32.44>
2017-08-20 11:03:17 +0800

    Add file

---
+++ back/src/main/java/my/edu/umk/pams/account/identity/model/AcSponsorship.java
@@ -0,0 +1,50 @@
+package my.edu.umk.pams.account.identity.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import my.edu.umk.pams.account.core.AcMetaObject;
+import my.edu.umk.pams.account.identity.model.AcSponsor;
+import my.edu.umk.pams.account.identity.model.AcStudent;
+
+/**
+ * ?? todo: coverage
+ *
+ * @author PAMS
+ */
+public interface AcSponsorship extends AcMetaObject {
+
+    AcSponsor getSponsor();
+
+    void setSponsor(AcSponsor sponsor);
+
+    AcStudent getStudent();
+
+    void setStudent(AcStudent student);
+    
+    String getReferenceNo();
+
+    void setReferenceNo(String referenceNo);
+    
+    BigDecimal getAmount();
+
+    void setAmount(BigDecimal amount);
+
+    void setStartDate(Date startDate);
+
+    Date getStartDate();
+    
+    void setEndDate(Date endDate);
+
+    Date getEndDate();
+    
+    String getAccountNo();
+
+    void setAccountNo(String accountNo);
+    
+    Boolean getActive();
+
+	void setActive(Boolean active);
+
+}
+ 
