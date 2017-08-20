49004f59db0820f06bf8914b37e7b8f43cedf4e2
hajar <hajar.ad@umk.edu.my>
2017-08-20 17:05:58 +0800

    create knockoff dialog

--- front/src/app/secure/billing/knockoffs/dialog/knockoff-creator.dialog.ts
+++ front/src/app/secure/billing/knockoffs/dialog/knockoff-creator.dialog.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { MdDialogRef } from '@angular/material';
 import { KnockoffActions } from "../../knockoffs/knockoff.action";
-import {BillingModuleState} from '../../index';
+import { BillingModuleState } from '../../index';
 import { Knockoff } from "../../../../shared/model/billing/knockoff.interface";
 import { Invoice } from "../../../../shared/model/billing/invoice.interface";
 import { AdvancePayment } from "../../../../shared/model/billing/advance-payment.interface";
@@ -16,12 +16,19 @@ import { AdvancePayment } from "../../../../shared/model/billing/advance-payment
 export class KnockoffCreatorDialog {
 
     private createForm: FormGroup;
+    private _advancePayment: AdvancePayment;
+    private edit: boolean = false;
 
     constructor( private formBuilder: FormBuilder,
         private store: Store<BillingModuleState>,
         private actions: KnockoffActions,
         private dialog: MdDialogRef<KnockoffCreatorDialog> ) {
     }
+    
+    set advancePayment(value: AdvancePayment) {
+        this._advancePayment = value;
+        this.edit = true;
+      }
 
     ngOnInit(): void {
         this.createForm = this.formBuilder.group( <Knockoff>{
@@ -35,6 +42,13 @@ export class KnockoffCreatorDialog {
             invoice: <Invoice>{},
             advancePayment: <AdvancePayment>{},
         } );
+        
+        if (this.edit) this.createForm.patchValue({advancePayment:this._advancePayment});
+    }
+
+    save( knockoff: Knockoff, isValid: boolean ) {
+        this.store.dispatch( this.actions.saveKnockoff( knockoff ) );
+        this.dialog.close();
     }
 
 }
