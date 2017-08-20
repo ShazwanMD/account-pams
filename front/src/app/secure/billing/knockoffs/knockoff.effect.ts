88494e2b7f69946e8c74485460c24862b4d6a495
hajar <hajar.ad@umk.edu.my>
2017-08-20 12:54:31 +0800

    Create new knockoff effect

---
+++ front/src/app/secure/billing/knockoffs/knockoff.effect.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect } from '@ngrx/effects';
+import { from } from 'rxjs/observable/from';
+import { Store } from '@ngrx/store';
+import { BillingService } from '../../../../services/billing.service';
+import { BillingModuleState } from '../index';
+import { KnockoffActions } from './knockoff.action';
+
+@Injectable()
+export class KnockoffEffects {
+
+    private KNOCKOFF: string[] = 'billingModuleState.knockoff'.split( '.' );
+
+    constructor( private actions$: Actions,
+        private knockoffActions: KnockoffActions,
+        private billingService: BillingService,
+        private store$: Store<BillingModuleState> ) {
+    }
+    
+    @Effect() findKnockoffs$ = this.actions$
+    .ofType(KnockoffActions.FIND_KNOCKOFFS)
+    .map((action) => action.payload)
+    .switchMap(() => this.billingService.findKnockoffs())
+    .map((knockoff) => this.knockoffActions.findKnockoffsSuccess(knockoff));
+    
+    @Effect() findKnockoffByReferenceNo$ = this.actions$
+    .ofType(KnockoffActions.FIND_KNOCKOFF_BY_REFERENCE_NO)
+    .map((action) => action.payload)
+    .switchMap((referenceNo) => this.billingService.findKnockoffByReferenceNo(referenceNo))
+    .map((knockoff) => this.knockoffActions.findKnockoffByReferenceNoSuccess(knockoff));
+    
+    @Effect() saveKnockoff$ = this.actions$
+    .ofType(KnockoffActions.SAVE_KNOCKOFF)
+    .map((action) => action.payload)
+    .switchMap((knockoff) => this.billingService.saveKnockoff(knockoff))
+    .map((knockoff) => this.knockoffActions.saveKnockoffSuccess(knockoff));
+}
