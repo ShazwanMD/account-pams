554a82738b5b41f51523885d263b730507a566f3
aidamardiana <aidamardiana>
2017-08-22 17:02:07 +0800

    Update interface AccountSponsorship - add sponsor

--- front/src/app/shared/model/account/account-sponsorship.interface.ts
+++ front/src/app/shared/model/account/account-sponsorship.interface.ts
@@ -2,6 +2,8 @@ import {MetaObject} from '../../../core/meta-object.interface';
 import {ChargeCode} from './charge-code.interface';
 import {AcademicSession} from './academic-session.interface';
 import { StudyMode } from "../common/study-mode.interface";
+import { Actor } from "../identity/actor.interface";
+import { Sponsor } from "../identity/sponsor.interface";
 export interface AccountSponsorship extends MetaObject {
 
   referenceNo: string;
@@ -11,6 +13,7 @@ export interface AccountSponsorship extends MetaObject {
   session: AcademicSession;
   startDate: Date;
   endDate: Date;
+  sponsor: Sponsor;
 
   // admission
   studyMode?: StudyMode;
