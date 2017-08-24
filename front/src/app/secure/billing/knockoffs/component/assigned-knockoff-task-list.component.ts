35272b3b5b960f15b87519a44e1f5073ced72e1b
hajar <hajar.ad@umk.edu.my>
2017-08-24 15:17:52 +0800

    Add asigned knockoff task

---
+++ front/src/app/secure/billing/knockoffs/component/assigned-knockoff-task-list.component.ts
@@ -0,0 +1,36 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {KnockoffTask} from '../../../../shared/model/billing/knockoff-task.interface';
+import {MdSnackBar, MdSnackBarConfig} from '@angular/material';
+
+@Component({
+  selector: 'pams-assigned-knockoff-task-list',
+  templateUrl: './assigned-knockoff-task-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AssignedKnockoffTaskListComponent {
+
+    private columns: any[] = [
+                              {name: 'issuedDate', label: 'Date'},                        
+                              {name: 'referenceNo', label: 'Reference No'},
+                              {name: 'invoice.referenceNo', label: 'Invoice'},
+                              {name: 'description', label: 'Description'},
+                              {name: 'amount', label: 'Total Amount'},
+                              {name: 'action', label: ''},
+                            ];
+
+  @Input() knockoffTask: KnockoffTask[];
+  @Output() view = new EventEmitter<KnockoffTask>();
+
+  constructor(private snackBar: MdSnackBar) {
+  }
+
+  viewTask(task: KnockoffTask): void {
+    console.log('Emitting task');
+    let config: MdSnackBarConfig = new MdSnackBarConfig();
+    config.duration = 2000;
+    let snackBarRef = this.snackBar.open('Viewing knockoff', 'OK', config);
+    snackBarRef.afterDismissed().subscribe(() => {
+      this.view.emit(task);
+    });
+  }
+}
