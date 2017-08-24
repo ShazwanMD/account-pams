abf625e743d502462d912f899b937e6e957f64d3
hajar <hajar.ad@umk.edu.my>
2017-08-24 15:20:35 +0800

    Add knockoff action & effect

--- front/src/app/secure/billing/knockoffs/knockoff.action.ts
+++ front/src/app/secure/billing/knockoffs/knockoff.action.ts
@@ -39,21 +39,184 @@ export class KnockoffActions {
       };
     }
     
-    static SAVE_KNOCKOFF = '[Knockoff] Save Knockoff';
+    static FIND_KNOCKOFF_TASK_BY_TASK_ID = '[Knockoff] Find Knockoff Task By Task Id';
 
-    saveKnockoff(knockoff, payment): Action {
+    findKnockoffTaskByTaskId(taskId): Action {
+      console.log('findKnockoffTaskByTaskId');
       return {
-        type: KnockoffActions.SAVE_KNOCKOFF,
-        payload: {knockoff: knockoff, payment: payment},
+        type: KnockoffActions.FIND_KNOCKOFF_TASK_BY_TASK_ID,
+        payload: taskId,
       };
     }
 
-    static SAVE_KNOCKOFF_SUCCESS = '[Knockoff] Save Knockoff Success';
+    static FIND_KNOCKOFF_TASK_BY_TASK_ID_SUCCESS = '[Knockoff] Find Knockoff Task By Task Id Success';
 
-    saveKnockoffSuccess(knockoff): Action {
+    findKnockoffTaskByTaskIdSuccess(task): Action {
+      console.log('findKnockoffTaskByTaskIdSuccess');
       return {
-        type: KnockoffActions.SAVE_KNOCKOFF_SUCCESS,
-        payload: knockoff
+        type: KnockoffActions.FIND_KNOCKOFF_TASK_BY_TASK_ID_SUCCESS,
+        payload: task,
       };
     }
+    
+    static START_KNOCKOFF_TASK = '[Knockoff] Start Knockoff Task';
+    startKnockoffTask(knockoff, referenceNo): Action {
+        console.log("kncokoff dlm action: " + knockoff);
+      return {
+        type: KnockoffActions.START_KNOCKOFF_TASK,
+        payload: {knockoff, referenceNo}
+      };
+    }
+
+    static START_KNOCKOFF_TASK_SUCCESS = '[Knockoff] Start Knockoff Task Success';
+
+    startKnockoffTaskSuccess(referenceNo): Action {
+      return {
+        type: KnockoffActions.START_KNOCKOFF_TASK_SUCCESS,
+        payload: referenceNo,
+      };
+    }
+
+    static COMPLETE_KNOCKOFF_TASK = '[Knockoff] Complete Knockoff Task';
+
+    completeKnockoffTask(knockoff): Action {
+      return {
+        type: KnockoffActions.COMPLETE_KNOCKOFF_TASK,
+        payload: knockoff,
+      };
+    }
+
+    static COMPLETE_KNOCKOFF_TASK_SUCCESS = '[Knockoff] Complete Knockoff Task Success';
+
+    completeKnockoffTaskSuccess(message): Action {
+      return {
+        type: KnockoffActions.COMPLETE_KNOCKOFF_TASK_SUCCESS,
+        payload: message,
+      };
+    }
+
+    static CLAIM_KNOCKOFF_TASK = '[Knockoff] Assign Knockoff Task';
+
+    claimKnockoffTask(knockoff): Action {
+      return {
+        type: KnockoffActions.CLAIM_KNOCKOFF_TASK,
+        payload: knockoff,
+      };
+    }
+
+    static CLAIM_KNOCKOFF_TASK_SUCCESS = '[Knockoff] Assign Knockoff Task Success';
+
+    claimKnockoffTaskSuccess(message): Action {
+      return {
+        type: KnockoffActions.CLAIM_KNOCKOFF_TASK_SUCCESS,
+        payload: message,
+      };
+    }
+
+    static RELEASE_KNOCKOFF_TASK = '[Knockoff] Release Knockoff Task';
+
+    releaseKnockoffTask(knockoff): Action {
+      return {
+        type: KnockoffActions.RELEASE_KNOCKOFF_TASK,
+        payload: knockoff,
+      };
+    }
+
+    static RELEASE_KNOCKOFF_TASK_SUCCESS = '[Knockoff] Release Knockoff Task Success';
+
+    releaseKnockoffTaskSuccess(message): Action {
+      return {
+        type: KnockoffActions.RELEASE_KNOCKOFF_TASK_SUCCESS,
+        payload: message,
+      };
+    }
+    
+    static FIND_COMPLETED_KNOCKOFFS = '[Knockoff] Find Completed Knockoff';
+
+    findCompletedKnockoffs(): Action {
+      return {
+        type: KnockoffActions.FIND_COMPLETED_KNOCKOFFS,
+      };
+    }
+
+    static FIND_COMPLETED_KNOCKOFFS_SUCCESS = '[Knockoff] Find Completed Knockoff Success';
+
+    findCompletedKnockoffsSuccess(knockoffs): Action {
+      console.log('findCompletedKnockoffsSuccess');
+      return {
+        type: KnockoffActions.FIND_COMPLETED_KNOCKOFFS_SUCCESS,
+        payload: knockoffs,
+      };
+    }
+
+    static FIND_ARCHIVED_KNOCKOFFS = '[Knockoff] Find Archived Knockoff';
+
+    findArchivedknockoffs(): Action {
+      return {
+        type: KnockoffActions.FIND_ARCHIVED_KNOCKOFFS,
+      };
+    }
+
+    static FIND_ARCHIVED_KNOCKOFFS_SUCCESS = '[Knockoff] Find Archived Knockoff Success';
+
+    findArchivedknockoffsSuccess(knockoffs): Action {
+      console.log('findArchivedInvoicesSuccess');
+      return {
+        type: KnockoffActions.FIND_ARCHIVED_KNOCKOFFS_SUCCESS,
+        payload: knockoffs,
+      };
+    }
+
+    static FIND_ASSIGNED_KNOCKOFF_TASKS = '[Knockoff] Find Assigned Knockoff Tasks';
+
+    findAssignedKnockoffTasks(): Action {
+      return {
+        type: KnockoffActions.FIND_ASSIGNED_KNOCKOFF_TASKS,
+      };
+    }
+
+    static FIND_ASSIGNED_KNOCKOFF_TASKS_SUCCESS = '[Knockoff] Find Assigned Knockoff Tasks Success';
+
+    findAssignedKnockoffTasksSuccess(tasks): Action {
+      console.log('findAssignedKnockoffTasksSuccess');
+      return {
+        type: KnockoffActions.FIND_ASSIGNED_KNOCKOFF_TASKS_SUCCESS,
+        payload: tasks,
+      };
+    }
+
+    static FIND_POOLED_KNOCKOFF_TASKS = '[Knockoff] Find Pooled Knockoff Tasks';
+
+    findPooledKnockoffTasks(): Action {
+      return {
+        type: KnockoffActions.FIND_POOLED_KNOCKOFF_TASKS,
+      };
+    }
+
+    static FIND_POOLED_KNOCKOFF_TASKS_SUCCESS = '[Knockoff] Find Pooled Knockoff Tasks Success';
+
+    findPooledKnockoffTasksSuccess(tasks): Action {
+      console.log('findAssignedInvoiceTasksSuccess');
+      return {
+        type: KnockoffActions.FIND_POOLED_KNOCKOFF_TASKS_SUCCESS,
+        payload: tasks,
+      };
+    }
+//    static SAVE_KNOCKOFF = '[Knockoff] Save Knockoff';
+//
+//    saveKnockoff(knockoff, payment): Action {
+//      return {
+//        type: KnockoffActions.SAVE_KNOCKOFF,
+//        payload: {knockoff: knockoff, payment: payment},
+//      };
+//    }
+//
+//    static SAVE_KNOCKOFF_SUCCESS = '[Knockoff] Save Knockoff Success';
+//
+//    saveKnockoffSuccess(knockoff): Action {
+//      return {
+//        type: KnockoffActions.SAVE_KNOCKOFF_SUCCESS,
+//        payload: knockoff
+//      };
+//    }
 }
--- front/src/app/secure/billing/knockoffs/knockoff.effect.ts
+++ front/src/app/secure/billing/knockoffs/knockoff.effect.ts
@@ -10,28 +10,94 @@ import { KnockoffActions } from './knockoff.action';
 export class KnockoffEffects {
 
     private KNOCKOFF: string[] = 'billingModuleState.knockoff'.split( '.' );
+    private KNOCKOFF_TASK: string[] = 'billingModuleState.knockoffTask'.split('.');
 
     constructor( private actions$: Actions,
         private knockoffActions: KnockoffActions,
         private billingService: BillingService,
         private store$: Store<BillingModuleState> ) {
     }
-    
+
     @Effect() findKnockoffs$ = this.actions$
-    .ofType(KnockoffActions.FIND_KNOCKOFFS)
-    .map((action) => action.payload)
-    .switchMap(() => this.billingService.findKnockoffs())
-    .map((knockoff) => this.knockoffActions.findKnockoffsSuccess(knockoff));
-    
+        .ofType( KnockoffActions.FIND_KNOCKOFFS )
+        .map(( action ) => action.payload )
+        .switchMap(() => this.billingService.findKnockoffs() )
+        .map(( knockoff ) => this.knockoffActions.findKnockoffsSuccess( knockoff ) );
+
     @Effect() findKnockoffByReferenceNo$ = this.actions$
-    .ofType(KnockoffActions.FIND_KNOCKOFF_BY_REFERENCE_NO)
-    .map((action) => action.payload)
-    .switchMap((referenceNo) => this.billingService.findKnockoffByReferenceNo(referenceNo))
-    .map((knockoff) => this.knockoffActions.findKnockoffByReferenceNoSuccess(knockoff));
+        .ofType( KnockoffActions.FIND_KNOCKOFF_BY_REFERENCE_NO )
+        .map(( action ) => action.payload )
+        .switchMap(( referenceNo ) => this.billingService.findKnockoffByReferenceNo( referenceNo ) )
+        .map(( knockoff ) => this.knockoffActions.findKnockoffByReferenceNoSuccess( knockoff ) );
+
+    @Effect() findCompletedKnockoffs$ = this.actions$
+        .ofType( KnockoffActions.FIND_COMPLETED_KNOCKOFFS )
+        .switchMap(() => this.billingService.findCompletedKnockoffs() )
+        .map(( knockoffs ) => this.knockoffActions.findCompletedKnockoffsSuccess( knockoffs ) );
+
+    @Effect() findArchivedknockoffs$ = this.actions$
+        .ofType( KnockoffActions.FIND_ARCHIVED_KNOCKOFFS )
+        .switchMap(() => this.billingService.findArchivedknockoffs() )
+        .map(( knockoffs ) => this.knockoffActions.findArchivedknockoffsSuccess( knockoffs ) );
+
+    @Effect() findAssignedKnockoffTasks$ = this.actions$
+        .ofType( KnockoffActions.FIND_ASSIGNED_KNOCKOFF_TASKS )
+        .switchMap(() => this.billingService.findAssignedKnockoffTasks() )
+        .map(( tasks ) => this.knockoffActions.findAssignedKnockoffTasksSuccess(tasks));
+
+    @Effect() findPooledKnockoffTasks$ = this.actions$
+        .ofType( KnockoffActions.FIND_POOLED_KNOCKOFF_TASKS )
+        .switchMap(() => this.billingService.findPooledKnockoffTasks() )
+        .map(( tasks ) => this.knockoffActions.findPooledKnockoffTasksSuccess(tasks) );
+
+    @Effect() findKnockoffTaskByTaskId$ = this.actions$
+        .ofType( KnockoffActions.FIND_KNOCKOFF_TASK_BY_TASK_ID )
+        .map(( action ) => action.payload )
+        .switchMap(( taskId ) => this.billingService.findKnockoffTaskByTaskId(taskId))
+        .map(( task ) => this.knockoffActions.findKnockoffTaskByTaskIdSuccess(task) );
     
-    @Effect() saveKnockoff$ = this.actions$
-    .ofType(KnockoffActions.SAVE_KNOCKOFF)
+    @Effect() startKnockoffTask$ = this.actions$
+    .ofType(KnockoffActions.START_KNOCKOFF_TASK)
     .map((action) => action.payload)
-    .switchMap((payload) => this.billingService.saveKnockoff(payload.knockoff, payload.payment))
-    .map((knockoff) => this.knockoffActions.saveKnockoffSuccess(knockoff));
+    .switchMap((payload) => this.billingService.startKnockoffTask(payload.knockoff, payload.referenceNo))
+    .map((referenceNo) => this.knockoffActions.startKnockoffTaskSuccess(referenceNo))
+    .mergeMap((action) => from([action,
+        this.knockoffActions.findAssignedKnockoffTasks(),
+        this.knockoffActions.findPooledKnockoffTasks(),
+      ],
+    ));
+
+  @Effect() completeKnockoffTask$ = this.actions$
+    .ofType(KnockoffActions.COMPLETE_KNOCKOFF_TASK)
+    .map((action) => action.payload)
+    .switchMap((knockoffTask) => this.billingService.completeKnockoffTask(knockoffTask))
+    .map((message) => this.knockoffActions.completeKnockoffTaskSuccess(message))
+    .mergeMap((action) => from([action,
+        this.knockoffActions.findAssignedKnockoffTasks(),
+        this.knockoffActions.findPooledKnockoffTasks(),
+        this.knockoffActions.findArchivedknockoffs(),
+      ],
+    ));
+
+  @Effect() claimKnockoffTask$ = this.actions$
+    .ofType(KnockoffActions.CLAIM_KNOCKOFF_TASK)
+    .map((action) => action.payload)
+    .switchMap((knockoffTask) => this.billingService.claimKnockoffTask(knockoffTask))
+    .map((message) => this.knockoffActions.claimKnockoffTaskSuccess(message))
+    .mergeMap((action) => from([action,
+                                this.knockoffActions.findAssignedKnockoffTasks(),
+                                this.knockoffActions.findPooledKnockoffTasks(),
+      ],
+    ));
+
+  @Effect() releaseKnockoffTask$ = this.actions$
+    .ofType(KnockoffActions.RELEASE_KNOCKOFF_TASK)
+    .map((action) => action.payload)
+    .switchMap((knockoffTask) => this.billingService.releaseKnockoffTask(knockoffTask))
+    .map((message) => this.knockoffActions.releaseKnockoffTaskSuccess(message))
+    .mergeMap((action) => from([action,
+                                this.knockoffActions.findAssignedKnockoffTasks(),
+                                this.knockoffActions.findPooledKnockoffTasks(),
+      ],
+    ));
 }
