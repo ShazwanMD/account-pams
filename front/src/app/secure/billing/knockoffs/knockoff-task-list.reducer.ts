3a522065f66036d4e65ba2a1f5a9aba3f9887cef
hajar <hajar.ad@umk.edu.my>
2017-08-24 15:12:07 +0800

    Knockoff reducer

---
+++ front/src/app/secure/billing/knockoffs/knockoff-task-list.reducer.ts
@@ -0,0 +1,29 @@
+import {Action} from '@ngrx/store';
+import {KnockoffActions} from './knockoff.action';
+import {KnockoffTask} from '../../../shared/model/billing/knockoff-task.interface';
+
+export type KnockoffTaskListState = KnockoffTask[];
+
+const initialState: KnockoffTaskListState = <KnockoffTask[]>[];
+
+export function assignedKnockoffTaskListReducer(state = initialState, action: Action): KnockoffTaskListState {
+  switch (action.type) {
+    case KnockoffActions.FIND_ASSIGNED_KNOCKOFF_TASKS_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export function pooledKnockoffTaskListReducer(state = initialState, action: Action): KnockoffTaskListState {
+  switch (action.type) {
+    case KnockoffActions.FIND_POOLED_KNOCKOFF_TASKS_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
---
+++ front/src/app/secure/billing/knockoffs/knockoff-task.reducer.ts
@@ -0,0 +1,18 @@
+import {Action} from '@ngrx/store';
+import {KnockoffActions} from './knockoff.action';
+import {KnockoffTask} from '../../../shared/model/billing/knockoff-task.interface';
+
+export type KnockoffTaskState = KnockoffTask;
+
+const initialState: KnockoffTaskState = <KnockoffTaskState>{};
+
+export function knockoffTaskReducer(state = initialState, action: Action): KnockoffTaskState {
+  switch (action.type) {
+    case KnockoffActions.FIND_KNOCKOFF_TASK_BY_TASK_ID_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
