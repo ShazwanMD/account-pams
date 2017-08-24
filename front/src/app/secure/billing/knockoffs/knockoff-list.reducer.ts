ba91a83529e92fc702e25f6e3f8e9d7d7ed6f029
hajar <hajar.ad@umk.edu.my>
2017-08-24 15:19:57 +0800

    update list reducer

--- front/src/app/secure/billing/knockoffs/knockoff-list.reducer.ts
+++ front/src/app/secure/billing/knockoffs/knockoff-list.reducer.ts
@@ -13,19 +13,22 @@ export function knockoffListReducer(state = initialState, action: Action): Knock
     case KnockoffActions.FIND_KNOCKOFFS_SUCCESS: {
       return action.payload;
     }
-    case KnockoffActions.SAVE_KNOCKOFF_SUCCESS: {
-      let index = _.findIndex(state, {id: action.payload.id});
-      if (index >= 0) {
-        return [
-          ...state.slice(0, index),
-          action.payload,
-          ...state.slice(index + 1),
-        ];
+    case KnockoffActions.FIND_COMPLETED_KNOCKOFFS_SUCCESS: {
+        return action.payload;
       }
-      return state;
-    }
     default: {
       return state;
     }
   }
 }
+
+export function archivedKnockoffListReducer(state = initialState, action: Action): KnockoffListState {
+    switch (action.type) {
+      case KnockoffActions.FIND_ARCHIVED_KNOCKOFFS_SUCCESS: {
+        return action.payload;
+      }
+      default: {
+        return state;
+      }
+    }
+  }
