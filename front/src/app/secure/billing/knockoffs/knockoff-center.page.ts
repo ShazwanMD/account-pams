b41f08369c6217c6e81fce7b1ea2f1d27607f563
hajar <hajar.ad@umk.edu.my>
2017-08-26 00:43:33 +0800

    Update center page

--- front/src/app/secure/billing/knockoffs/knockoff-center.page.ts
+++ front/src/app/secure/billing/knockoffs/knockoff-center.page.ts
@@ -7,7 +7,7 @@ import {KnockoffActions} from './knockoff.action';
 import {KnockoffTask} from '../../../shared/model/billing/knockoff-task.interface';
 import {BillingModuleState} from '../index';
 import {MdDialog, MdDialogConfig, MdDialogRef} from '@angular/material';
-import {Invoice} from '../../../shared/model/billing/invoice.interface';
+import { Knockoff } from "../../../shared/model/billing/knockoff.interface";
 
 @Component({
   selector: 'pams-knockoff-center',
@@ -17,13 +17,12 @@ import {Invoice} from '../../../shared/model/billing/invoice.interface';
 export class KnockoffCenterPage implements OnInit {
 
   private ASSIGNED_KNOCKOFF_TASKS = 'billingModuleState.assignedKnockoffTasks'.split('.');
-//  private POOLED_INVOICE_TASKS = 'billingModuleState.pooledInvoiceTasks'.split('.');
-//  private ARCHIVED_INVOICES = 'billingModuleState.archivedInvoices'.split('.');
+  private POOLED_KNOCKOFF_TASKS = 'billingModuleState.pooledKnockoffTasks'.split('.');
+  private ARCHIVED_KNOCKOFF = 'billingModuleState.archivedKnockoffs'.split('.');
   private assignedKnockoffTasks$: Observable<KnockoffTask[]>;
-//  private pooledInvoiceTasks$: Observable<InvoiceTask[]>;
-//  private archivedInvoices$: Observable<Invoice[]>;
-//  private creatorDialogRef: MdDialogRef<InvoiceTaskCreatorDialog>;
-
+  private pooledKnockoffTasks$: Observable<KnockoffTask[]>;
+  private archivedKnockoffs$: Observable<Knockoff[]>;
+  
   constructor(private router: Router,
               private route: ActivatedRoute,
               private actions: KnockoffActions,
@@ -31,17 +30,17 @@ export class KnockoffCenterPage implements OnInit {
               private vcf: ViewContainerRef,
               private dialog: MdDialog) {
     this.assignedKnockoffTasks$ = this.store.select(...this.ASSIGNED_KNOCKOFF_TASKS);
-//    this.pooledInvoiceTasks$ = this.store.select(...this.POOLED_INVOICE_TASKS);
-//    this.archivedInvoices$ = this.store.select(...this.ARCHIVED_INVOICES);
+    this.pooledKnockoffTasks$ = this.store.select(...this.POOLED_KNOCKOFF_TASKS);
+    this.archivedKnockoffs$ = this.store.select(...this.ARCHIVED_KNOCKOFF);
   }
 
   goBack(route: string): void {
-    this.router.navigate(['/invoices']);
+    this.router.navigate(['/knokoffs']);
   }
 
   claimTask(task: KnockoffTask) {
     console.log('invoice: ' + task.taskId);
-    this.store.dispatch(this.actions.claimKnockoffTask(task));
+    //this.store.dispatch(this.actions.claimKnockoffTask(task));
   }
 
   viewTask(task: KnockoffTask) {
@@ -72,8 +71,8 @@ export class KnockoffCenterPage implements OnInit {
   ngOnInit(): void {
     console.log('find assigned knockoff tasks');
     this.store.dispatch(this.actions.findAssignedKnockoffTasks());
-//    this.store.dispatch(this.actions.findPooledInvoiceTasks());
-//    this.store.dispatch(this.actions.findArchivedInvoices());
+    this.store.dispatch(this.actions.findPooledKnockoffTasks());
+    this.store.dispatch(this.actions.findArchivedknockoffs());
   }
 }
 
