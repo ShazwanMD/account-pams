dccc9a7926751ef7103ba5226968795ac5c9e9c2
nurazieta <azieta.ma@umk.edu.my>
2017-08-27 18:04:44 +0800

    add RefundPaymentTask

---
+++ back/src/main/java/my/edu/umk/pams/account/web/module/billing/vo/RefundPaymentTask.java
@@ -0,0 +1,73 @@
+package my.edu.umk.pams.account.web.module.billing.vo;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import my.edu.umk.pams.account.web.module.core.vo.Task;
+
+public class RefundPaymentTask extends Task {
+
+	private RefundPayment refundPayment;
+	private Date issuedDate;
+	private BigDecimal amount;
+	private String description;
+	private String paymentNo;
+
+	public RefundPayment getRefundPayment() {
+		return refundPayment;
+	}
+
+	public void setRefundPayment(RefundPayment refundPayment) {
+		this.refundPayment = refundPayment;
+	}
+
+	public Date getIssuedDate() {
+		return issuedDate;
+	}
+
+	public void setIssuedDate(Date issuedDate) {
+		this.issuedDate = issuedDate;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getPaymentNo() {
+		return paymentNo;
+	}
+
+	public void setPaymentNo(String paymentNo) {
+		this.paymentNo = paymentNo;
+	}
+	
+	
+    @JsonCreator
+    public static RefundPaymentTask create(String jsonString) {
+    	RefundPaymentTask o = null;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            o = mapper.readValue(jsonString, RefundPaymentTask.class);
+        } catch (IOException e) {
+            // handle
+        }
+        return o;
+    }
+
+}
