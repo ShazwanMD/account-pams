fb94866854afb60e36a8a3750914ae5f628395e9
nurazieta <azieta.ma@umk.edu.my>
2017-08-27 18:02:36 +0800

    add RefundRouter

---
+++ back/src/main/java/my/edu/umk/pams/account/billing/workflow/router/RefundRouter.java
@@ -0,0 +1,51 @@
+package my.edu.umk.pams.account.billing.workflow.router;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.Validate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import my.edu.umk.pams.account.billing.model.AcReceipt;
+import my.edu.umk.pams.account.billing.model.AcRefundPayment;
+import my.edu.umk.pams.account.billing.service.BillingService;
+import my.edu.umk.pams.account.common.router.RouterServiceSupport;
+
+@Component("refundRouter")
+public class RefundRouter extends RouterServiceSupport{
+
+    private static final Logger LOG = LoggerFactory.getLogger(RefundRouter.class);
+
+    @Autowired
+    private BillingService billingService;
+
+    public List<String> findRegistererCandidates(Long refundId) {
+        Validate.notNull(refundId, "Id must not be null");
+
+        String candidate = null;
+        AcRefundPayment refundPayment = billingService.findRefundPaymentById(refundId);
+        candidate = "GRP_ADM";
+
+        // publish access event
+        // publishAccessEvent(receipt, identityService.findGroupByName(candidate), AcPermission.VIEW);
+
+        return Arrays.asList(candidate);
+    }
+
+    public List<String> findVerifierCandidates(Long refundId) {
+        Validate.notNull(refundId, "Id must not be null");
+
+        String candidate = null;
+
+        AcRefundPayment refundPayment = billingService.findRefundPaymentById(refundId);
+        candidate = "GRP_ADM";
+
+        // publish access event
+        // publishAccessEvent(receipt, identityService.findGroupByName(candidate), AcPermission.VIEW);
+
+        return Arrays.asList(candidate);
+    }
+}
