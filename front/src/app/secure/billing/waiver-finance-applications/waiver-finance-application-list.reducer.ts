b42be6ccfa7a50f93ec64ec830ead859f23d1ea9
aidamardiana <aidamardiana>
2017-08-27 17:58:32 +0800

    Add WaiverFinanceApplicationListReducer

---
+++ front/src/app/secure/billing/waiver-finance-applications/waiver-finance-application-list.reducer.ts
@@ -0,0 +1,30 @@
+import {Action} from '@ngrx/store';
+import {WaiverApplication} from '../../../shared/model/financialaid/waiver-application.interface';
+import { WaiverFinanceApplicationActions } from "./waiver-finance-application.action";
+import { WaiverFinanceApplication } from "../../../shared/model/billing/waiver-finance-application.interface";
+
+export type WaiverFinanceApplicationListState = WaiverFinanceApplication[];
+
+const initialState: WaiverFinanceApplicationListState = <WaiverFinanceApplication[]>[];
+
+export function archivedWaiverFinanceApplicationListReducer(state = initialState, action: Action): WaiverFinanceApplicationListState {
+  switch (action.type) {
+    case WaiverFinanceApplicationActions.FIND_ARCHIVED_WAIVER_FINANCE_APPLICATIONS_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export function waiverFinanceApplicationListReducer(state = initialState, action: Action): WaiverFinanceApplicationListState {
+  switch (action.type) {
+    case WaiverFinanceApplicationActions.FIND_COMPLETED_WAIVER_FINANCE_APPLICATIONS_SUCCESS: {
+      return action.payload;
+    }
+    default: {
+      return state;
+    }
+  }
+}
