2e8f70d091b92d41467203fa7159ff580307321b
aidamardiana <aidamardiana>
2017-08-27 18:02:48 +0800

    Add WaiverFinanceApplication interface

---
+++ front/src/app/shared/model/billing/waiver-finance-application.interface.ts
@@ -0,0 +1,17 @@
+import {Document} from '../../../core/document.interface';
+import {AcademicSession} from '../account/academic-session.interface';
+import {Account} from '../account/account.interface';
+export interface WaiverFinanceApplication extends Document {
+  referenceNo: string;
+  sourceNo: string;
+  description: string;
+  reason: string;
+  memo: string;
+  balance: number;
+  effectiveBalance: number;
+  waivedAmount: number;
+  gracedAmount: number;
+  account: Account;
+  academicSession: AcademicSession;
+}
+
