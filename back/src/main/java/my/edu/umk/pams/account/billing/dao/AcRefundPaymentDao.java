07f582be2b9f6420c61e67b79440b1e69f81648f
nurazieta <azieta.ma@umk.edu.my>
2017-08-28 12:37:54 +0800

    add List<AcRefundPayment>

--- back/src/main/java/my/edu/umk/pams/account/billing/dao/AcRefundPaymentDao.java
+++ back/src/main/java/my/edu/umk/pams/account/billing/dao/AcRefundPaymentDao.java
@@ -1,11 +1,21 @@
 package my.edu.umk.pams.account.billing.dao;
 
+import java.util.List;
+
+import my.edu.umk.pams.account.billing.model.AcKnockoff;
 import my.edu.umk.pams.account.billing.model.AcRefundPayment;
+import my.edu.umk.pams.account.core.AcFlowState;
 import my.edu.umk.pams.account.core.GenericDao;
 import my.edu.umk.pams.account.identity.model.AcUser;
 
 public interface AcRefundPaymentDao extends GenericDao<Long, AcRefundPayment> {
 
+	List<AcRefundPayment> find(String filter, Integer offset, Integer limit);
+	
+    List<AcRefundPayment> findByFlowState(AcFlowState flowState);
+
+    List<AcRefundPayment> findByFlowStates(AcFlowState... flowStates);
+	
 	AcRefundPayment findByReferenceNo(String referenceNo);
 	
 	boolean hasRefundPayment(AcRefundPayment refundPayment);
