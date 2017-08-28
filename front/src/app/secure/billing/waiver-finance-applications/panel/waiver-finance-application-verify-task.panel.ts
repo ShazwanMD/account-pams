cca31e7692c51052e61251c76d636f3be3fb0e23
aidamardiana <aidamardiana>
2017-08-28 16:46:15 +0800

    Add WaiverFinanceApplicationVerifyTaskPanel

---
+++ front/src/app/secure/billing/waiver-finance-applications/panel/waiver-finance-application-verify-task.panel.ts
@@ -0,0 +1,39 @@
+import { BillingModuleState } from './../../index';
+import {Component, Input, OnInit, ViewContainerRef} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MdDialog, MdSnackBar} from '@angular/material';
+import {Store} from '@ngrx/store';
+import {WaiverApplicationTask} from '../../../../shared/model/financialaid/waiver-application-task.interface';
+import { WaiverFinanceApplicationActions } from "../waiver-finance-application.action";
+import { WaiverFinanceApplicationTask } from "../../../../shared/model/billing/waiver-finance-application-task.interface";
+
+@Component({
+  selector: 'pams-waiver-finance-application-verify-task',
+  templateUrl: './waiver-finance-application-verify-task.panel.html',
+})
+
+export class WaiverFinanceApplicationVerifyTaskPanel implements OnInit {
+
+  @Input() waiverFinanceApplicationTask: WaiverFinanceApplicationTask;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private viewContainerRef: ViewContainerRef,
+              private actions: WaiverFinanceApplicationActions,
+              private store: Store<BillingModuleState>,
+              private dialog: MdDialog,
+              private snackBar: MdSnackBar) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.store.dispatch(this.actions.completeWaiverFinanceApplicationTask(this.waiverFinanceApplicationTask));
+    this.goBack();
+  }
+
+  goBack(): void {
+    this.router.navigate(['/secure/billing/waiver-finance-applications']);
+  }
+}
