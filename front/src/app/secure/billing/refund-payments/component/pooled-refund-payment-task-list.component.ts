f91f9135b8686897792a4e475b11c8992aa84484
nurazieta <azieta.ma@umk.edu.my>
2017-08-30 16:25:43 +0800

    add PooledRefundPaymentTaskListComponent

---
+++ front/src/app/secure/billing/refund-payments/component/pooled-refund-payment-task-list.component.ts
@@ -0,0 +1,35 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {RefundPaymentTask} from '../../../../shared/model/billing/refund-payment-task.interface';
+import {MdSnackBar, MdSnackBarConfig, MdSnackBarRef, SimpleSnackBar} from '@angular/material';
+
+@Component({
+  selector: 'pams-pooled-refund-payment-task-list',
+  templateUrl: './pooled-refund-payment-task-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class PooledRefundPaymentTaskListComponent {
+
+    private columns: any[] = [
+                              {name: 'issuedDate', label: 'Date'},                        
+                              {name: 'referenceNo', label: 'Reference No'},
+                              {name: 'invoice.referenceNo', label: 'Invoice'},
+                              {name: 'description', label: 'Description'},
+                              {name: 'amount', label: 'Total Amount'},
+                              {name: 'action', label: ''},
+                            ];
+
+  @Input() refundPaymentTask: RefundPaymentTask[];
+  @Output() claim: EventEmitter<RefundPaymentTask> = new EventEmitter<RefundPaymentTask>();
+
+  constructor(private snackBar: MdSnackBar) {
+  }
+
+  claimTask(task: RefundPaymentTask): void {
+      console.log('Emitting task');
+      let snackBarRef: MdSnackBarRef<SimpleSnackBar> = this.snackBar.open('Claiming invoice');
+      snackBarRef.dismiss();
+      snackBarRef.afterDismissed().subscribe(() => {
+        this.claim.emit(task);
+      });
+    }
+}
