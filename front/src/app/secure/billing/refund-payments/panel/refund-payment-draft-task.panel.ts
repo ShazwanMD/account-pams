0f5f754f2e41444b5b2bac0616658f9e70524481
nurazieta <azieta.ma@umk.edu.my>
2017-08-30 16:21:23 +0800

    add RefundPaymentDraftTaskPanel

---
+++ front/src/app/secure/billing/refund-payments/panel/refund-payment-draft-task.panel.ts
@@ -0,0 +1,43 @@
+import {Component, Input, OnInit, ViewContainerRef} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {MdDialog, MdDialogConfig, MdSnackBar} from '@angular/material';
+import {RefundPaymentActions} from '../refund-payment.action';
+import {Store} from '@ngrx/store';
+import {Observable} from 'rxjs';
+import {BillingModuleState} from '../../index';
+import {RefundPaymentTask} from '../../../../shared/model/billing/refund-payment-task.interface';
+import { TdDialogService } from "@covalent/core";
+
+@Component({
+  selector: 'pams-refund-payment-draft-task',
+  templateUrl: './refund-payment-draft-task.panel.html',
+})
+
+export class RefundPaymentDraftTaskPanel implements OnInit {
+
+  @Input() refundPaymentTask: RefundPaymentTask;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute,
+              private viewContainerRef: ViewContainerRef,
+              private actions: RefundPaymentActions,
+              private store: Store<BillingModuleState>,
+              private dialog: MdDialog,
+              private _dialogService: TdDialogService,
+              private snackBar: MdSnackBar) {
+  }
+
+  ngOnInit(): void {
+    //this.store.dispatch(this.actions.findInvoiceItems(this.invoiceTask.invoice));
+  }
+
+  register() {
+    this.store.dispatch(this.actions.completeRefundPaymentTask(this.refundPaymentTask));
+    this.goBack();
+  }
+
+  goBack(): void {
+    this.router.navigate(['/secure/billing/invoices']);
+  }
+
+}
