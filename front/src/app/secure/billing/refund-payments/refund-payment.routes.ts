bf5047ae89d180e5f3e927fe17f58e9f76c36581
nurazieta <azieta.ma@umk.edu.my>
2017-08-30 16:29:12 +0800

    add RefundPaymentRoutes

---
+++ front/src/app/secure/billing/refund-payments/refund-payment.routes.ts
@@ -0,0 +1,10 @@
+import {Routes} from '@angular/router';
+import {RefundPaymentCenterPage} from './refund-payment-center.page';
+import {RefundPaymentTaskDetailPage} from './refund-payment-task-detail.page';
+//import {RefundPaymentDetailPage} from './refund-payment-detail.page';
+
+export const RefundPaymentRoutes: Routes = [
+  {path: 'billing/refund-payments', component: RefundPaymentCenterPage},
+  {path: 'billing/refund-payments/refund-payment-task-detail/:taskId', component: RefundPaymentTaskDetailPage},
+  //{path: 'billing/refund-payments/refund-payment-detail/:referenceNo', component: RefundPaymentDetailPage},
+];
