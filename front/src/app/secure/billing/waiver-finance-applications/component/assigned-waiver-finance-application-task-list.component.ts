ab31206b814ff370522a84fc299154b14d6f64ea
aidamardiana <aidamardiana>
2017-08-30 18:01:40 +0800

    Update AssignedWaiverFinanceApplicationTaskListComponent

--- front/src/app/secure/billing/waiver-finance-applications/component/assigned-waiver-finance-application-task-list.component.ts
+++ front/src/app/secure/billing/waiver-finance-applications/component/assigned-waiver-finance-application-task-list.component.ts
@@ -42,45 +42,47 @@ export class AssignedWaiverFinanceApplicationTaskListComponent {
     });
   }
 
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'referenceNo';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+  
 
-  ngAfterViewInit(): void {
-    this.filteredData = this.waiverFinanceApplicationTasks;
-    this.filteredTotal = this.waiverFinanceApplicationTasks.length;
-    this.filter();
-  }
+  // filteredData: any[];
+  // filteredTotal: number;
+  // searchTerm: string = '';
+  // fromRow: number = 1;
+  // currentPage: number = 1;
+  // pageSize: number = 10;
+  // sortBy: string = 'referenceNo';
+  // sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
 
-  sort(sortEvent: ITdDataTableSortChangeEvent): void {
-    this.sortBy = sortEvent.name;
-    this.sortOrder = sortEvent.order;
-    this.filter();
-  }
+  // ngAfterViewInit(): void {
+  //   this.filteredData = this.waiverFinanceApplicationTasks;
+  //   this.filteredTotal = this.waiverFinanceApplicationTasks.length;
+  //   this.filter();
+  // }
 
-  search(searchTerm: string): void {
-    this.searchTerm = searchTerm;
-    this.filter();
-  }
+  // sort(sortEvent: ITdDataTableSortChangeEvent): void {
+  //   this.sortBy = sortEvent.name;
+  //   this.sortOrder = sortEvent.order;
+  //   this.filter();
+  // }
 
-  page(pagingEvent: IPageChangeEvent): void {
-    this.fromRow = pagingEvent.fromRow;
-    this.currentPage = pagingEvent.page;
-    this.pageSize = pagingEvent.pageSize;
-    this.filter();
-  }
+  // search(searchTerm: string): void {
+  //   this.searchTerm = searchTerm;
+  //   this.filter();
+  // }
 
-  filter(): void {
-    let newData: any[] = this.waiverFinanceApplicationTasks;
-    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
-    this.filteredTotal = newData.length;
-    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
-    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
-    this.filteredData = newData;
-  }
+  // page(pagingEvent: IPageChangeEvent): void {
+  //   this.fromRow = pagingEvent.fromRow;
+  //   this.currentPage = pagingEvent.page;
+  //   this.pageSize = pagingEvent.pageSize;
+  //   this.filter();
+  // }
+
+  // filter(): void {
+  //   let newData: any[] = this.waiverFinanceApplicationTasks;
+  //   newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+  //   this.filteredTotal = newData.length;
+  //   newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+  //   newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+  //   this.filteredData = newData;
+  // }
 }
