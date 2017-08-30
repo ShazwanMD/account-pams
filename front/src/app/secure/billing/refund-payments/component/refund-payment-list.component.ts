f0d65b7026d08f40b8cd7c370d6e7bc887a48309
nurazieta <azieta.ma@umk.edu.my>
2017-08-30 16:23:51 +0800

    add RefundPaymentListComponent

---
+++ front/src/app/secure/billing/refund-payments/component/refund-payment-list.component.ts
@@ -0,0 +1,23 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {MdSnackBar} from '@angular/material';
+import {RefundPayment} from '../../../../shared/model/billing/refund-payment.interface';
+
+@Component({
+  selector: 'pams-refund-payment-list',
+  templateUrl: './refund-payment-list.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RefundPaymentListComponent {
+
+  private columns: any[] = [
+    {name: 'issuedDate', label: 'Date'},                        
+    {name: 'referenceNo', label: 'Reference No'},
+    {name: 'invoice.referenceNo', label: 'Invoice'},
+    {name: 'description', label: 'Description'},
+    {name: 'amount', label: 'Total Amount'},
+    {name: 'action', label: ''},
+  ];
+
+  @Input() refundPayment: RefundPayment[];
+
+}
