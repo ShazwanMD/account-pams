1bcb70198227f61642b16a0fd5c35e030cf75fc2
nurazieta <azieta.ma@umk.edu.my>
2017-09-11 14:43:30 +0800

    update changes

--- back/src/main/java/my/edu/umk/pams/account/billing/workflow/task/InvoiceApproveTask.java
+++ back/src/main/java/my/edu/umk/pams/account/billing/workflow/task/InvoiceApproveTask.java
@@ -58,15 +58,15 @@ public class InvoiceApproveTask extends BpmnActivityBehavior
         billingService.post(invoice);
 
 //        // fire event
-//        applicationContext.publishEvent(new InvoiceApprovedEvent(invoice));
-//
-//        AcAccount account = invoice.getAccount();
-//        AccountPayload payload = new AccountPayload();
-//        payload.setCode(account.getCode());
-//        payload.setMatricNo(account.getActor().getIdentityNo());
-//        payload.setOutstanding(accountService.hasBalance(account));
-//        payload.setBalance(accountService.sumBalanceAmount(account));
-//        AccountRevisedEvent event = new AccountRevisedEvent(payload);
-//        applicationContext.publishEvent(event);
+        applicationContext.publishEvent(new InvoiceApprovedEvent(invoice));
+
+        AcAccount account = invoice.getAccount();
+        AccountPayload payload = new AccountPayload();
+        payload.setCode(account.getCode());
+        payload.setMatricNo(account.getActor().getIdentityNo());
+        payload.setOutstanding(accountService.hasBalance(account));
+        payload.setBalance(accountService.sumBalanceAmount(account));
+        AccountRevisedEvent event = new AccountRevisedEvent(payload);
+        applicationContext.publishEvent(event);
     }
 }
