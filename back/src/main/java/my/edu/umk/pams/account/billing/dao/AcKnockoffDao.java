2077c02ea7bf66d2dc03a8a6179b5e268efa5868
hajar <hajar.ad@umk.edu.my>
2017-09-16 13:11:50 +0800

    Add dao for find knockoff by invoice

--- back/src/main/java/my/edu/umk/pams/account/billing/dao/AcKnockoffDao.java
+++ back/src/main/java/my/edu/umk/pams/account/billing/dao/AcKnockoffDao.java
@@ -2,9 +2,16 @@ package my.edu.umk.pams.account.billing.dao;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+
 import my.edu.umk.pams.account.billing.model.AcInvoice;
 import my.edu.umk.pams.account.billing.model.AcKnockoff;
+import my.edu.umk.pams.account.billing.model.AcKnockoffInvoice;
+import my.edu.umk.pams.account.billing.model.AcReceipt;
+import my.edu.umk.pams.account.billing.model.AcReceiptInvoice;
 import my.edu.umk.pams.account.core.AcFlowState;
+import my.edu.umk.pams.account.core.AcMetaState;
 import my.edu.umk.pams.account.core.GenericDao;
 import my.edu.umk.pams.account.identity.model.AcUser;
 
@@ -17,6 +24,8 @@ public interface AcKnockoffDao extends GenericDao<Long, AcKnockoff> {
     List<AcKnockoff> findByFlowState(AcFlowState flowState);
 
     List<AcKnockoff> findByFlowStates(AcFlowState... flowStates);
+    
+    List<AcKnockoffInvoice> find(AcKnockoff knockoff);
 	
 	boolean hasKnockoff(AcKnockoff knockoff);
 	
