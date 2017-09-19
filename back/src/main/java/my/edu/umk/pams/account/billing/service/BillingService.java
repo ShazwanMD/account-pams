88539423aa5b249ef2502e8cdd8f21406b0712f8
nurazieta <azieta.ma@umk.edu.my>
2017-09-19 16:46:53 +0800

    create findReceiptsAccountCharge

--- back/src/main/java/my/edu/umk/pams/account/billing/service/BillingService.java
+++ back/src/main/java/my/edu/umk/pams/account/billing/service/BillingService.java
@@ -281,6 +281,8 @@ public interface BillingService {
     List<AcReceiptItem> findReceiptItems(AcReceipt receipt, AcInvoice invoice);
     
     List<AcReceiptInvoice> findReceipts(AcReceipt receipt);
+    
+    List<AcReceiptAccountCharge> findReceiptsAccountCharge(AcReceipt receipt);
 
     Integer countReceipt(AcReceiptType type);
 
