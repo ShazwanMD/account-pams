e2dd0beda4fdb45f5e438d0169cf80cff1b7ea7f
nurazieta <azieta.ma@umk.edu.my>
2017-09-20 10:44:11 +0800

    add receiptAccountChargeListReducer

--- front/src/app/secure/billing/index.ts
+++ front/src/app/secure/billing/index.ts
@@ -58,6 +58,7 @@ import {invoiceCreditNoteListReducer, InvoiceCreditNoteListState} from './invoic
 import {invoiceDebitNoteListReducer, InvoiceDebitNoteListState} from './invoices/invoice-debit-note-list.reducer';
 import {archivedReceiptListReducer, ReceiptListState} from './receipts/receipt-list.reducer';
 import {receiptInvoiceListReducer, ReceiptInvoiceListState} from './receipts/receipt-invoice.reducer';
+import {receiptAccountChargeListReducer, ReceiptAccountChargeListState} from './receipts/receipt-account-charge.reducer';
 import { AdvancePaymentSubModule } from "./advance-payments/index";
 import {advancePaymentListReducer, AdvancePaymentListState} from './advance-payments/advance-payment-list.reducer';
 import {AdvancePayment} from '../../shared/model/billing/advance-payment.interface';
@@ -103,7 +104,7 @@ export interface BillingModuleState {
   receipt: ReceiptState;
   receiptItems: ReceiptItemListState;
   receiptInvoice: ReceiptInvoiceListState;
-  receiptAccountCharge: AccountChargeListState;
+  receiptAccountCharge: ReceiptAccountChargeListState;
   // selectedInvoice: // previewed invoice to be applied
   // selectedInvoiceItems: // previewed invoice item to be applied
   assignedDebitNoteTasks: DebitNoteTaskListState;
@@ -213,7 +214,7 @@ export const billingModuleReducers = {
   receipts: receiptReducer,
   receiptItems: receiptItemListReducer,
   receiptInvoice: receiptInvoiceListReducer,
-  receiptAccountCharge: accountChargeListReducer,
+  receiptAccountCharge: receiptAccountChargeListReducer,
   assignedDebitNoteTasks: assignedDebitNoteTaskListReducer,
   pooledDebitNoteTasks: pooledDebitNoteTaskListReducer,
   archivedDebitNotes: archivedDebitNoteListReducer,
