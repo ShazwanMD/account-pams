69f1ea51307dc860df62e22032baf65be92ef650
aidamardiana <mardiana.wa@umk.edu.my>
2017-09-24 17:01:10 +0800

    DebitNoteReceiptDialog

--- front/src/app/secure/billing/receipts/dialog/debit-note-receipt-creator.dialog.ts
+++ front/src/app/secure/billing/receipts/dialog/debit-note-receipt-creator.dialog.ts
@@ -1,58 +1,91 @@
 import { DebitNote } from './../../../../shared/model/billing/debit-note.interface';
-import {Component, OnInit, ViewContainerRef, Input} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
-import {ActivatedRoute, Router} from '@angular/router';
-import {BillingModuleState} from '../../index';
-import {MdDialogRef} from '@angular/material';
-import {ReceiptActions} from '../receipt.action';
-import {Store} from '@ngrx/store';
-import {Receipt} from '../../../../shared/model/billing/receipt.interface';
-import {ReceiptItem} from '../../../../shared/model/billing/receipt-item.interface';
-import {ReceiptInvoice} from '../../../../shared/model/billing/receipt-invoice.interface';
-import {ChargeCode} from '../../../../shared/model/account/charge-code.interface';
-import {Invoice} from '../../../../shared/model/billing/invoice.interface';
+import { Component, OnInit, ViewContainerRef, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReceiptItem } from '../../../../shared/model/billing/receipt-item.interface';
+import { Receipt } from '../../../../shared/model/billing/receipt.interface';
+import { BillingModuleState } from '../../index';
+import { MdDialogRef } from '@angular/material';
+import { ReceiptActions } from '../receipt.action';
+import { Store } from '@ngrx/store';
+import { Invoice } from "../../../../shared/model/billing/invoice.interface";
+import { InvoiceItem } from "../../../../shared/model/billing/invoice-item.interface";
+import { Account } from "../../../../shared/model/account/account.interface";
+import { TdDialogService, ITdDataTableColumn } from "@covalent/core";
+import { ChargeCode } from "../../../../shared/model/account/charge-code.interface";
+import { Observable } from "rxjs/Observable";
 import { AccountCharge } from "../../../../shared/model/account/account-charge.interface";
 
-@Component({
-  selector: 'pams-debit-note-receipt-creator',
-  templateUrl: './debit-note-receipt-creator.dialog.html',
-})
+@Component( {
+    selector: 'pams-debit-note-receipt-creator',
+    templateUrl: './debit-note-receipt-creator.dialog.html',
+} )
 
-export class DebitNoteReceiptDialog implements OnInit {
+export class DebitNoteReceiptCreatorDialog implements OnInit {
 
-  private createForm: FormGroup;
-  private edit: boolean = false;
-  private _receipt: Receipt;
+    private createForm: FormGroup;
+    private displayForm: FormGroup;
+    private _receipt: Receipt;
+    private _debitNote: DebitNote;
+    private edit: boolean = false;
 
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              private formBuilder: FormBuilder,
-              private viewContainerRef: ViewContainerRef,
-              private store: Store<BillingModuleState>,
-              private actions: ReceiptActions,
-              private dialog: MdDialogRef<DebitNoteReceiptDialog>) {
-  }
+    constructor( private router: Router,
+        private route: ActivatedRoute,
+        private formBuilder: FormBuilder,
+        private viewContainerRef: ViewContainerRef,
+        private store: Store<BillingModuleState>,
+        private actions: ReceiptActions,
+        private _dialogService: TdDialogService,
+        private dialog: MdDialogRef<DebitNoteReceiptCreatorDialog> ) {
+    }
 
-  set receipt(value: Receipt) {
-    this._receipt = value;
-    this.edit = true;
-  }
+    set receipt( value: Receipt ) {
+        this._receipt = value;
+    }
 
-  ngOnInit(): void {
-      this.createForm = this.formBuilder.group({
-      //referenceNo: '',
-      debitNote: <DebitNote>{},
-      receipt: <Receipt>{},
-    });
-    if (this.edit) this.createForm.patchValue(this._receipt);
-  }
+    set debitNote( value: DebitNote ) {
+        this._debitNote = value;
+        this.edit = true;
 
-  save(receipt: Receipt, isValid: boolean) {
-    
-      console.log("receiptNo" + this._receipt.referenceNo);
-      console.log("Debit Note ref No" + this.createForm.get('debitNote').value.referenceNo);
+    }
+
+    ngOnInit(): void {
+        this.createForm = this.formBuilder.group({
+            id: [undefined],
+            description: [''],
+            dueAmount: [0],
+            totalAmount:[0],
+            adjustedAmount: [0],
+            appliedAmount: [0],
+            price: [0],
+            unit: [0],
+            chargeCode: [<ChargeCode>{}],
+            invoice: [<Invoice>{}],
+            debitNote: [<DebitNote>{}]
+        } );
+
+        if ( this.edit )
+            this.createForm.patchValue( { debitNote: this._debitNote } );
+            this.createForm.patchValue( { dueAmount: this._debitNote.balanceAmount } );
+            
+            if(this._debitNote.balanceAmount <= this._receipt.totalPayment) {
+                this.createForm.patchValue( { appliedAmount: this._debitNote.balanceAmount } );
+                this.createForm.patchValue( { totalAmount: 0 } );
+            } 
+            
+            if(this._debitNote.balanceAmount > this._receipt.totalPayment){
+                this.createForm.patchValue( { appliedAmount: this._receipt.totalPayment } );
+                this.createForm.patchValue( { totalAmount: this._debitNote.balanceAmount-this._receipt.totalPayment } );
+            }
+    }
+
+    submit( item: ReceiptItem, isValid: boolean ) {
       
-    this.store.dispatch(this.actions.addReceiptInvoiceItems(this._receipt, this.createForm.get('debitNote').value));
-    this.dialog.close();
-  }
+        console.log("hantar dh kan" + this._receipt.referenceNo);
+        this.store.dispatch( this.actions.addReceiptItem( this._receipt, item ) );
+        this.dialog.close();
+        
+       // this._receipt.totalPayment = this._receipt.totalPayment - item.appliedAmount;
+        //this.store.dispatch( this.actions.updateReceipt(this._receipt) );
+    }
 }
