0fb1301d3b8f178384cc6e3a1b8674994973edec
aidamardiana <mardiana.wa@umk.edu.my>
2017-09-26 18:06:44 +0800

    Add DebitNoteUnpaidSelectComponent

---
+++ front/src/app/secure/billing/knockoffs/component/debit-note-select-unpaid.component.ts
@@ -0,0 +1,46 @@
+import { DebitNoteActions } from './../../debit-notes/debit-note.action';
+import { DebitNote } from './../../../../shared/model/billing/debit-note.interface';
+import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Store} from '@ngrx/store';
+import {FormControl} from '@angular/forms';
+import {BillingModuleState} from '../../index';
+import {Invoice} from '../../../../shared/model/billing/invoice.interface';
+import {Receipt} from '../../../../shared/model/billing/receipt.interface';
+import { InvoiceActions } from "../../invoices/invoice.action";
+
+@Component({
+  selector: 'pams-debit-note-select-unpaid',
+  templateUrl: './debit-note-select-unpaid.component.html',
+})
+export class DebitNoteUnpaidSelectComponent implements OnInit {
+
+  private DEBIT_NOTE = 'billingModuleState.debitNoteList'.split('.');
+  private debitNotes$: Observable<DebitNote[]>;
+  private selected: DebitNote;
+
+  @Input() placeholder: string;
+  @Input() innerFormControl: FormControl;
+  @Input() preSelected: DebitNote;
+  @Input() receipt: Receipt;
+
+  constructor(private store: Store<BillingModuleState>,
+              private actions: DebitNoteActions) {
+    this.debitNotes$ = this.store.select(...this.DEBIT_NOTE);
+  }
+
+  ngOnInit() {
+    if (this.preSelected) {
+      this.debitNotes$.subscribe((debitNotes) => {
+        this.selected = debitNotes.find((debitNote) => debitNote.id == this.preSelected.id);
+      });
+    }
+  }
+
+  selectChangeEvent(event: DebitNote) {
+    this.innerFormControl.setValue(event, {emitEvent: false});
+    this.selected = event;
+  }
+}
+}
+
