37dbbaddfbd869b9b6db3d8f6c63d89bbc9e86a1
nurazieta <azieta.ma@umk.edu.my>
2017-10-02 15:54:58 +0800

    pagination pooled receipt

--- front/src/app/secure/billing/receipts/component/pooled-receipt-task-list.component.ts
+++ front/src/app/secure/billing/receipts/component/pooled-receipt-task-list.component.ts
@@ -1,4 +1,19 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  ChangeDetectionStrategy, 
+  AfterViewInit, 
+  OnChanges, 
+  SimpleChange
+} from '@angular/core';
+import {
+TdDataTableSortingOrder,
+TdDataTableService,
+ITdDataTableSortChangeEvent,
+IPageChangeEvent
+} from '@covalent/core';
 import {MdSnackBar} from '@angular/material';
 import {ReceiptTask} from '../../../../shared/model/billing/receipt-task.interface';
 
@@ -7,7 +22,7 @@ import {ReceiptTask} from '../../../../shared/model/billing/receipt-task.interfa
   templateUrl: './pooled-receipt-task-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PooledReceiptTaskListComponent {
+export class PooledReceiptTaskListComponent implements OnChanges{
 
   @Input() receiptTasks: ReceiptTask[];
   @Output() claim = new EventEmitter<ReceiptTask>();
@@ -20,9 +35,17 @@ export class PooledReceiptTaskListComponent {
     {name: 'flowState', label: 'Status'},
     {name: 'action', label: ''},
   ];
-
-  constructor(private snackBar: MdSnackBar) {
-  }
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+  constructor(private _dataTableService: TdDataTableService,
+    private snackBar: MdSnackBar) {
+}
 
   claimTask(task: ReceiptTask): void {
     console.log('Emitting task');
@@ -30,5 +53,36 @@ export class PooledReceiptTaskListComponent {
     snackBarRef.afterDismissed().subscribe(() => {
       this.claim.emit(task);
     });
+  }
+
+  ngOnChanges(changes: {[ propName: string]: SimpleChange}) {
+    if (changes['receiptTasks']){
+        this.filteredData = changes['receiptTasks'].currentValue; 
+        this.filteredTotal = changes['receiptTasks'].currentValue.length;
+        this.filter();
+      }
+    }
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+  filter(): void {
+    let newData: any[] = this.receiptTasks;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
   }
 }
