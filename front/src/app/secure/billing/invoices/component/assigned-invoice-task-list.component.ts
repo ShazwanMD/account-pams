90cb9a8c18e587aef8e8fc2574326a34ff9c4be7
nurazieta <azieta.ma@umk.edu.my>
2017-10-03 09:38:03 +0800

    pagination invoice assigned

--- front/src/app/secure/billing/invoices/component/assigned-invoice-task-list.component.ts
+++ front/src/app/secure/billing/invoices/component/assigned-invoice-task-list.component.ts
@@ -1,4 +1,19 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  ChangeDetectionStrategy, 
+  AfterViewInit, 
+  OnChanges, 
+  SimpleChange
+} from '@angular/core';
+import {
+TdDataTableSortingOrder,
+TdDataTableService,
+ITdDataTableSortChangeEvent,
+IPageChangeEvent
+} from '@covalent/core';
 import {InvoiceTask} from '../../../../shared/model/billing/invoice-task.interface';
 import {MdSnackBar, MdSnackBarConfig} from '@angular/material';
 
@@ -7,7 +22,7 @@ import {MdSnackBar, MdSnackBarConfig} from '@angular/material';
   templateUrl: './assigned-invoice-task-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AssignedInvoiceTaskListComponent {
+export class AssignedInvoiceTaskListComponent implements OnChanges{
 
   private columns: any[] = [
     {name: 'issuedDate', label: 'Date'},
@@ -21,11 +36,21 @@ export class AssignedInvoiceTaskListComponent {
     {name: 'action', label: ''},
   ];
 
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
   @Input() invoiceTasks: InvoiceTask[];
   @Output() view = new EventEmitter<InvoiceTask>();
 
-  constructor(private snackBar: MdSnackBar) {
-  }
+  constructor(private _dataTableService: TdDataTableService,
+    private snackBar: MdSnackBar) {
+}
 
   viewTask(task: InvoiceTask): void {
     console.log('Emitting task');
@@ -35,5 +60,36 @@ export class AssignedInvoiceTaskListComponent {
     snackBarRef.afterDismissed().subscribe(() => {
       this.view.emit(task);
     });
+  }
+
+  ngOnChanges(changes: {[ propName: string]: SimpleChange}) {
+    if (changes['invoiceTasks']){
+        this.filteredData = changes['invoiceTasks'].currentValue; 
+        this.filteredTotal = changes['invoiceTasks'].currentValue.length;
+        this.filter();
+      }
+    }
+  sort(sortEvent: ITdDataTableSortChangeEvent): void {
+    this.sortBy = sortEvent.name;
+    this.sortOrder = sortEvent.order;
+    this.filter();
+  }
+  search(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+    this.filter();
+  }
+  page(pagingEvent: IPageChangeEvent): void {
+    this.fromRow = pagingEvent.fromRow;
+    this.currentPage = pagingEvent.page;
+    this.pageSize = pagingEvent.pageSize;
+    this.filter();
+  }
+  filter(): void {
+    let newData: any[] = this.invoiceTasks;
+    newData = this._dataTableService.filterData(newData, this.searchTerm, true);
+    this.filteredTotal = newData.length;
+    newData = this._dataTableService.sortData(newData, this.sortBy, this.sortOrder);
+    newData = this._dataTableService.pageData(newData, this.fromRow, this.currentPage * this.pageSize);
+    this.filteredData = newData;
   }
 }
