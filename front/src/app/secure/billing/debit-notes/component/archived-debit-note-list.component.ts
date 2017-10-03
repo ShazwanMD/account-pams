fc9aa521e28d1caef8d21be4ab4fa1b0cf509844
nurazieta <azieta.ma@umk.edu.my>
2017-10-03 11:16:17 +0800

    repair pagination debit

--- front/src/app/secure/billing/debit-notes/component/archived-debit-note-list.component.ts
+++ front/src/app/secure/billing/debit-notes/component/archived-debit-note-list.component.ts
@@ -1,20 +1,28 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  ChangeDetectionStrategy, 
+  AfterViewInit, 
+  OnChanges, 
+  SimpleChange
+} from '@angular/core';
+import {
+TdDataTableSortingOrder,
+TdDataTableService,
+ITdDataTableSortChangeEvent,
+IPageChangeEvent
+} from '@covalent/core';
 import {MdSnackBar} from '@angular/material';
 import {DebitNote} from '../../../../shared/model/billing/debit-note.interface';
 import {DebitNoteTask} from '../../../../shared/model/billing/debit-note-task.interface';
-import {
-  IPageChangeEvent,
-  ITdDataTableSortChangeEvent,
-  TdDataTableService,
-  TdDataTableSortingOrder
-} from '@covalent/core';
-
 @Component({
   selector: 'pams-archived-debit-note-list',
   templateUrl: './archived-debit-note-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArchivedDebitNoteListComponent {
+export class ArchivedDebitNoteListComponent implements OnChanges {
 
   private columns: any[] = [
     {name: 'debitNoteDate', label: 'Date'},
@@ -30,12 +38,21 @@ export class ArchivedDebitNoteListComponent {
     {name: 'action', label: ''},
   ];
 
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
   @Input() debitNotes: DebitNoteTask[];
   @Output() view = new EventEmitter<DebitNote>();
 
-  constructor(private snackBar: MdSnackBar,
-              private _dataTableService: TdDataTableService) {
-  }
+  constructor(private _dataTableService: TdDataTableService,
+    private snackBar: MdSnackBar) {
+}
 
   viewDebitNote(debit: DebitNote): void {
     console.log('Emitting task');
@@ -44,40 +61,28 @@ export class ArchivedDebitNoteListComponent {
       this.view.emit(debit);
     });
   }
-
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'referenceNo';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-
-  ngAfterViewInit(): void {
-    this.filteredData = this.debitNotes;
-    this.filteredTotal = this.debitNotes.length;
-    this.filter();
-  }
-
+  ngOnChanges(changes: {[ propName: string]: SimpleChange}) {
+    if (changes['debitNotes']){
+        this.filteredData = changes['debitNotes'].currentValue; 
+        this.filteredTotal = changes['debitNotes'].currentValue.length;
+        this.filter();
+      }
+    }
   sort(sortEvent: ITdDataTableSortChangeEvent): void {
     this.sortBy = sortEvent.name;
     this.sortOrder = sortEvent.order;
     this.filter();
   }
-
   search(searchTerm: string): void {
     this.searchTerm = searchTerm;
     this.filter();
   }
-
   page(pagingEvent: IPageChangeEvent): void {
     this.fromRow = pagingEvent.fromRow;
     this.currentPage = pagingEvent.page;
     this.pageSize = pagingEvent.pageSize;
     this.filter();
   }
-
   filter(): void {
     let newData: any[] = this.debitNotes;
     newData = this._dataTableService.filterData(newData, this.searchTerm, true);
