6c42af3868a92db7ec07f4a388b08e7e89dab461
nurazieta <azieta.ma@umk.edu.my>
2017-10-03 11:32:38 +0800

    pagination credit archieved

--- front/src/app/secure/billing/credit-notes/component/archived-credit-note-list.component.ts
+++ front/src/app/secure/billing/credit-notes/component/archived-credit-note-list.component.ts
@@ -1,20 +1,29 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  ChangeDetectionStrategy, 
+  AfterViewInit, 
+  OnChanges, 
+  SimpleChange
+} from '@angular/core';
+import {
+TdDataTableSortingOrder,
+TdDataTableService,
+ITdDataTableSortChangeEvent,
+IPageChangeEvent
+} from '@covalent/core';
 import {MdSnackBar} from '@angular/material';
 import {CreditNote} from '../../../../shared/model/billing/credit-note.interface';
 import {CreditNoteTask} from '../../../../shared/model/billing/credit-note-task.interface';
-import {
-  IPageChangeEvent,
-  ITdDataTableSortChangeEvent,
-  TdDataTableService,
-  TdDataTableSortingOrder
-} from '@covalent/core';
 
 @Component({
   selector: 'pams-archived-credit-note-list',
   templateUrl: './archived-credit-note-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArchivedCreditNoteListComponent {
+export class ArchivedCreditNoteListComponent implements OnChanges{
 
   private columns: any[] = [
     {name: 'creditNoteDate', label: 'Date'},
@@ -30,6 +39,15 @@ export class ArchivedCreditNoteListComponent {
     {name: 'action', label: ''},
   ];
 
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
   @Input() creditNotes: CreditNoteTask[];
   @Output() view = new EventEmitter<CreditNote>();
 
@@ -44,40 +62,28 @@ export class ArchivedCreditNoteListComponent {
       this.view.emit(credit);
     });
   }
-
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'referenceNo';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-
-  ngAfterViewInit(): void {
-    this.filteredData = this.creditNotes;
-    this.filteredTotal = this.creditNotes.length;
-    this.filter();
-  }
-
+  ngOnChanges(changes: {[ propName: string]: SimpleChange}) {
+    if (changes['creditNotes']){
+        this.filteredData = changes['creditNotes'].currentValue; 
+        this.filteredTotal = changes['creditNotes'].currentValue.length;
+        this.filter();
+      }
+    }
   sort(sortEvent: ITdDataTableSortChangeEvent): void {
     this.sortBy = sortEvent.name;
     this.sortOrder = sortEvent.order;
     this.filter();
   }
-
   search(searchTerm: string): void {
     this.searchTerm = searchTerm;
     this.filter();
   }
-
   page(pagingEvent: IPageChangeEvent): void {
     this.fromRow = pagingEvent.fromRow;
     this.currentPage = pagingEvent.page;
     this.pageSize = pagingEvent.pageSize;
     this.filter();
   }
-
   filter(): void {
     let newData: any[] = this.creditNotes;
     newData = this._dataTableService.filterData(newData, this.searchTerm, true);
