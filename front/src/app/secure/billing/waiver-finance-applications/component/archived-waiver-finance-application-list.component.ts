081c1002c9af440406aac70947fb2260682bb945
nurazieta <azieta.ma@umk.edu.my>
2017-10-05 14:44:29 +0800

    update archived waiver finance ts in billing

--- front/src/app/secure/billing/waiver-finance-applications/component/archived-waiver-finance-application-list.component.ts
+++ front/src/app/secure/billing/waiver-finance-applications/component/archived-waiver-finance-application-list.component.ts
@@ -1,19 +1,28 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  ChangeDetectionStrategy, 
+  AfterViewInit, 
+  OnChanges, 
+  SimpleChange
+} from '@angular/core';
+import {
+TdDataTableSortingOrder,
+TdDataTableService,
+ITdDataTableSortChangeEvent,
+IPageChangeEvent
+} from '@covalent/core';
 import {MdSnackBar} from '@angular/material';
 import {WaiverFinanceApplication} from '../../../../shared/model/billing/waiver-finance-application.interface';
-import {
-  IPageChangeEvent,
-  ITdDataTableSortChangeEvent,
-  TdDataTableService,
-  TdDataTableSortingOrder
-} from '@covalent/core';
 
 @Component({
   selector: 'pams-archived-waiver-finance-application-list',
   templateUrl: './archived-waiver-finance-application-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ArchivedWaiverFinanceApplicationListComponent {
+export class ArchivedWaiverFinanceApplicationListComponent implements OnChanges{
 
   @Input() waiverFinanceApplications: WaiverFinanceApplication[];
   @Output() view = new EventEmitter<WaiverFinanceApplication>();
@@ -29,6 +38,15 @@ export class ArchivedWaiverFinanceApplicationListComponent {
     {name: 'action', label: ''},
   ];
 
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
+
   constructor(private snackBar: MdSnackBar,
               private _dataTableService: TdDataTableService) {
   }
@@ -41,39 +59,28 @@ export class ArchivedWaiverFinanceApplicationListComponent {
     });
   }
 
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'referenceNo';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-
-  ngAfterViewInit(): void {
-    this.filteredData = this.waiverFinanceApplications;
-    this.filteredTotal = this.waiverFinanceApplications.length;
-    this.filter();
-  }
-
+  ngOnChanges(changes: {[ propName: string]: SimpleChange}) {
+    if (changes['waiverFinanceApplications']){
+        this.filteredData = changes['waiverFinanceApplications'].currentValue; 
+        this.filteredTotal = changes['waiverFinanceApplications'].currentValue.length;
+        this.filter();
+      }
+    }
   sort(sortEvent: ITdDataTableSortChangeEvent): void {
     this.sortBy = sortEvent.name;
     this.sortOrder = sortEvent.order;
     this.filter();
   }
-
   search(searchTerm: string): void {
     this.searchTerm = searchTerm;
     this.filter();
   }
-
   page(pagingEvent: IPageChangeEvent): void {
     this.fromRow = pagingEvent.fromRow;
     this.currentPage = pagingEvent.page;
     this.pageSize = pagingEvent.pageSize;
     this.filter();
   }
-
   filter(): void {
     let newData: any[] = this.waiverFinanceApplications;
     newData = this._dataTableService.filterData(newData, this.searchTerm, true);
