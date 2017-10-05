6fb7d9a39af309c3252035844f0e1f2366c332b2
nurazieta <azieta.ma@umk.edu.my>
2017-10-05 14:45:57 +0800

    update assigned waiver ts in billing

--- front/src/app/secure/billing/waiver-finance-applications/component/pooled-waiver-finance-application-task-list.component.ts
+++ front/src/app/secure/billing/waiver-finance-applications/component/pooled-waiver-finance-application-task-list.component.ts
@@ -1,19 +1,28 @@
-import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from '@angular/core';
+import {
+  Component, 
+  Input, 
+  EventEmitter, 
+  Output, 
+  ChangeDetectionStrategy, 
+  AfterViewInit, 
+  OnChanges, 
+  SimpleChange
+} from '@angular/core';
+import {
+TdDataTableSortingOrder,
+TdDataTableService,
+ITdDataTableSortChangeEvent,
+IPageChangeEvent
+} from '@covalent/core';
 import {MdSnackBar} from '@angular/material';
 import {WaiverFinanceApplicationTask} from '../../../../shared/model/billing/waiver-finance-application-task.interface';
-import {
-  IPageChangeEvent,
-  ITdDataTableSortChangeEvent,
-  TdDataTableService,
-  TdDataTableSortingOrder
-} from '@covalent/core';
 
 @Component({
   selector: 'pams-pooled-waiver-finance-application-task-list',
   templateUrl: './pooled-waiver-finance-application-task-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PooledWaiverFinanceApplicationTaskListComponent {
+export class PooledWaiverFinanceApplicationTaskListComponent implements OnChanges{
 
   @Input() waiverFinanceApplicationTasks: WaiverFinanceApplicationTask[];
   @Output() claim = new EventEmitter<WaiverFinanceApplicationTask>();
@@ -28,7 +37,14 @@ export class PooledWaiverFinanceApplicationTaskListComponent {
     {name: 'flowState', label: 'Status'},
     {name: 'action', label: ''}
   ];
-
+  filteredData: any[];
+  filteredTotal: number;
+  searchTerm: string = '';
+  fromRow: number = 1;
+  currentPage: number = 1;
+  pageSize: number = 5;
+  sortBy: string = 'referenceNo';
+  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
   constructor(private snackBar: MdSnackBar,
               private _dataTableService: TdDataTableService) {
   }
@@ -41,40 +57,28 @@ export class PooledWaiverFinanceApplicationTaskListComponent {
     });
   }
 
-  filteredData: any[];
-  filteredTotal: number;
-  searchTerm: string = '';
-  fromRow: number = 1;
-  currentPage: number = 1;
-  pageSize: number = 10;
-  sortBy: string = 'referenceNo';
-  sortOrder: TdDataTableSortingOrder = TdDataTableSortingOrder.Descending;
-
-
-  ngAfterViewInit(): void {
-    this.filteredData = this.waiverFinanceApplicationTasks;
-    this.filteredTotal = this.waiverFinanceApplicationTasks.length;
-    this.filter();
-  }
-
+  ngOnChanges(changes: {[ propName: string]: SimpleChange}) {
+    if (changes['waiverFinanceApplicationTasks']){
+        this.filteredData = changes['waiverFinanceApplicationTasks'].currentValue; 
+        this.filteredTotal = changes['waiverFinanceApplicationTasks'].currentValue.length;
+        this.filter();
+      }
+    }
   sort(sortEvent: ITdDataTableSortChangeEvent): void {
     this.sortBy = sortEvent.name;
     this.sortOrder = sortEvent.order;
     this.filter();
   }
-
   search(searchTerm: string): void {
     this.searchTerm = searchTerm;
     this.filter();
   }
-
   page(pagingEvent: IPageChangeEvent): void {
     this.fromRow = pagingEvent.fromRow;
     this.currentPage = pagingEvent.page;
     this.pageSize = pagingEvent.pageSize;
     this.filter();
   }
-
   filter(): void {
     let newData: any[] = this.waiverFinanceApplicationTasks;
     newData = this._dataTableService.filterData(newData, this.searchTerm, true);
