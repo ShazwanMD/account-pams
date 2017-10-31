66493fce742b0e716c7154372b3fc83fdf30dac7
aidamardiana <mardiana.wa@umk.edu.my>
2017-10-31 12:57:31 +0800

    Add column paid

--- front/src/app/secure/billing/invoices/component/archived-invoice-list.component.ts
+++ front/src/app/secure/billing/invoices/component/archived-invoice-list.component.ts
@@ -33,6 +33,7 @@ export class ArchivedInvoiceListComponent implements OnChanges {
     { name: 'flowState', label: 'Status' },
     { name: 'creatorUsername', label: 'Creator' },
     { name: 'createdDate', label: 'Created Date' },
+    { name: 'paid', label: 'Status' },
     { name: 'action', label: '' },
   ];
 
