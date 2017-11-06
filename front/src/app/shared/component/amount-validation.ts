0d9099a5b4fd1af038ec14cad5b3aa852188102f
aidamardiana <mardiana.wa@umk.edu.my>
2017-11-06 17:04:51 +0800

    Update validation

--- front/src/app/shared/component/amount-validation.ts
+++ front/src/app/shared/component/amount-validation.ts
@@ -1,9 +1,10 @@
 import {AbstractControl} from '@angular/forms';
 export class AmountValidation {
     static CheckAmount(AC: AbstractControl) {
+       let totalPayment = AC.get ('totalPaid').value;
        let appliedAmount = AC.get('appliedAmount').value; // to get value in input tag
        let dueAmount = AC.get('dueAmount').value; // to get value in input tag
-        if(appliedAmount >= dueAmount) {
+        if(appliedAmount > dueAmount || appliedAmount > totalPayment ) {
             console.log('false');
             AC.get('appliedAmount').setErrors( {CheckAmount: true} )
         } else {
