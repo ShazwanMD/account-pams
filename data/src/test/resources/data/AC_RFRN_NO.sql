7b2af25a8d6a0d6901e2fa6b41591dc384259a90
hajar <hajar.ad@umk.edu.my>
2017-12-03 15:34:38 +0800

    update reference no

--- data/src/test/resources/data/AC_RFRN_NO.sql
+++ data/src/test/resources/data/AC_RFRN_NO.sql
@@ -8,7 +8,7 @@ INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, P
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcPromoCode.referenceNo', 1, 'Promo Code Reference No Format', 1, 'STLT', '{#a}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
-VALUES (nextval('SQ_AC_RFRN_NO'), 'AcInvoice.referenceNo', 1, 'Invoice Reference No Format', 1, 'INVC', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
+VALUES (nextval('SQ_AC_RFRN_NO'), 'AcInvoice.referenceNo', 1, 'Invoice Reference No Format', 1, 'INVC', '{#academicSession.getCode()}-{#j}', '000000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcInvoice.invoiceNo', 1, 'Invoice Reference No Format', 1, 'INVC', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
@@ -20,10 +20,10 @@ INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, P
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcReceipt.receiptNo', 1, 'Receipt Reference No Format', 1, 'RCPT', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
-VALUES (nextval('SQ_AC_RFRN_NO'), 'AcDebitNote.referenceNo', 1, 'Debit Note Reference No Format', 1, 'DTNT', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
+VALUES (nextval('SQ_AC_RFRN_NO'), 'AcDebitNote.referenceNo', 1, 'Debit Note Reference No Format', 1, 'DTNT', '{#academicSession.getCode()}-{#j}', '000000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
-VALUES (nextval('SQ_AC_RFRN_NO'), 'AcCreditNote.referenceNo', 1, 'Credit Note Reference No Format', 1, 'CTNT', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
+VALUES (nextval('SQ_AC_RFRN_NO'), 'AcCreditNote.referenceNo', 1, 'Credit Note Reference No Format', 1, 'CTNT', '{#academicSession.getCode()}-{#j}', '000000', 1, 0, CURRENT_TIMESTAMP );
 
 INSERT INTO AC_RFRN_NO (ID, CODE, CURRENT_VALUE, DESCRIPTION, INCREMENT_VALUE, PREFIX, REFERENCE_FORMAT, SEQUENCE_FORMAT, M_ST, C_ID, C_TS )
 VALUES (nextval('SQ_AC_RFRN_NO'), 'AcWaiverApplication.referenceNo', 1, 'Waiver Application Reference No Format', 1, 'WVR', '{#academicSession.getCode()}-{#j}', '000', 1, 0, CURRENT_TIMESTAMP );
