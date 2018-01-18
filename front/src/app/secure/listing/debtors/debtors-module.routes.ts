992fac7a4a83217bdc2c3490f6fd2ad94f68865d
nurazieta <azieta.ma@umk.edu.my>
2018-01-18 15:36:00 +0800

    listing debtors

---
+++ front/src/app/secure/listing/debtors/debtors-module.routes.ts
@@ -0,0 +1,6 @@
+import {Routes} from '@angular/router';
+import { DebtorsCenterPage } from './debtors-center.page';
+
+export const DebtorsModuleRoutes: Routes = [
+  {path: 'listing/debtors', component: DebtorsCenterPage},
+];
